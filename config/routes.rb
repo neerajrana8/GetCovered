f16d683673c1f698f503a629fb0c8f2cc7a41021
Dylan Gaines <dylan@dylangaines.com>
2019-04-02 15:45:16 -0700

    Setting up Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'SuperAdmin', at: '/v2/auth'
 
   mount_devise_token_auth_for 'Staff', at: 'auth'
+
+  mount_devise_token_auth_for 'User', at: 'auth'
+  as :user do
+    # Define routes for User within this block.
+  end
   as :staff do
     # Define routes for Staff within this block.
   end
