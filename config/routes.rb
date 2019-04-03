6743d4aa43b4fbbc23c1d04aca74b07c11ccea52
NikitaKis <900940@mail.ru>
2019-04-03 14:12:57 +0400

    Routes split into 4 files

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'Staff', at: 'auth'
 
   mount_devise_token_auth_for 'User', at: 'auth'
-  as :user do
-    # Define routes for User within this block.
-  end
-  as :staff do
-    # Define routes for Staff within this block.
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  draw :public
+  draw :staff
+  draw :superadmin
+  draw :user
+  
 end
---
+++ config/routes/staff.rb
@@ -0,0 +1,3 @@
+namespace :stuff do
+  
+end
