30782a8c9c93a02124d9ca4123235938ba58a135
sbaha88 <sbaha88@gmail.com>
2019-04-18 13:59:33 +0600

    Added trackable to Devise models

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -4,7 +4,7 @@ class Staff < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   include SetAsOwner
   include RecordChange
   include DeviseTokenAuth::Concerns::User
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   include RecordChange
   include DeviseTokenAuth::Concerns::User
 
