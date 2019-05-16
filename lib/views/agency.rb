bdcfca414e995e8aa9e4885038965fec050d6394
DylanRothko <dylan@dylangaines.com>
2019-05-16 09:34:46 -0700

    Fixes for agency view file example from call

--- lib/views/agency.rb
+++ lib/views/agency.rb
@@ -1,10 +1,9 @@
 @view = {
   table_name: "agencies",
-  has_many: [
+  relationship_plural: [
     "staffs": { index: false, show: true, short: false }
   ], # List has many models included on views (optional)
-  has_one: [], # List has one included on views (optional)
-  belongs_to: [
+  relationship_singular: [
     "staff": { index: false, show: true, short: false }
   ], # List belongs to included on views (optional)
   short: [ :id, :title, :created_at, :updated_at ], # list of fields to include in _short form defined as a string or symbol
@@ -13,14 +12,15 @@
       available: true,
       exclusions: {
         models: [], # model name to be excluded from view in this namespace
-        fields: [], # field name to be excluded from view in this namespace 
+        fields: [ :stripe_id ], # field name to be excluded from view in this namespace 
       }
     },
-    account: {
+    staff: {
       available: true,
       exclusions: {
-        models: [], # model name to be excluded from view in this namespace, defined as a string or symbol
-        fields: [], # field name to be excluded from view in this namespace, defined as a string or symbol
+        models: [ :staffs ], # model name to be excluded from view in this namespace, defined as a string or symbol
+        fields: [ :stripe_id, :whitelabel, :tos_accepted, 
+                  :tos_accepted_at, :tos_acceptance_ip ], # field name to be excluded from view in this namespace, defined as a string or symbol
       }      
     },
     user: {
