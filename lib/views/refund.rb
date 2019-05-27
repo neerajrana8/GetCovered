639ae5bc2428f680d649764e0ccdbeb8016fa416
NikitaKis <900940@mail.ru>
2019-05-27 09:24:13 +0400

    Refund view template

---
+++ lib/views/refund.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+@view = {
+  table_name: 'refunds',
+  relationship_plural: [
+    "notifications": { index: true, show: true, short: false }
+  ],
+  relationship_singular: [
+    "charge": { index: false, show: true, short: false }
+  ],
+  short: [],
+  namespaces: [
+    utility: {
+      available: true,
+      exclusions: {
+        models: [],
+        fields: [:stripe_id]
+      }
+    },
+    staff: {
+      available: true,
+      exclusions: {
+        models: [:staffs],
+        fields: %i[stripe_id]
+      }
+    },
+    user: {
+      available: false
+    },
+    public: {
+      available: false
+    }
+  ]
+}
+
+return @view
