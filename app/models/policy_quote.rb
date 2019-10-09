0b3a73e7655edbcd34f3d2bff09dfaf6f88a2338
sbaha88 <sbaha88@gmail.com>
2019-10-09 17:31:21 +0600

    use billing strategy from policy_application

--- app/models/policy_quote.rb
+++ app/models/policy_quote.rb
@@ -50,76 +50,28 @@ class PolicyQuote < ApplicationRecord
     billing_started = false
 
     if !policy.nil? && policy_premium.total > 0 && status == "accepted"
-      policy.agency.billing_strategies.each do |strategy|
-        strategy.new_business['payments'].each_with_index do |payment, index|
-          amount = policy_premium.total * (payment.to_f / 100)
-          next if amount == 0
+      policy_application.billing_strategy.new_business['payments'].each_with_index do |payment, index|
+        amount = policy_premium.total * (payment.to_f / 100)
+        next if amount == 0
 
-          # Due date is today for the first invoice. After that it is due date
-          # after effective date of the policy
-          due_date = index == 0 ? status_updated_on : policy.effective_date + index.months
-          invoice = policy.invoices.new do |inv|
-            inv.due_date        = due_date
-            inv.available_date  = due_date + available_period
-            inv.user            = policy.primary_user
-            inv.amount          = amount
-          end
-          invoice.save
+        # Due date is today for the first invoice. After that it is due date
+        # after effective date of the policy
+        due_date = index == 0 ? status_updated_on : policy.effective_date + index.months
+        invoice = policy.invoices.new do |inv|
+          inv.due_date        = due_date
+          inv.available_date  = due_date + available_period
+          inv.user            = policy.primary_user
+          inv.amount          = amount
         end
-        charge_invoice = policy.invoices.first.pay(allow_upcoming: true)
+        invoice.save
+      end
+      charge_invoice = policy.invoices.first.pay(allow_upcoming: true)
 
-        if charge_invoice[:success] == true
-          return true
-        end
+      if charge_invoice[:success] == true
+        return true
       end
     end
-    billing_status
-  end
-
-
-  # Build Invoices from Schedule
-  #
-
-  def build_invoices_from_strategy
-    build_complete = false
-
-    error_hash = {}
-
-    if policy && premium.to_f > 0
-      policy.agency.billing_strategies.each do |strategy|
-        # This calculates due_date
-        due_date = status_updated_on + 12 / strategy.new_business['payments_per_term']
-        invoice = policy.invoices.new(
-          due_date: due_date,
-          available_date: available_date
-        )
-
-        policy_rates.each do |r|
-          price = r.premium / strategy.new_business['payments']
-
-          line_item = invoice.line_items.new(title: r.title, price: price.floor)
-        end
-
-        sub_total = invoice.line_items.inject(0) { |result, line_item| result += line_item.price }
-
-        if sub_total < inv['amount'].to_i
-          adjustment = invoice.line_items.new(title: "#{ self.carrier.title } Adjustment", price: (inv['amount'] - sub_total))
-        end
-
-        tmp_total = invoice.line_items.inject(0) { |result, line_item| result += line_item.price }
-
-        invoice.subtotal = tmp_total
-        invoice.total = tmp_total
-
-        if invoice.save()
-          inv['invoice_id'] = invoice.id
-        else
-          pp invoice.errors
-          error_hash[inv['index'].to_s] = true
-        end
-
-      end
-    end
+    billing_started
   end
 
   private
