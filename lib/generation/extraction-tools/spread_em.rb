f71d464869e0a4ef28a79a3e23786ccf24a57baf
Joshua Brinsfield <sapphous@gmail.com>
2019-10-16 18:54:05 -0500

    Add nice pre-generation tools

---
+++ lib/generation/extraction-tools/spread_em.rb
@@ -0,0 +1,38 @@
+# utilities
+def get_field_type_string(f_data)
+  to_return = f_data['type'] # for now we just directly return the type, no special markup like including enum options etc... but this is here in case we want to add that later
+  return to_return
+end
+def get_association_type_string(a_data)
+  to_return = a_data['type']
+  to_return = "#{to_return}(#{a_data['target'].join(',')})" unless a_data['target'].blank?
+  return to_return
+end
+
+
+# prepare
+scheme_file = File.read(File.join(__dir__, "scheme.json"))
+scheme = JSON.parse(scheme_file)
+models_csv = ""
+fields_csv = ""
+associations_csv = ""
+
+
+
+# build models csv
+models_csv = scheme['models'].keys.join("\n")
+
+# build fields csv
+fields_csv = scheme['models'].map{|mk, mv| mv['fields'].map{|fk,fv| [mk, fv['name'], fv['type']].join(',') } }.flatten.join("\n")
+
+# build associations csv
+associations_csv = scheme['models'].map{|mk, mv| mv['associations'].map{|ak,av| [mk, "\"#{av['target'].join(',')}\"", av['through'] || '', av['type'], ak].join(',') } }.flatten.join("\n")
+
+
+
+# spit out csvs
+['models', 'fields', 'associations'].each do |csv_thang|
+  File.open(File.join(__dir__, "#{csv_thang}.csv"), "w") do |f|
+    f.write(eval("#{csv_thang}_csv"))
+  end
+end
