6d3bc5a2f0235117a3083462e1a0f8075278b527
evanilukhin <evanilukhin@gmail.com>
2019-11-14 19:01:04 +0300

    User Controller API
    Fix missed casting
    Little refactoring

--- app/controllers/v2/user/users_controller.rb
+++ app/controllers/v2/user/users_controller.rb
@@ -5,54 +5,48 @@
 module V2
   module User
     class UsersController < UserController
-      
       before_action :set_user,
-        only: [:update, :show]
-      
-      def show
-      end
-      
+                    only: %i[update show]
+
+      def show; end
+
       def update
         if update_allowed?
           if @user.update_as(current_user, update_params)
-            render :show,
-              status: :ok
+            render :show, status: :ok
           else
-            render json: @user.errors,
-              status: :unprocessable_entity
+            render json: @user.errors, status: :unprocessable_entity
           end
         else
-          render json: { success: false, errors: ['Unauthorized Access'] },
-            status: :unauthorized
+          render json: { success: false, errors: ['Unauthorized Access'] }, status: :unauthorized
         end
       end
-      
-      
+
       private
-      
-        def view_path
-          super + "/users"
-        end
-        
-        def update_allowed?
-          true
-        end
-        
-        def set_user
-          @user = access_model(::User, params[:id])
-        end
-        
-        def update_params
-          return({}) if params[:user].blank?
-          params.require(:user).permit(
-            notification_options: {}, settings: {},
-            profile_attributes: [
-              :birth_date, :contact_email, :contact_phone, :first_name,
-              :job_title, :last_name, :middle_name, :suffix, :title
-            ]
-          )
-        end
-        
+
+      def view_path
+        super + '/users'
+      end
+
+      def update_allowed?
+        true
+      end
+
+      def set_user
+        @user = access_model(::User, params[:id])
+      end
+
+      def update_params
+        return({}) if params[:user].blank?
+
+        params.require(:user).permit(
+          notification_options: {}, settings: {},
+          profile_attributes: %i[
+            birth_date contact_email contact_phone first_name
+            job_title last_name middle_name suffix title
+          ]
+        )
+      end
     end
-  end # module User
+  end
 end
--- app/controllers/v2/user_controller.rb
+++ app/controllers/v2/user_controller.rb
@@ -4,18 +4,25 @@
 
 module V2
   class UserController < V2Controller
-    
     before_action :authenticate_user!
-    
+
     private
 
-      def view_path
-        super + "/user"
+    def view_path
+      super + '/user'
+    end
+
+    # this method returns:
+    # - current user if you trying to get user/users with the same id;
+    # - all model_class' objects (example result: current_user.invoices) if model_id is nil;
+    # - find a model_class' object (example result: current_user.invoices.find(model_id)) if model_id present.
+    def access_model(model_class, model_id = nil)
+      if model_class == ::User && (model_id == current_user.id.to_s) # model_id can be nil or string
+        current_user
+      else
+        model_class_params = model_id.nil? ? [:itself] : [:find, model_id]
+        current_user.send(model_class.name.underscore.pluralize).send(*model_class_params)
       end
-      
-      def access_model(model_class, model_id = nil)
-        model_class == ::User && model_id == current_user.id ? current_user : current_user.send(model_class.name.underscore.pluralize).send(*(model_id.nil? ? [:itself] : [:find, model_id]))
-      end
-      
+    end
   end
 end
