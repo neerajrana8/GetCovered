f6d9814c3c10bfd06792cd135f68d44ae704906b
evanilukhin <evanilukhin@gmail.com>
2019-11-15 11:01:08 +0300

    User Invoices API
    Remove redundant check

--- app/controllers/v2/user_controller.rb
+++ app/controllers/v2/user_controller.rb
@@ -13,11 +13,11 @@ module V2
     end
 
     # this method returns:
-    # - current user if you trying to get user/users with the same id;
+    # - current user if you trying to get user, even if you try to get/modify user with another id;
     # - all model_class' objects (example result: current_user.invoices) if model_id is nil;
     # - find a model_class' object (example result: current_user.invoices.find(model_id)) if model_id present.
     def access_model(model_class, model_id = nil)
-      if model_class == ::User && (model_id == current_user.id.to_s) # model_id can be nil or string
+      if model_class == ::User
         current_user
       else
         model_class_params = model_id.nil? ? [:itself] : [:find, model_id]
