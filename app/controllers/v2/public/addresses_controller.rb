9db89288afca3401b7c130841b5c8cda43f48a8e
Dylan Gaines <dylan@dylangaines.com>
2019-12-30 18:04:20 -0800

    Adding agency_id to addresses response

--- app/controllers/v2/public/addresses_controller.rb
+++ app/controllers/v2/public/addresses_controller.rb
@@ -12,8 +12,29 @@ module V2
 			  	@ids = @addresses.map { |a| a["_source"]["addressable_id"] }
 			  	
 			  	@insurables = Insurable.find(@ids)
-			  
-					render json: @insurables.to_json({ :include => [:addresses, :insurables] }),
+					
+					@response = []
+					
+					unless @insurables.nil?
+						@insurables.each do |i|
+							@response.push({
+								id: @insurable.id,
+								title: @insurable.title,
+								enabled: @insurable.enabled,
+								account_id: @insurable.account_id,
+								agency_id: @insurable.account.agency_id,
+								insurable_type_id: @insurable.insurable_type_id,
+								category: @insurable.category,
+								covered: @insurable.covered,
+								created_at: @insurable.created_at,
+								updated_at: @insurable.updated_at,
+								addresses: @insurable.addresses,
+								insurables: @insurable.insurables
+							})							
+						end
+					end
+					
+					render json: @response.to_json,
                	 status: :ok 
 			  else
 			  	render json: [].to_json,
