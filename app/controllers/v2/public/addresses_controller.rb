6164e47c0b7c0a60e87a06e092bd31713e511a89
DylanRothko <dylan@dylangaines.com>
2019-12-30 18:06:08 -0800

    Fixing controller error

--- app/controllers/v2/public/addresses_controller.rb
+++ app/controllers/v2/public/addresses_controller.rb
@@ -18,18 +18,18 @@ module V2
 					unless @insurables.nil?
 						@insurables.each do |i|
 							@response.push({
-								id: @insurable.id,
-								title: @insurable.title,
-								enabled: @insurable.enabled,
-								account_id: @insurable.account_id,
-								agency_id: @insurable.account.agency_id,
-								insurable_type_id: @insurable.insurable_type_id,
-								category: @insurable.category,
-								covered: @insurable.covered,
-								created_at: @insurable.created_at,
-								updated_at: @insurable.updated_at,
-								addresses: @insurable.addresses,
-								insurables: @insurable.insurables
+								id: i.id,
+								title: i.title,
+								enabled: i.enabled,
+								account_id: i.account_id,
+								agency_id: i.account.agency_id,
+								insurable_type_id: i.insurable_type_id,
+								category: i.category,
+								covered: i.covered,
+								created_at: i.created_at,
+								updated_at: i.updated_at,
+								addresses: i.addresses,
+								insurables: i.insurables
 							})							
 						end
 					end
