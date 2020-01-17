e88e53ca58cff933437fc7a5b61d1d30760168f0
Dylan Gaines <dylan@dylangaines.com>
2020-01-16 19:13:35 -0800

    elasticsearch index fix

---
+++ db/seeds/elasticsearch.rb
@@ -0,0 +1,9 @@
+@models = [Account, Address, Agency, Carrier, Claim, 
+					 Insurable, Invoice, Lease, PolicyApplication,
+					 PolicyQuote, Policy, Profile, Staff, User]
+					 
+@models.each do |m|
+	m.__elasticsearch__.create_index!
+	m.__elasticsearch__.create_index! force: true
+	m.import force: true 
+end
