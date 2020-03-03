cb8a8520e633a41a78d8a76b9b5d2ab7affde15a
evanilukhin <evanilukhin@gmail.com>
2020-03-03 20:08:42 +0300

    Fix wrong reset password url

--- app/controllers/devise/users/passwords_controller.rb
+++ app/controllers/devise/users/passwords_controller.rb
@@ -1,3 +1,28 @@
 class Devise::Users::PasswordsController < DeviseTokenAuth::PasswordsController
-  # include PasswordMethods
+  def create
+    return render_create_error_missing_email unless resource_params[:email]
+
+    @email = get_case_insensitive_field_from_resource_params(:email)
+    @resource = find_resource(:uid, @email)
+    if @resource
+      yield @resource if block_given?
+      @resource.settings['last_reset_password_base_url'] = request.base_url
+      @resource.save
+      @resource.send_reset_password_instructions(
+        email: @email,
+        provider: 'email',
+        redirect_url: @redirect_url,
+        client_config: params[:config_name],
+        request_base_url: @request_base_url
+      )
+
+      if @resource.errors.empty?
+        return render_create_success
+      else
+        render_create_error @resource.errors
+      end
+    else
+      render_not_found_error
+    end
+  end
 end
