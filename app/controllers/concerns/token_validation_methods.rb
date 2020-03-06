03da0b3498add1c1fc3d7f4bd74b268edebc7ee2
evanilukhin <evanilukhin@gmail.com>
2020-03-06 13:37:23 +0300

    GCV2-1512 Add suffix to profile_attributes in validate_token response - fix validate_token for staff

--- app/controllers/concerns/token_validation_methods.rb
+++ app/controllers/concerns/token_validation_methods.rb
@@ -7,24 +7,13 @@ module TokenValidationMethods
     def validate_token
       # @resource will have been set by set_user_by_token concern
       if @resource
-        render template: show_json_path(@resource.class.name)
+        render json: @resource.as_json
       else
         render json: {
           success: false,
           errors: ["Invalid login credentials"]
         }, status: 401
-      end  
-    end
-  end
-
-  def show_json_path(resource_type)
-    case resource_type
-    when 'User'
-      'v2/user/users/show'
-    when 'Staff'
-      'v2/staff_super_admin/staffs/show'
-    else
-      ''
+      end
     end
   end
 end
