f530d5b4b8021f9b74bf96bc23f1680952f2ab59
DylanRothko <dylan@dylangaines.com>
2020-03-18 12:40:05 -0700

    fixing merge conflicts with fe4c490e5b1d62bace1ca9e41cac2773fbfcfa32

--- app/controllers/v2/public/policy_applications_controller.rb
+++ app/controllers/v2/public/policy_applications_controller.rb
@@ -53,21 +53,24 @@ module V2
       end
       
       def create_policy_users
+        error_status = []
         params[:policy_application][:policy_users_attributes].each_with_index do |policy_user, index|
 
           if ::User.where(email: policy_user[:user_attributes][:email]).exists?
             
             @user = ::User.find_by_email(policy_user[:user_attributes][:email])
             
-              if index == 0
-              if @user.invitation_accepted_at.nil?
+            if index == 0
+              if @user.invitation_accepted_at? == false
                 @application.users << @user
+                error_status << false
               else
     	          render json: {
       	          error: "User Account Exists",
       	          message: "A User has already signed up with this email address.  Please log in to complete your application"
     	          }.to_json,
-    	          status: 401              
+    	          status: 401 
+    	          error_status << true           
               end                
             else
               @application.users << @user
@@ -97,6 +100,7 @@ module V2
           end
           
         end
+        return error_status.include?(true) ? false : true
       end
       
       def create_commercial
@@ -109,51 +113,54 @@ module V2
 				                                                      title: 'Annually').take
 		    
 		    if @application.save
-  		    create_policy_users()
-  		    if @application.update(status: 'complete')
-            # Commercial Application Saved
-            
-    		    @application.primary_user().invite!
-            quote_attempt = @application.crum_quote()
-            
-  					if quote_attempt[:success] == true
-  						
-  						@application.primary_user().set_stripe_id()
-  						
-  						@quote = @application.policy_quotes.last
-  						@quote.generate_invoices_for_term()
-  						@premium = @quote.policy_premium
-  						
-  						response = { 
-  							quote: { 
-  								id: @quote.id, 
-  								status: @quote.status, 
-  								premium: @premium
-  							},
-  							invoices: @quote.invoices,
-  							user: { 
-  								id: @application.primary_user().id,
-  								stripe_id: @application.primary_user().stripe_id
-  							},
-  							billing_strategies: []
-  						}
-  						
-  						if @premium.base >= 500000
-  							BillingStrategy.where(agency: @application.agency_id, policy_type: @application.policy_type).each do |bs|
-  							  response[:billing_strategies]	<< { id: bs.id, title: bs.title }
-                end
-  						end
-  							  
-  						render json: response.to_json, status: 200
-  																
+		    
+  		    if create_policy_users()
+    		    if @application.update(status: 'complete')
+              # Commercial Application Saved
+              
+      		    @application.primary_user().invite!
+              quote_attempt = @application.crum_quote()
+              
+    					if quote_attempt[:success] == true
+    						
+    						@application.primary_user().set_stripe_id()
+    						
+    						@quote = @application.policy_quotes.last
+    						@quote.generate_invoices_for_term()
+    						@premium = @quote.policy_premium
+    						
+    						response = { 
+  	  						id: @application.id,
+    							quote: { 
+    								id: @quote.id, 
+    								status: @quote.status, 
+    								premium: @premium
+    							},
+    							invoices: @quote.invoices,
+    							user: { 
+    								id: @application.primary_user().id,
+    								stripe_id: @application.primary_user().stripe_id
+    							},
+    							billing_strategies: []
+    						}
+    						
+    						if @premium.base >= 500000
+    							BillingStrategy.where(agency: @application.agency_id, policy_type: @application.policy_type).each do |bs|
+    							  response[:billing_strategies]	<< { id: bs.id, title: bs.title }
+                  end
+    						end
+    							  
+    						render json: response.to_json, status: 200
+    																
+    					else
+    						render json: { error: "Quote Failed", message: quote_attempt[:message] },
+    									 status: 422							
+    					end
   					else
-  						render json: { error: "Quote Failed", message: quote_attempt[:message] },
-  									 status: 500							
+              render json: @application.errors.to_json,
+                     status: 422					
   					end
-					else
-            render json: @application.errors.to_json,
-                   status: 422					
-					end			  		  
+  		    end		  		  
   		  else         
           
           # Commercial Application Save Error
@@ -177,6 +184,7 @@ module V2
         end
 	      
 	      if @application.save
+<<<<<<< HEAD
   	      create_policy_users()
   	      if @application.update status: "complete"
 
@@ -219,6 +227,52 @@ module V2
 	          render json: @application.errors.to_json,
 	                 status: 422	
           end
+=======
+  	      if create_policy_users()
+    	      if @application.update status: "complete"
+  
+            	# if application.status updated to complete
+            	@application.qbe_estimate()
+            	@quote = @application.policy_quotes.last
+    					if @application.status != "quote_failed" || @application.status != "quoted"
+    						# if application quote success or failure
+    						@application.qbe_quote(@quote.id) 
+    						@application.reload()
+    						@quote.reload()
+    						
+    						if @quote.status == "quoted"	
+    							
+    							@application.primary_user().set_stripe_id()
+    						  
+    							render json: { 
+  	  							id: @application.id,
+    								quote: { 
+    									id: @quote.id, 
+    									status: @quote.status, 
+    									premium: @quote.policy_premium 
+    								},
+    								invoices: @quote.invoices.order('due_date ASC'),
+    								user: { 
+    									id: @application.primary_user().id,
+    									stripe_id: @application.primary_user().stripe_id
+    								}
+    							}.to_json, status: 200
+    						
+    						else
+    							render json: { error: "Quote Failed", message: "Quote could not be processed at this time" },
+    										 status: 500	
+    						end
+    					else
+    						render json: { error: "Application Unavailable", message: "Application cannot be quoted at this time" },
+    									 status: 400							
+    					end
+      	      
+            else
+  	          render json: @application.errors.to_json,
+  	                 status: 422	
+            end
+  	      end
+>>>>>>> fe4c490... Updates to prevent users who accepted their invitation from purchasing a policy
   	    else
           render json: @application.errors.to_json,
                  status: 422   
