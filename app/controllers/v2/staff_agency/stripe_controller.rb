3e65400e842749a75ed8b3948de9f334fa81694b
sbaha88 <sbaha88@gmail.com>
2020-03-31 23:06:13 +0600

    WIP: stripe connect

---
+++ app/controllers/v2/staff_agency/stripe_controller.rb
@@ -0,0 +1,47 @@
+##
+# V2 StaffAgency Users Controller
+# File: app/controllers/v2/staff_agency/users_controller.rb
+
+module V2
+  module StaffAgency
+    class StripeController < StaffAgencyController
+
+      def stripe_button_link
+        stripe_url = 'https://connect.stripe.com/express/oauth/authorize'
+        redirect_uri = v2_agency_stripe_connect_url
+        environment = ENV.fetch('RAILS_ENV', 'development')
+        client_id = Rails.application.credentials.stripe[environment.to_sym]&.[](:client_id)
+        
+        render json: { stripe_url: "#{stripe_url}?redirect_uri=#{redirect_uri}&client_id=#{client_id}" }, status: 200
+      end
+
+      def connect
+        # Send the authorization code to Stripe's API.
+        code = params[:code]
+        begin
+          response = Stripe::OAuth.token({
+            grant_type: 'authorization_code',
+            code: code,
+          })
+        rescue Stripe::OAuth::InvalidGrantError
+          render json: { error: 'Invalid authorization code: ' + code }, status: 400
+        rescue Stripe::StripeError
+          
+          render json: { error: 'An unknown error occurred.' }, status: 500
+        end
+
+        connected_account_id = response.stripe_user_id
+        save_account_id(connected_account_id)
+
+        # Render some HTML or redirect to a different page.
+        
+        render json: { success: true }, status: 200
+      end
+
+      def save_account_id(connected_account_id)
+        current_staff.organizable.update_attribute(:stripe_id, connected_account_id)
+      end
+
+    end
+  end
+end
