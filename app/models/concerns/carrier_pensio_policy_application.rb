7d01e47c0e0c1e01e5f76ae4c185f11694559b66
Dylan Gaines <dylan@dylangaines.com>
2020-03-31 17:17:16 -0700

    Setting up base carrier concern files

---
+++ app/models/concerns/carrier_pensio_policy_application.rb
@@ -0,0 +1,18 @@
+# =Pensio Policy Application Functions Concern
+# file: +app/models/concerns/carrier_pensio_policy_application.rb+
+
+module CarrierPensioPolicyApplication
+  extend ActiveSupport::Concern
+
+
+  included do
+	  
+	  # Generate Quote
+	  # 
+	  
+	  def pensio_quote(quote_id = nil)  
+		  
+		end
+		
+	end
+end
