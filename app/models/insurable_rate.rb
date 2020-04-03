a5ab7a08e8370b5aad50765714b017f6c6eac653
Joshua Brinsfield <sapphous@gmail.com>
2020-04-03 17:16:41 -0400

    Handle Insurable, InsurableRate, and InsurableType basic validations

--- app/models/insurable_rate.rb
+++ app/models/insurable_rate.rb
@@ -50,6 +50,9 @@ class InsurableRate < ApplicationRecord
 #     scope "#{scope_name}_insured".to_sym, -> { where(number_insured: number_insured) }
 #       
 #   end
+
+  validates :premium,
+    numericality: { greater_than_or_equal_to: 0 }
   
   enum interval: ["month", "quarter", "bi_annual", "annual"]
 
