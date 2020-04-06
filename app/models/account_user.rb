8300648f2a73516efbd8b644192a4aa4daea02cc
Joshua Brinsfield <sapphous@gmail.com>
2020-04-06 18:25:41 -0400

    Clean up AccountUser uniqueness validation

--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -3,26 +3,15 @@
 
 class AccountUser < ApplicationRecord
   
-  belongs_to :account,
-    required: true
-  belongs_to :user,
-    required: true
+  belongs_to :account
+  belongs_to :user
 
   # Validations
   
   validates_presence_of :status
   
-  validate :account_user_is_unique,
-    unless: Proc.new { |au| au.account.nil? || au.user.nil? }
+  validates_uniqueness_of :account_id, scope: "user_id"
 
   enum status: ['pending', 'enabled', 'disabled']
-  
-  private
-    
-    def account_user_is_unique
-      if AccountUser.where(user_id: user_id, account_id: account_id).count > 1
-        errors.add(:user, "already belongs to account")  
-      end
-    end
     
 end
