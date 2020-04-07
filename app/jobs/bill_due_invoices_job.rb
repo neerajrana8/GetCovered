dd06a30f54ed4419b554f7ee0a5c65814e8bcb61
Joshua Brinsfield <sapphous@gmail.com>
2020-04-07 18:21:05 -0400

    Fix invoices query in BillDueInvoicesJob for invoice polymorphism

--- app/jobs/bill_due_invoices_job.rb
+++ app/jobs/bill_due_invoices_job.rb
@@ -9,6 +9,8 @@ class BillDueInvoicesJob < ApplicationJob
   private
 
   def set_invoices
-    @invoices = Invoice.includes(:policy).where(policies: { billing_enabled: true, policy_in_system: true }, due_date: Time.current.to_date).available
+    @invoices = Invoice.joins("LEFT JOIN policies ON (policies.id = invoices.invoiceable_product_id AND invoices.invoiceable_product_id = 'Policy')")
+                       .where(policies: { billing_enabled: true, policy_in_system: true }, due_date: Time.current.to_date)
+                       .available
   end
 end
