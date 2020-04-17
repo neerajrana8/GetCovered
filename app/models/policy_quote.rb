605fbab89d87455b76e74864a97e4658861dd893
Joshua Brinsfield <sapphous@gmail.com>
2020-04-17 02:14:50 -0400

    Set up InvoiceableQuote concern

--- app/models/policy_quote.rb
+++ app/models/policy_quote.rb
@@ -9,6 +9,7 @@ class PolicyQuote < ApplicationRecord
 	include CarrierQbePolicyQuote
 	include CarrierCrumPolicyQuote
 	include ElasticsearchSearchable
+  include InvoiceableQuote
 
   before_save :set_status_updated_on,
   	if: Proc.new { |quote| quote.status_changed? }
@@ -237,88 +238,6 @@ class PolicyQuote < ApplicationRecord
 			end
 		end
 	end
-	
-	def generate_invoices_for_term(renewal = false, refresh = false)
-    invoices_generated = false
-    
-    unless renewal
-	    
-	    invoices.destroy_all if refresh
-	    
-	  	if policy_premium.calculation_base > 0 && 
-		  	 status == "quoted" && 
-		  	 invoices.count == 0
-         
-        # calculate sum of weights (should be 100, but just in case it's 33+33+33 or something)
-        payment_weight_total = policy_application.billing_strategy.new_business['payments'].inject(0){|sum,p| sum + p }.to_d
-        payment_weight_total = 100.to_d if payment_weight_total <= 0 # this can never happen unless someone fills new_business with 0s invalidly, but you can't be too careful
-        
-        # calculate invoice charges
-        to_charge = policy_application.billing_strategy.new_business['payments'].map.with_index do |payment, index|
-          {
-            due_date: index == 0 ? status_updated_on : policy_application.effective_date + index.months,
-            term_first_date: policy_application.effective_date + index.months,
-            fees: (policy_premium.amortized_fees * payment / payment_weight_total).floor + (index == 0 ? policy_premium.deposit_fees : 0),
-            total: (policy_premium.calculation_base * payment / payment_weight_total).floor + (index == 0 ? policy_premium.deposit_fees : 0)
-          }
-        end.select{|tc| tc[:total] > 0 }
-        # set term_last_dates
-        (0...(to_charge.length - 1)).each do |charge_index|
-          to_charge[charge_index][:term_last_date] = to_charge[charge_index + 1][:term_first_date] - 1.day
-        end
-        to_charge.last[:term_last_date] = policy_application.effective_date + 12.months - 1.day
-        # add any rounding errors to the first charge
-        to_charge[0][:fees] += policy_premium.total_fees - to_charge.inject(0){|sum,tc| sum + tc[:fees] }
-        to_charge[0][:total] += policy_premium.total - to_charge.inject(0){|sum,tc| sum + tc[:total] }
-        
-        # create invoices
-        begin
-          ActiveRecord::Base.transaction do
-            to_charge.each.with_index do |tc, tci|
-              invoices.create!({
-                due_date:       tc[:due_date],
-                available_date: tc[:due_date] - available_period,
-                term_first_date: tc[:term_first_date],
-                term_last_date: tc[:term_last_date],
-                user:           policy_application.primary_user,
-                status:         "quoted",
-                
-                total:          tc[:total], # subtotal & total are calculated automatically from line items, but if we pass one manually validations will fail if it doesn't match the calculation
-                line_items_attributes: [
-                  tci == 0 ? {
-                    title: "Deposit Fees",
-                    price: policy_premium.deposit_fees,
-                    refundability: 'no_refund'
-                  } : nil,
-                  {
-                    title: "Amortized Fees",
-                    price: tc[:fees] - (tci == 0 ? policy_premium.deposit_fees : 0),
-                    refundability: 'no_refund'
-                  },
-                  {
-                    title: "Premium Payment",
-                    price: tc[:total] - tc[:fees],
-                    refundability: 'prorated_refund'
-                  }
-                ].select{|lia| !lia.nil? && lia[:amount] > 0 }
-              })
-            end
-            invoices_generated = true
-          end
-        rescue ActiveRecord::RecordInvalid => e
-          puts e.to_s
-        rescue
-          puts "Unknown error during invoice creation"
-        end
-				
-		  end
-	  else
-	  	# Set up Renewal Invoice Generation
-	  end
-    
-    return invoices_generated
-    
-  end
 
   def start_billing
 
