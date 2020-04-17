e20d5482af254147c1b88717bd33f33bdcbbe9ce
Joshua Brinsfield <sapphous@gmail.com>
2020-04-17 02:16:24 -0400

    Run line item migration

--- db/migrate/20200415195800_add_fields_to_line_item.rb
+++ db/migrate/20200415195800_add_fields_to_line_item.rb
@@ -1,6 +1,5 @@
 class AddFieldsToLineItem < ActiveRecord::Migration[5.2]
   def change
     add_column :line_items, :refundability, :integer, null: false
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_180131) do
+ActiveRecord::Schema.define(version: 2020_04_15_195800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -604,6 +604,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_180131) do
     t.bigint "invoice_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "refundability", null: false
     t.index ["invoice_id"], name: "index_line_items_on_invoice_id"
   end
 
