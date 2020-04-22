623d03fc5ce2acb2200f722328dc53f29cfc8c7c
Joshua Brinsfield <sapphous@gmail.com>
2020-04-22 16:13:13 -0400

    Fix charge/invoice tracking migration

--- db/migrate/20200418011236_upgrade_invoice_system.rb
+++ db/migrate/20200418011236_upgrade_invoice_system.rb
@@ -2,9 +2,9 @@ class UpgradeInvoiceSystem < ActiveRecord::Migration[5.2]
   def up
     # add proration_reduction & active_dispute_count to Invoice
     add_column :invoices, :proration_reduction, :integer, null: false, default: 0
-    add_column :invoices, :active_dispute_count, :integer, null: false, default: 0
+    add_column :invoices, :disputed_charge_count, :integer, null: false, default: 0
     
-    # restore defaults & non-null constraints to policy billing dispute fields
+    # fix up policy billing dispute fields
     change_column_default :policies, :billing_dispute_count, 0
     change_column_default :policies, :billing_dispute_status, 0
     change_column_null :policies, :billing_dispute_count, false
@@ -51,7 +51,7 @@ class UpgradeInvoiceSystem < ActiveRecord::Migration[5.2]
     remove_column :line_items, :priced_in
     remove_column :line_items, :category
     remove_column :line_items, :refundability
-    remove_column :invoices, :active_dispute_count
+    remove_column :invoices, :disputed_charge_count
     remove_column :invoices, :proration_reduction
   end
   
