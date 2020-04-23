3eb0fb7e9983c585a91367511e4f85effb865546
Joshua Brinsfield <sapphous@gmail.com>
2020-04-22 20:05:33 -0400

    Add paranoia gate to Invoice#mark_line_items_priced_in

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -339,7 +339,14 @@ class Invoice < ApplicationRecord
   end
   
   def mark_line_items_priced_in
-    self.line_items.update_all(priced_in: true)
+    # it's redundant to calculate these twice, but I'm paranoid about validations being missed or called early; better safe than sorry when dealing with money!
+    self.calculate_subtotal
+    self.calculate_total
+    if self.errors.blank?
+      self.line_items.update_all(priced_in: true)
+    else
+      throw :abort
+    end
   end
 
   # History Methods
