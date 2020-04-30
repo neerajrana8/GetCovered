1a7a3718f3c0cd8d889079ac68f47047c5ca8fc9
PavelProtchenko <pprotchenko@rzdit.ru>
2020-04-30 16:07:15 +0300

    fix change documents into document

--- app/models/concerns/carrier_pensio_policy.rb
+++ app/models/concerns/carrier_pensio_policy.rb
@@ -39,11 +39,11 @@ module CarrierPensioPolicy
         file << summary_pdf
       end
       
-      if documents.attach(io: File.open(agreement_save_path), filename: "#{ number }-agreement.pdf", content_type: 'application/pdf')
+      if document.attach(io: File.open(agreement_save_path), filename: "#{ number }-agreement.pdf", content_type: 'application/pdf')
 				File.delete(agreement_save_path) if File.exist?(agreement_save_path) 	
 			end
       
-      if documents.attach(io: File.open(summary_save_path), filename: "#{ number }-summary.pdf", content_type: 'application/pdf')
+      if document.attach(io: File.open(summary_save_path), filename: "#{ number }-summary.pdf", content_type: 'application/pdf')
 				File.delete(summary_save_path) if File.exist?(summary_save_path) 	
 			end
       
