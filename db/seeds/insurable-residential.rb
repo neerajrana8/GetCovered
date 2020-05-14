7dce6db6d6cf1b601e0a460fa3eb6098d57919b8
Joshua Brinsfield <sapphous@gmail.com>
2020-05-14 19:25:11 -0400

    Fix up insurable residential seeds

--- db/seeds/insurable-residential.rb
+++ db/seeds/insurable-residential.rb
@@ -123,39 +123,69 @@ require 'faker'
     zip_code: "06902",
     plus_four: "2452",
     primary: true
-  } 	
+  },
+  {
+    street_number: "1304",
+    street_name: "University City Blvd",
+    city: "Blacksburg",
+    county: "MONTGOMERY",
+    state: "VA",
+    zip_code: "24060",
+    plus_four: "2904",
+    primary: true
+  },
+  {
+    street_number: "2501",
+    street_name: "Veterans Memorial Pkwy",
+    city: "Tuscaloosa",
+    county: "TUSCALOOSA",
+    state: "AL",
+    zip_code: "35404",
+    plus_four: "4147",
+    primary: true
+  },
+  {
+    street_number: "1725",
+    street_name: "Harvey Mitchell Pkwy S",
+    city: "College Station",
+    county: "BRAZOS",
+    state: "TX",
+    zip_code: "77840",
+    plus_four: "6312",
+    primary: true
+  }
 ]
 
 @building_name_options = ['Estates', 'Gardens', 'Homes', 'Place']
 @residential_community_insurable_type = InsurableType.find(1)
 @residential_unit_insurable_type = InsurableType.find(4)
 
-@count = 2
-@accounts = Account.all
 
-qbe_created = 0
-msi_created = 0
-@accounts.each do |account|
-	@count.times do |i|
-    # QBE communities
-    
-		addr = @addresses[qbe_created % @addresses.length]#Insurable.residential_communities.count % @addresses.length]
-    
-		args = { 
-			carrier_id: 1,
-			policy_type_id: 1,
-			state: addr[:state],
-			zip_code: addr[:zip_code],
-			plus_four: addr[:plus_four]
-		}
-
-		if account.agency.offers_policy_type_in_region(args)
+@accounts = Account.all.to_a.map{|acct| { account: acct, assignments: 0 } }
+while @addresses.length > 0
+  # set up address and args
+  addr = @addresses.pop
+  args = {
+    policy_type_id: 1,
+    state: addr[:state],
+    zip_code: addr[:zip_code],
+    plus_four: addr[:plus_four]
+  }
+  # assign to the account with the least assignments so far that will accept it
+  @accounts.sort{|a,b| a[:assignments] <=> b[:assignments] }.each do |account_data|
+    # extract account and setup flag to tell us when we successfuly assigned it
+    account = account_data[:account]
+    assigned = false
+    # now try to assign it to the account under various carriers (yes, we duplicate it for each carrier that will accept it)
+    # try qbe
+    args[:carrier_id] = 1
+    if account.agency.offers_policy_type_in_region(args)
 			@community = account.insurables.new(title: "#{Faker::Movies::LordOfTheRings.location} #{@building_name_options[rand(0..3)]}", 
 																					insurable_type: @residential_community_insurable_type, 
 																					enabled: true, category: 'property',
 																					addresses_attributes: [ addr ])			
 			if @community.save
-        qbe_created += 1
+        assigned = true
 		  	
 		  	account.staff
 		  					.order("RANDOM()")
@@ -204,21 +234,10 @@ msi_created = 0
 		  	@community.reset_qbe_rates(true, true)
 			else	
 				pp @community.errors
-			end	
-		end		
-    
-    # MSI communities
-    
-		addr = @addresses[msi_created % @addresses.length]#Insurable.residential_communities.count % @addresses.length]
-    
-		args = { 
-			carrier_id: 5,
-			policy_type_id: 1,
-			state: addr[:state],
-			zip_code: addr[:zip_code],
-			plus_four: addr[:plus_four]
-		}
-
+			end
+    end
+    # try msi
+    args[:carrier_id] = 5
 		if account.agency.offers_policy_type_in_region(args)
 			@community = account.insurables.new(title: "#{Faker::Movies::LordOfTheRings.location} #{@building_name_options[rand(0..3)]}", 
 																					insurable_type: @residential_community_insurable_type, 
@@ -227,7 +246,7 @@ msi_created = 0
 			unless @community.save
 				pp @community.errors
       else
-        msi_created += 1
+        assigned = true
 		  	# create assignments (with pointless random ordering)
 		  	account.staff.order("RANDOM()").each do |staff|
 			  	Assignment.create!(staff: staff, assignable: @community)
@@ -271,6 +290,11 @@ msi_created = 0
         end
 			end	
 		end		
-		
-	end
+    # break out of the loop if we managed to assign it
+    if assigned
+      account_data[:assignments] += 1
+      break
+    end
+  end
 end
+
