81e95d31852cc7a79f0db007e6b224ab8cc50794
sbaha88 <sbaha88@gmail.com>
2020-05-18 17:27:16 +0600

    Another error with variables

--- app/jobs/policies/create_from_quote_job.rb
+++ app/jobs/policies/create_from_quote_job.rb
@@ -36,7 +36,7 @@ module Policies
          policy_quote.policy_premium.update(policy: policy)
 
         policy.send(issue_policy_method(integration_designation))
-        send_acceptance_email_to_primary_user(policy.primary_user)
+        send_acceptance_email_to_primary_user(policy)
         # invite_users(policy)
         # UserCoverageMailer.with(policy: policy, user: policy.primary_user).proof_of_coverage.deliver
       else
@@ -46,7 +46,8 @@ module Policies
 
     private
 
-    def send_acceptance_email_to_primary_user(user)
+    def send_acceptance_email_to_primary_user(policy)
+      user = policy.primary_user
       user.skip_invitation = true
       user.invite!
       UserCoverageMailer.with(policy: policy, user: user).acceptance_email.deliver
