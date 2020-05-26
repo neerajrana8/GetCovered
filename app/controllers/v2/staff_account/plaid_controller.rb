17a848fa4315b09564e34498b292fb28827e7b22
evanilukhin <evanilukhin@gmail.com>
2020-05-26 09:49:48 +0300

    GCV2-2016 API Plaid connect returns 500 error for accounts

--- app/controllers/v2/staff_account/plaid_controller.rb
+++ app/controllers/v2/staff_account/plaid_controller.rb
@@ -7,24 +7,24 @@ module V2
     class PlaidController < StaffAccountController
       
       def connect
-        environment =  case ENV['RAILS_ENV']
-          when 'development'
-            :sandbox
-          when 'awsdev'
-            :sandbox
-          when 'test'
-            :sandbox
-          when 'aws_staging'
-            :development
-          when 'production'
-            :production
-          else
-            :sandbox
+        environment = case ENV['RAILS_ENV']
+                      when 'development'
+                        :sandbox
+                      when 'awsdev'
+                        :sandbox
+                      when 'test'
+                        :sandbox
+                      when 'aws_staging'
+                        :development
+                      when 'production'
+                        :production
+                      else
+                        :sandbox
           end
         client = ::Plaid::Client.new(env: environment,
-          client_id: Rails.application.credentials.plaid[:client_id],
-          secret: Rails.application.credentials.plaid[ENV['RAILS_ENV'].to_sym]&.[](:secret_key),
-          public_key: Rails.application.credentials.plaid[:public_key])
+                                     client_id: Rails.application.credentials.plaid[:client_id],
+                                     secret: Rails.application.credentials.plaid[ENV['RAILS_ENV'].to_sym]&.[](:secret_key),
+                                     public_key: Rails.application.credentials.plaid[:public_key])
           
         exchange_token_response = client.item.public_token.exchange(params[:plaid_public_token])
         access_token = exchange_token_response['access_token']
@@ -34,11 +34,13 @@ module V2
           source_id: stripe_response['stripe_bank_account_token'], 
           source_type: 'bank_account'
         )
-         if payment_profile.save
+        if payment_profile.save
           render json: { success: true }, status: 200
-         else
+        else
           render json: { errors: payment_profile.errors }, status: :unprocessable_entity
-         end
+        end
+      rescue Plaid::InvalidInputError => e
+        render json: { error: e.error_message }, status: :unprocessable_entity
       end
     end
   end
