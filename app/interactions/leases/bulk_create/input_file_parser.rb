ff97406a73967dce8ab14dbb45e52f8ec2feafd5
evanilukhin <evanilukhin@gmail.com>
2020-05-31 13:06:29 +0300

    GCV2-2054 API for the lease Importer - add refiner

--- app/interactions/leases/bulk_create/input_file_parser.rb
+++ app/interactions/leases/bulk_create/input_file_parser.rb
@@ -2,7 +2,7 @@ module Leases
   module BulkCreate
     class InputFileParser < ActiveInteraction::Base
       file :input_file
-      
+      integer :insurable_id, default: nil
 
       HEADERS = %w[start_date end_date status lease_type unit
                    tenant_one_email tenant_one_first_name tenant_one_last_name tenant_one_birthday
@@ -16,7 +16,10 @@ module Leases
         parsed_file.each_with_index do |row, index|
           next unless row_valid?(row, index + 2)
 
-          result << params(row)
+          refined_row = refine_row(row, row_number)
+          next unless refined_row
+
+          result << params(refined_row)
         end
         result
       end
@@ -33,20 +36,12 @@ module Leases
           return false
         end
 
-        if row['tenant_one_email'].present? && (row['tenant_one_email'].blank? || row['tenant_one_email'] > 18.years.ago)
-          errors[:bad_rows] << {
-            message: "Row #{row_number} has the incorrect of birth",
-            row: row_number
-          }
-          return
+        if row['tenant_one_email'].present? && !tenant_data_valid?(row, row_number, 'one')
+          return false
         end
 
-        if row['tenant_two_email'].present? && (row['tenant_two_email'].blank? || row['tenant_two_email'] > 18.years.ago)
-          errors[:bad_rows] << {
-            message: "Row #{row_number} has the incorrect of birth",
-            row: row_number
-          }
-          return
+        if row['tenant_two_email'].present? && !tenant_data_valid?(row, row_number, 'two')
+          return false
         end
 
         unless VALID_LEASE_TYPES.include?(row['lease_type'])
@@ -54,7 +49,7 @@ module Leases
             message: "Row #{row_number} has the incorrect lease_type",
             row: row_number
           }
-          return
+          return false
         end
 
         unless Lease.statuses.keys.include?(row['status'])
@@ -62,26 +57,67 @@ module Leases
             message: "Row #{row_number} has the incorrect status",
             row: row_number
           }
-          return
+          return false
         end
 
-        begin
-          if Date.strptime(row['start_date'], DATE_FORMAT) > Date.strptime(row['end_date'], DATE_FORMAT)
-            errors[:bad_rows] << {
-              message: "In the Row #{row_number} end_date is earlier than start_date",
-              row: row_number
-            }
-            return
-          end
-        rescue ArgumentError => _e
+        if parse_date(row['start_date']) > parse_date(row['end_date'])
           errors[:bad_rows] << {
-            message: "Row #{row_number} has invalid end start_date or end_date",
+            message: "In the Row #{row_number} end_date is earlier than start_date",
+            row: row_number
+          }
+          return false
+        end
+
+        true
+      rescue ArgumentError => _e
+        errors[:bad_rows] << {
+          message: "Row #{row_number} contains invalid date",
+          row: row_number
+        }
+
+        return false
+      end
+
+      def tenant_data_valid?(row, row_number, tenant)
+        if row["tenant_#{tenant}_birthday"].blank? || parse_date(row["tenant_#{tenant}__birthday"]) > 18.years.ago
+          errors[:bad_rows] << {
+            message: "Tenant #{tenant} in the row #{row_number} should be older than 18",
             row: row_number
           }
           return
         end
 
+        empty_tenant_rows = %W[tenant_#{tenant}_first_name tenant_#{tenant}_last_name].select do |column|
+          row[column].blank?
+        end
+
+        if empty_tenant_rows.any?
+          errors[:bad_rows] << {
+            message: "Next columns in the row #{row_number} should be present: #{empty_tenant_rows.join(', ')}",
+            row: row_number
+          }
+          return false
+        end
+
         true
+      end
+
+      def parse_date(date_string)
+        Date.strptime(date_string, DATE_FORMAT)
+      end
+
+      def refine_row(row, row_number)
+        unit_id = Insurable.where(insurable_id: insurable_id, title: row['unit']).take&.id
+        if unit_id.present?
+          row['insurable_id'] = unit_id
+          row
+        else
+          errors[:bad_rows] << {
+            message: "Unit #{row['unit']} in the row #{row_number} doesn't exist in the system",
+            row: row_number
+          }
+          false
+        end
       end
 
       def params(refined_row)
@@ -91,7 +127,7 @@ module Leases
             end_date: refined_row['end_date'],
             status: refined_row['status'],
             lease_type: refined_row['lease_type'],
-            insurable: refined_row['insurable_id']
+            insurable_id: refined_row['insurable_id']
           },
           lease_users: lease_users_params(refined_row)
         }
