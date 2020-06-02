d19dc6475a0385b5a29e10199e90e8bf053d743a
DylanRothko <dylan@dylangaines.com>
2020-06-02 12:33:13 -0700

    Fixing minimum amounts for rent guarantee policies

--- app/models/concerns/carrier_pensio_policy_application.rb
+++ app/models/concerns/carrier_pensio_policy_application.rb
@@ -40,9 +40,18 @@ module CarrierPensioPolicyApplication
 						else
 							0.035
 						end
+						
+				  minimum_premium = 
+				    if guarantee_option == 12
+  				    108000
+  				  elsif guarantee_option == 6
+  				    90000
+  				  else
+  				    42000
+  				  end
 					
 					unchecked_premium = ((( self.fields["monthly_rent"] * 100 ) * 12 ) * multiplier ).to_i
-					checked_premium = unchecked_premium < 42000 ? 42000 : unchecked_premium
+					checked_premium = unchecked_premium < minimum_premium ? minimum_premium : unchecked_premium
 				  
 				  premium = PolicyPremium.new base: checked_premium, policy_quote: quote, 
 				                              billing_strategy: quote.policy_application.billing_strategy
