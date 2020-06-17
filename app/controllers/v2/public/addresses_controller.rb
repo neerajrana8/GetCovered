0decd8337b968a7c31e87709113481715b9250a1
evanilukhin <evanilukhin@gmail.com>
2020-06-17 20:28:02 +0300

    Add an ability to delete addresses from insurables

--- app/controllers/v2/public/addresses_controller.rb
+++ app/controllers/v2/public/addresses_controller.rb
@@ -5,44 +5,42 @@
 module V2
   module Public
     class AddressesController < PublicController
-	  	
-	  	def index
-		  	if params[:search].presence
-			  	@addresses = Address.search_insurables(params[:search])
-			  	@ids = @addresses.map { |a| a["_source"]["addressable_id"] }
-			  	
-			  	@insurables = Insurable.find(@ids)
-					
-					@response = []
-					
-					unless @insurables.nil?
-						@insurables.each do |i|
-							@response.push({
-								id: i.id,
-								title: i.title,
-								enabled: i.enabled,
-								account_id: i.account_id,
-								agency_id: i.account.agency_id,
-								insurable_type_id: i.insurable_type_id,
-								category: i.category,
-								covered: i.covered,
-								created_at: i.created_at,
-								updated_at: i.updated_at,
-								addresses: i.addresses,
-								insurables: i.units
-							}) if [1,2,3].include?(i.insurable_type_id)						
-						end
-					end
-					
-					render json: @response.to_json,
-               	 status: :ok 
-			  else
-			  	render json: [].to_json,
-			  				 status: :ok
-			  end	
-			  
-		  end
-	  	 
-	  end
-	end
+
+      def index
+        if params[:search].presence
+          @addresses = Address.search_insurables(params[:search])
+          @ids = @addresses.map { |a| a['_source']['addressable_id'] }
+
+          @insurables = Insurable.find(@ids)
+
+          @response = []
+
+          @insurables&.each do |i|
+            if (InsurableType::COMMUNITIES_IDS | InsurableType::BUILDINGS_IDS).include?(i.insurable_type_id)
+              @response.push(
+                id: i.id,
+                title: i.title,
+                enabled: i.enabled,
+                account_id: i.account_id,
+                agency_id: i.account.agency_id,
+                insurable_type_id: i.insurable_type_id,
+                category: i.category,
+                covered: i.covered,
+                created_at: i.created_at,
+                updated_at: i.updated_at,
+                addresses: i.addresses,
+                insurables: i.units
+              )
+            end
+          end
+
+          render json: @response.to_json,
+                 status: :ok
+        else
+          render json: [].to_json,
+                 status: :ok
+        end
+      end
+    end
+  end
 end
