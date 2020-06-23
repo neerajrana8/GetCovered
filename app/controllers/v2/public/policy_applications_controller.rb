83f0f2bce7f2b806ed66acae0ec8bb93a74f1608
Joshua Brinsfield <sapphous@gmail.com>
2020-06-23 15:27:30 -0400

    Fix get-coverage-options http status codes to 200 only

--- app/controllers/v2/public/policy_applications_controller.rb
+++ app/controllers/v2/public/policy_applications_controller.rb
@@ -524,7 +524,7 @@ module V2
         )
         # done
         render json: results.select{|k,v| k != :errors }.merge(results[:errors] ? { estimated_premium_errors: [results[:errors][:external]].flatten } : {}),
-          status: results[:valid] ? 200 : 202
+          status: 200
       end
 
 
