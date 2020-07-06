9cf3c05e42b07d7f6c70a26200660b616c55e84e
evanilukhin <evanilukhin@gmail.com>
2020-07-06 11:03:33 +0300

    GCV2-2337 Add adding an existing community to a master policy - add test

--- spec/requests/master_policies_api_spec.rb
+++ spec/requests/master_policies_api_spec.rb
@@ -7,6 +7,8 @@ describe 'Master Policies API spec', type: :request do
   let(:agent)   { FactoryBot.create(:staff, role: 'agent', organizable: agency) }
   let(:community1) { FactoryBot.create(:insurable, :residential_community, account: account) }
   let(:community2) { FactoryBot.create(:insurable, :residential_community, account: account) }
+  let(:community_new) { FactoryBot.create(:insurable, :residential_community, account: account) }
+
   let(:master_policy) do
     FactoryBot.create(
       :policy,
@@ -25,13 +27,29 @@ describe 'Master Policies API spec', type: :request do
 
     it 'should return list of included communities' do
       get "/v2/staff_agency/master_policies/#{master_policy.id}/communities",
-           headers: @headers.reverse_merge(base_headers)
+          headers: @headers.reverse_merge(base_headers)
 
       expect(response.status).to eq(200)
       response_json = JSON.parse(response.body)
       response_community_titles = response_json.map { |community| community['title'] }
       expect(response_json.count).to eq(2)
       expect(response_community_titles).to match_array(%w[2 1])
+    end
+
+    it 'should add a new community' do
+      request = lambda do
+        post "/v2/staff_agency/master_policies/#{master_policy.id}/add_insurable",
+             params: {
+               insurable_id: community_new.id,
+               account_id: account.id,
+               carrier_id: agency.carriers.take.id
+             }.to_json,
+             headers: @headers.reverse_merge(base_headers)
+      end
+
+      expect { request.call }.to change { master_policy.insurables.communities.count }.by(1)
+      expect(master_policy.insurables.communities.count).to eq(3)
+      expect(response.status).to eq(200)
     end
   end
 end
