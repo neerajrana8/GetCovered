6b65b123cfd3f53ca489f0bf60b6d0c4ad12d9f3
Joshua Brinsfield <sapphous@gmail.com>
2020-07-07 18:53:55 -0400

    Fix PolicyGroup#invoices and put Policy-like scopes on PolicyGroup

--- app/models/policy_group.rb
+++ app/models/policy_group.rb
@@ -1,6 +1,7 @@
 class PolicyGroup < ApplicationRecord
   has_many :policies
-  has_many :invoices, as: :invoiceable
+  has_many :policy_group_quotes
+  has_many :invoices, through: :policy_group_quotes
   has_one :policy_application
   has_one :policy_group_premium
 
@@ -9,4 +10,8 @@ class PolicyGroup < ApplicationRecord
   belongs_to :account, optional: true
   belongs_to :carrier
   belongs_to :policy_type
+  
+  scope :current, -> { where(status: %i[BOUND BOUND_WITH_WARNING]) }
+  scope :policy_in_system, ->(policy_in_system) { where(policy_in_system: policy_in_system) }
+  scope :unpaid, -> { where(billing_dispute_status: ['BEHIND', 'REJECTED']) }
 end
