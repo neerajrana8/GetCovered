b2579f62e63283da922e0e2cc7f40c483d47482f
Joshua Brinsfield <sapphous@gmail.com>
2020-07-07 17:37:52 -0400

    Fix PolicyGroup#invoice association

--- app/models/policy_group.rb
+++ app/models/policy_group.rb
@@ -1,6 +1,7 @@
 class PolicyGroup < ApplicationRecord
   has_many :policies
-  has_many :invoices, as: :invoiceable
+  has_many :policy_group_quotes
+  has_many :invoices, through: :policy_group_quotes
   has_one :policy_application
   has_one :policy_group_premium
 
