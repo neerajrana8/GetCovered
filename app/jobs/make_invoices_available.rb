55936391a1f788c83d9fe09053015eaac1179a5e
Joshua Brinsfield <sapphous@gmail.com>
2020-07-07 23:07:30 -0400

    Add MakeInvoicesAvailable job

---
+++ app/jobs/make_invoices_available.rb
@@ -0,0 +1,14 @@
+class MakeInvoicesAvailableJob < ApplicationJob
+  queue_as :default
+  before_perform :set_invoices
+
+  def perform(*args)
+    @invoices.each{|invoice| invoice.update(status: 'available') }
+  end
+
+  private
+
+    def set_invoices
+      @invoices = ::Invoice.where(status: 'upcoming').where("available_date <= '#{Time.current.to_date.to_s(:db)}'")
+    end
+end
