bbb785668af6d078ccd6baebbef750891dba9cd3
evanilukhin <evanilukhin@gmail.com>
2020-07-09 10:53:03 +0300

    GCV2-2355 Error message says that the "user already exists" for a new user when he backs to the f... - add condition for the update user's params

--- app/controllers/v2/public/policy_applications_controller.rb
+++ app/controllers/v2/public/policy_applications_controller.rb
@@ -72,9 +72,11 @@ module V2
           if ::User.where(email: policy_user[:user_attributes][:email]).exists?
           
             @user = ::User.find_by_email(policy_user[:user_attributes][:email])
-                
-            @user.update(policy_user[:user_attributes])
-            @user.profile.update(policy_user[:user_attributes][:profile_attributes])
+
+            if @user.invitation_accepted_at? == false
+              @user.update(policy_user[:user_attributes])
+              @user.profile.update(policy_user[:user_attributes][:profile_attributes])
+            end
 
             if index == 0
               if @user.invitation_accepted_at? == false
@@ -108,7 +110,6 @@ module V2
 
                 tmp_full = Address.new(policy_user[:user_attributes][:address_attributes]).set_full_searchable
 
-                ap "DEBUG full_searchable. Address in: |#{tmp_full}|. Address user: |#{@user.address.full_searchable}|"
                 if @user.address.full_searchable != tmp_full
                   render(json: {
                     error: 'Address mismatch',
