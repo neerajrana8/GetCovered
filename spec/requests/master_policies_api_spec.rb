7d5cb2fe0c8f1e3a6c52ccfa1d0db07a14053ef4
evanilukhin <evanilukhin@gmail.com>
2020-07-28 14:48:42 +0300

    GCV2-2352 Improve error representing from the backend for residental and rent guarantee form - add test for creating master policies

--- spec/requests/master_policies_api_spec.rb
+++ spec/requests/master_policies_api_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 include ActionController::RespondWith
 
 describe 'Master Policies API spec', type: :request do
-  let(:agency)  { FactoryBot.create(:agency) }
-  let(:account) { FactoryBot.create(:account, agency: agency) }
-  let(:agent)   { FactoryBot.create(:staff, role: 'agent', organizable: agency) }
-  let(:staff)   { FactoryBot.create(:staff, role: 'staff', organizable: account) }
-  let(:community1) { FactoryBot.create(:insurable, :residential_community, account: account) }
-  let(:community2) { FactoryBot.create(:insurable, :residential_community, account: account) }
-  let(:community_new) { FactoryBot.create(:insurable, :residential_community, account: account) }
-  let(:unit) { FactoryBot.create(:insurable, :residential_unit, account: account, insurable: community1) }
+  let!(:agency)  { FactoryBot.create(:agency) }
+  let!(:account) { FactoryBot.create(:account, agency: agency) }
+  let!(:agent)   { FactoryBot.create(:staff, role: 'agent', organizable: agency) }
+  let!(:staff)   { FactoryBot.create(:staff, role: 'staff', organizable: account) }
+  let!(:community1) { FactoryBot.create(:insurable, :residential_community, account: account) }
+  let!(:community2) { FactoryBot.create(:insurable, :residential_community, account: account) }
+  let!(:community_new) { FactoryBot.create(:insurable, :residential_community, account: account) }
+  let!(:unit) { FactoryBot.create(:insurable, :residential_unit, account: account, insurable: community1) }
 
   let(:master_policy) do
     FactoryBot.create(
@@ -29,6 +29,17 @@ describe 'Master Policies API spec', type: :request do
     before :each do
       login_staff(agent)
       @headers = get_auth_headers_from_login_response_headers(response)
+    end
+
+    it 'creates a master policy' do
+      request = lambda do
+        post '/v2/staff_agency/master-policies',
+             params: master_policy_params.to_json,
+             headers: @headers.reverse_merge(base_headers)
+      end
+
+      expect { request.call }.to change { Policy.where(policy_type_id: PolicyType::MASTER_ID).count }.by(1)
+      expect(response.status).to eq(201)
     end
 
     it 'returns list of included communities' do
@@ -86,5 +97,31 @@ describe 'Master Policies API spec', type: :request do
       expect { request.call }.to change { unit.policies.current.count }.by(1)
       expect(response.status).to eq(200)
     end
+  end
+
+  private
+
+  def master_policy_params
+    {
+      carrier_id: agency.carriers.take.id,
+      account_id: account.id,
+      base: 100,
+      total: 100,
+      calculation_base: 100,
+      carrier_base: 0,
+      policy: {
+        effective_date: Time.zone.now + 1.day,
+        expiration_date: Time.zone.now + 1.month,
+        number: 'MPN',
+        policy_coverages_attributes: [
+          {
+            limit: 100,
+            designation: 'coverage_c',
+            deductible: 10,
+            enabled: true
+          }
+        ]
+      }
+    }
   end
 end
