33dd05de088023537cc608a5a9413184dcaeab38
Joshua Brinsfield <sapphous@gmail.com>
2020-08-01 00:51:13 -0400

    Remove IRC mutability migration

--- db/migrate/20200728211240_add_coverage_options_mutability_to_insurable_rate_configurations.rb
+++
@@ -1,6 +0,0 @@
-class AddCoverageOptionsMutabilityToInsurableRateConfigurations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :insurable_rate_configurations, :coverage_options_mutability, :jsonb, default: [], null: false
-    change_column_default :insurable_rate_configurations, :coverage_options, from: [], to: {}
-  end
-end
