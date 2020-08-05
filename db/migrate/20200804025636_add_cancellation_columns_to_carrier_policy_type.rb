6db6b2812f10f8c68b2ccf74617c413af503adb6
Joshua Brinsfield <sapphous@gmail.com>
2020-08-05 02:51:04 -0400

    Generalize pensio situation

--- db/migrate/20200804025636_add_cancellation_columns_to_carrier_policy_type.rb
+++ db/migrate/20200804025636_add_cancellation_columns_to_carrier_policy_type.rb
@@ -4,11 +4,12 @@ class AddCancellationColumnsToCarrierPolicyType < ActiveRecord::Migration[5.2]
     add_column :carrier_policy_types, :max_days_for_full_refund, :integer, null: true, default: 30
     
     # set pensio premium_refundable to false & update all pensio premium line items accordingly
-    ::CarrierPolicyType.where(carrier_id: 4).update_all(premium_refundable: false)
+    nonrefundables = [4] # pensio
+    ::CarrierPolicyType.where(carrier_id: nonrefundables).update_all(premium_refundable: false)
     LineItem.where(
-      invoice_id: Invoice.select(:id).where(invoiceable_type: 'PolicyQuote', invoiceable_id: PolicyQuote.select(:id).references(:policy_applications).includes(:policy_application).where(policy_applications: { carrier_id: 4 }))
+      invoice_id: Invoice.select(:id).where(invoiceable_type: 'PolicyQuote', invoiceable_id: PolicyQuote.select(:id).references(:policy_applications).includes(:policy_application).where(policy_applications: { carrier_id: nonrefundables }))
                   .or(
-                    Invoice.select(:id).where(invoiceable_type: 'PolicyGroupQuote', invoiceable_id: PolicyGroupQuote.select(:id).references(:policy_application_groups).includes(:policy_application_group).where(policy_application_groups: { carrier_id: 4 }))
+                    Invoice.select(:id).where(invoiceable_type: 'PolicyGroupQuote', invoiceable_id: PolicyGroupQuote.select(:id).references(:policy_application_groups).includes(:policy_application_group).where(policy_application_groups: { carrier_id: nonrefundables }))
                   ),
       category: ['base_premium', 'special_premium', 'taxes']
     ).update_all(refundability: 'no_refund')
