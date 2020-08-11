476ee25e851956c982d1b0f4c159a633db0c1712
evanilukhin <evanilukhin@gmail.com>
2020-08-12 00:50:17 +0300

    Master policies - fix params

--- app/controllers/v2/staff_agency/master_policies_controller.rb
+++ app/controllers/v2/staff_agency/master_policies_controller.rb
@@ -224,7 +224,7 @@ module V2
 
         permitted_params = params.require(:policy).permit(
           :account_id, :agency_id, :auto_renew, :carrier_id, :effective_date,
-          :expiration_date, :number, system_data: [:landlord_supplemental],
+          :expiration_date, :number, system_data: [:landlord_sumplimental],
           policy_coverages_attributes: %i[policy_application_id limit deductible enabled designation]
         )
 
@@ -236,7 +236,7 @@ module V2
 
         permitted_params = params.require(:policy).permit(
           :account_id, :agency_id, :auto_renew, :carrier_id, :effective_date,
-          :expiration_date, :number, system_data: [:landlord_supplemental],
+          :expiration_date, :number, system_data: [:landlord_sumplimental],
           policy_coverages_attributes: %i[id policy_application_id policy_id
                                           limit deductible enabled designation]
         )
