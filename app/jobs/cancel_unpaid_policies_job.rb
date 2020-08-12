48f668c773cb4853b09a98cb5f2c06e67d43c795
Joshua Brinsfield <sapphous@gmail.com>
2020-08-12 19:51:32 -0400

    Fix up cancellation job

--- app/jobs/cancel_unpaid_policies_job.rb
+++ app/jobs/cancel_unpaid_policies_job.rb
@@ -2,15 +2,16 @@ class CancelUnpaidPoliciesJob < ApplicationJob
   queue_as :default
   before_perform :set_policies
 
+  # MOOSE WARNING: policy group stuff commented out for now because PGs introduce problems (no #cancel method, what if someone cancels an individual policy, how are we tracking unearned_premium anyway, etc.)
 
   def perform(*_args)
     curdate = Time.current.to_date
     @policies.each do |policy|
       policy.cancel('nonpayment', curdate)
     end
-    @policy_groups.each do |policy_group|
-      policy_group.cancel('nonpayment', curdate)
-    end
+    #@policy_groups.each do |policy_group|
+    #  policy_group.cancel('nonpayment', curdate)
+    #end
   end
 
   private
@@ -18,7 +19,7 @@ class CancelUnpaidPoliciesJob < ApplicationJob
     def set_policies
       cutoff = Time.current.to_date - 30.days
       @policies = Policy.policy_in_system(true).current.where(billing_status: 'BEHIND').where('billing_behind_since <= ?', cutoff)
-      @policy_groups = PolicyGroup.policy_in_system(true).current.where(billing_status: 'BEHIND').where('billing_behind_since <= ?', cutoff)
+      #@policy_groups = PolicyGroup.policy_in_system(true).current.where(billing_status: 'BEHIND').where('billing_behind_since <= ?', cutoff)
     end
 
 
