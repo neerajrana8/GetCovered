cd8f2e0a67d897aa7d1f45d65bb70a7dbad40ef1
Joshua Brinsfield <sapphous@gmail.com>
2020-08-27 18:06:45 -0400

    Fix billing math, I think

--- app/models/concerns/carrier_msi_policy_application.rb
+++ app/models/concerns/carrier_msi_policy_application.rb
@@ -102,9 +102,14 @@ module CarrierMsiPolicyApplication
               return false
             end
             reading = "MSI_TotalPremiumAmt"
+            total_paid = nil
+            total_installment = nil
+            fee_installment = nil
+            premium_installment = nil
+            down_payment = nil
             begin
               reading = "MSI_TotalPremiumAmt"
-              total_paid = (payment_data.dig("MSI_TotalPremiumAmt", "Amt").to_d * 100).ceil                 # the total amount paid to msi, excluding the policy fee
+              total_paid = (payment_data.dig("MSI_TotalPremiumAmt", "Amt").to_d * 100).ceil                 # the total amount paid to msi, excluding the installment fees
               reading = "MSI_InstallmentPaymentAmount"
               total_installment = (payment_data.dig("MSI_InstallmentPaymentAmount", "Amt").to_d * 100).ceil # the total amount paid at each installment, excluding the first
               reading = "MSI_InstallmentFeeAmount"
@@ -138,7 +143,7 @@ module CarrierMsiPolicyApplication
             quote.update(carrier_payment_data: { 'product_id' => product_uid, 'payment_methods' => payment_methods, 'policy_fee' => msi_policy_fee, 'installment_fee' => fee_installment, 'installment_total' => total_installment })
             # build policy premium
             premium = PolicyPremium.new(
-              base: total_paid - fee_installment * installment_count,
+              base: total_paid,
               taxes: 0,
               external_fees: fee_installment * installment_count,
               only_fees_internal: true,
@@ -156,7 +161,7 @@ module CarrierMsiPolicyApplication
               #quote.generate_invoices_for_term MOOSE WARNING: uncomment if there are ever internal ones...
               # generate external invoices
               gotten_schedule = msi_get_payment_schedule(payment_plan, installment_day: self.fields.find{|f| f['title'] == "Installment Day" }&.[]('value') || 1)
-              last_premium_installment = total_paid - (down_payment + (total_installment * (gotten_schedule.length - 2) + fee_installment))
+              last_premium_installment = total_paid - (down_payment + (total_installment * (installment_count - 1))
               last_premium_installment = premium_installment if last_premium_installment < 0 || gotten_schedule.length <= 2 # should NEVER EVER happen, but letting a negative in would be much worse than overcharging by a few cents and refunding later
               gotten_schedule.each.with_index do |dates, ind|
                 quote.invoices.create!(dates.merge({
