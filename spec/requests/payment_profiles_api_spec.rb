6331592abbc005445a16ecaeb8bf447b80c6ae5a
Joshua Brinsfield <sapphous@gmail.com>
2020-09-03 17:40:52 -0400

    Make payment profiles scream

--- spec/requests/payment_profiles_api_spec.rb
+++ spec/requests/payment_profiles_api_spec.rb
@@ -18,7 +18,6 @@ describe 'PaymentProfile API spec', type: :request do
     end
     
     it 'should create PaymentProfile' do
-      expect(PaymentProfile.count).to eq(0)
       expect { create_profile }.to change { PaymentProfile.count }.by(1)
       expect(PaymentProfile.count).to eq(1)
       result = JSON.parse response.body
