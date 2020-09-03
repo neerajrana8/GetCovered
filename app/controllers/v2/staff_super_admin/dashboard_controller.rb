48e7ec4ebce11e79184bdfce857d19f330411c09
Ivan Ilyukhin <evanilukhin@gmail.com>
2020-09-03 18:43:04 +0300

    Add uninsured_units route to the dashboard (#570)
    * Add uninsured_units route to the dashboard

--- app/controllers/v2/staff_super_admin/dashboard_controller.rb
+++ app/controllers/v2/staff_super_admin/dashboard_controller.rb
@@ -70,6 +70,14 @@ module V2
         end
       end
 
+      def uninsured_units
+        units_relation = Insurable.where(insurable_type_id: InsurableType::UNITS_IDS, covered: false)
+        units_relation = units_relation.where(insurable_id: params[:insurable_id]) if params[:insurable_id].present?
+        @insurables = paginator(units_relation)
+
+        render template: 'v2/shared/insurables/index', status: :ok
+      end
+
       def reports
         min = params[:start]
         max = params[:end]
---
+++ app/views/v2/shared/insurables/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @insurables, partial: 'v2/shared/insurables/index_partial.json.jbuilder', as: :insurable
