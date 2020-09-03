eb9f8f98f3cabc67fc4418085780cd27d4ac2228
Joshua Brinsfield <sapphous@gmail.com>
2020-09-03 17:24:28 -0400

    Fix references to cancellation_date_date and cancellation_code

--- app/jobs/master_coverage_cancel_job.rb
+++ app/jobs/master_coverage_cancel_job.rb
@@ -6,10 +6,10 @@ class MasterCoverageCancelJob < ApplicationJob
     return if master_policy.nil? || master_policy.policy_type_id != PolicyType::MASTER_ID
 
     master_policy.policies.master_policy_coverages.each do |policy|
-      policy.update(status: 'CANCELLED', cancellation_date_date: Time.zone.now, expiration_date: Time.zone.now)
+      policy.update(status: 'CANCELLED', cancellation_date: Time.zone.now, expiration_date: Time.zone.now)
       policy.insurables.take.update(covered: false)
     end
 
-    master_policy.update(status: 'CANCELLED', cancellation_date_date: Time.zone.now, expiration_date: Time.zone.now)
+    master_policy.update(status: 'CANCELLED', cancellation_date: Time.zone.now, expiration_date: Time.zone.now)
   end
 end
