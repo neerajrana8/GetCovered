1853feceb8faa25a36e6a87ae03f160cae28fa55
evanilukhin <evanilukhin@gmail.com>
2020-09-30 11:50:54 +0300

    GCV2-2725 On Profile Attributes Page set pre-populated defaults - update error handling

--- app/interactions/branding_profiles/create_from_default.rb
+++ app/interactions/branding_profiles/create_from_default.rb
@@ -40,6 +40,12 @@ module BrandingProfiles
             merge(branding_profile_id: @branding_profile.id)
         end
       branding_profile_attributes = BrandingProfileAttribute.create(branding_profile_attributes_params)
+      bad_attributes = branding_profile_attributes.select { |attribute| attribute.errors.any? }
+      if bad_attributes.any?
+        errors[:branding_profile_attributes] = []
+        errors[:branding_profile_attributes] << bad_attributes.map(&:errors)
+        raise ActiveRecord::Rollback
+      end
     end
 
     def create_pages
@@ -50,16 +56,37 @@ module BrandingProfiles
           merge(branding_profile_id: @branding_profile.id, agency_id: agency.id)
       end
       pages = Page.create(pages_params)
+
+      bad_pages = pages.select { |page| page.errors.any? }
+      if bad_pages.any?
+        errors[:pages] = []
+        errors[:pages] << bad_pages.map(&:errors)
+        raise ActiveRecord::Rollback
+      end
     end
 
     def create_faqs
       faqs_params = default_branding_profile.faqs.map do |faq|
-        faq.
-          attributes.
-          except('id', 'created_at', 'updated_at').
-          merge(branding_profile_id: @branding_profile.id)
+        faq_params =
+          faq.
+            attributes.
+            except('id', 'created_at', 'updated_at').
+            merge(branding_profile_id: @branding_profile.id, faq_questions_attributes: [])
+        faq.faq_questions do |faq_question|
+          faq_params[:faq_questions_attributes] << faq_question.attributes.slice('question', 'answer')
+        end
+        faq_params
       end
+
       faqs = Faq.create(faqs_params)
+
+      bad_faqs = faqs.select { |faq| faq.errors.any? }
+      if bad_faqs.any?
+        errors[:faqs] = []
+        errors[:faqs] << bad_faqs.map(&:errors)
+        raise ActiveRecord::Rollback
+      end
+
     end
   end
 end
