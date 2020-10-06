eef140aaa3887ddd72550bbfd630fe240f1a4451
evanilukhin <evanilukhin@gmail.com>
2020-10-06 17:50:17 +0300

    GCV2-2776 Payment step on the Rent Guarantee should be shown right after filling up the main application - update mailer

---
+++ app/mailers/user_coverages/rent_guarantee_mailer.rb
@@ -0,0 +1,11 @@
+module UserCoverages
+  class RentGuaranteeMailer < UserCoverageMailer
+    def proof_of_coverage
+      attach_all_documents
+      @login_url = "#{BrandingProfiles::FindByObject.run!(object: @policy.agency)&.url}/auth/login"
+      @contact_email = @policy.agency.owner&.email
+      @pensio_will_pay = @policy.policy_application.fields['monthly_rent'].to_f * @policy.policy_application.fields["guarantee_option"].to_i
+      mail(subject: 'Congratulations on Your New Rent Guarantee Policy')
+    end
+  end
+end
