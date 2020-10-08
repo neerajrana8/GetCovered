be5a49206a994fde04aa96925f2afc49b8814b4d
evanilukhin <evanilukhin@gmail.com>
2020-10-08 10:11:24 +0300

    Add API to the admin dashboard for refunds

--- app/controllers/v2/staff_agency/policy_cancellation_requests_controller.rb
+++ app/controllers/v2/staff_agency/policy_cancellation_requests_controller.rb
@@ -5,36 +5,15 @@
 module V2
   module StaffAgency
     class PolicyCancellationRequestsController < StaffAgencyController
-      before_action :set_change_request, only: %i[show approve decline]
-
-      def index
-        @change_requests = paginator(relation)
-        render json: @change_requests, status: :ok
-      end
-
-      def show
-        render json: @change_request, status: :ok
-      end
-
-      def approve
-        @change_request.changeable.cancel('manual_cancellation_with_refunds')
-        @change_request.update(status: :approved)
-      end
-
-      def decline
-        @change_request.update(status: :declined)
-      end
+      include PolicyCancellationRequestsMethods
 
       private
 
-      def set_change_request
-        @change_request = relation.find(params[:id])
-      end
-
       def relation
         ChangeRequest.
-          joins('INNER JOIN policies ON (change_requests.changeable_id == policies.id AND change_requests.changeable_type = "Policy"').
-          where(customized_action: 'cancel').
+          joins('INNER JOIN policies ON (change_requests.changeable_id = policies.id AND change_requests.changeable_type = \'Policy\')').
+          where(change_requests: { customized_action: 'cancel' }).
+          where(policies: { agency_id: @agency.id }).
           order(created_at: :desc)
       end
     end
