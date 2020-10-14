ac04db15abe0c299ccce5fd7aea664fe2414aadb
evanilukhin <evanilukhin@gmail.com>
2020-10-14 17:31:12 +0300

    GCV2-2823 Refunds Notifications Emails - create emails

---
+++ app/mailers/policies/cancellation_mailer.rb
@@ -0,0 +1,34 @@
+module Policies
+  class CancellationMailer < ApplicationMailer
+    before_action :set_variables
+
+    default to: -> { @user.email },
+            from: -> { 'no-reply@getcoveredinsurance.com' }
+
+    def refund_request
+      mail(subject: "#{@agency.title} - #{@policy.policy_type.title} Refund Request", bcc: @agency.owner&.email)
+    end
+
+    def cancel_request
+      mail(subject: "#{@agency.title} - #{@policy.policy_type.title} Cancellation Request", bcc: @agency.owner&.email)
+    end
+
+    def cancel_confirmation
+      mail(subject: "#{@agency.title} - #{@policy.policy_type.title} Policy Cancellation", bcc: @agency.owner&.email)
+    end
+
+    private
+
+    def set_variables
+      @policy = params[:policy]
+      @request_date = params[:request_date]
+      @user = @policy.primary_user
+      @agency = @policy.agency
+      @contact_email =
+        BrandingProfiles::FindByObject.run!(object: @agency)&.
+          branding_profile_attributes&.
+          find_by_name('contact_email')&.
+          value
+    end
+  end
+end
