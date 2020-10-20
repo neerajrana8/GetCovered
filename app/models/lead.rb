423389b87f4cf4f0c66ab0708c93dabb11ec6d88
ravilgaifullin <ravil.gaifullin@nixsolutions.com>
2020-10-20 16:11:56 +0300

    GCV2-2813 lead agency_id remake and tracking_url

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -5,7 +5,7 @@ class Lead < ApplicationRecord
 
   has_one :profile, as: :profileable
   has_one :address, as: :addressable
-
+  
   has_many :lead_events, dependent: :destroy
 
   accepts_nested_attributes_for :address, :profile, update_only: true
