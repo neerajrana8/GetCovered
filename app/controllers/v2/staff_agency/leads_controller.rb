0ec34788b00e2aea4ffca77f6f370a167afb8c89
ravilgaifullin <ravil.gaifullin@nixsolutions.com>
2020-10-22 19:10:23 +0300

    GCV2-2813 fixed 500 error

--- app/controllers/v2/staff_agency/leads_controller.rb
+++ app/controllers/v2/staff_agency/leads_controller.rb
@@ -2,14 +2,25 @@ module V2
   module StaffAgency
     class LeadsController < StaffAgencyController
 
+      before_action :set_substrate, only: :index
+
       def index
-        super(:@leads, access_model(::Lead).includes(:profile, :tracking_url))
+        super(:@leads, @substrate)
         render 'v2/shared/leads/index'
       end
 
       def show
         @lead = access_model(::Lead, params[:id])
         render 'v2/shared/leads/show'
+      end
+
+      private
+
+      def set_substrate
+        super
+        if @substrate.nil?
+          @substrate = access_model(::Lead).includes(:profile, :tracking_url)
+        end
       end
     end
   end
