14d2543c78642d725aedcc3fb91b3152ba34d3a6
evanilukhin <evanilukhin@gmail.com>
2020-10-26 09:44:36 +0300

    GCV2-2894 Import/Export branding profiles

--- app/controllers/concerns/branding_profiles_methods.rb
+++ app/controllers/concerns/branding_profiles_methods.rb
@@ -2,8 +2,9 @@ module BrandingProfilesMethods
   extend ActiveSupport::Concern
 
   def import
+    file = params[:input_file].open
     branding_profile_import =
-      BrandingProfiles::Import.run(branding_profile_params: branding_profile_import_params, agency: @agency)
+      BrandingProfiles::Import.run(branding_profile_params: JSON.parse(file.read), agency: @agency)
     if branding_profile_import.valid?
       @branding_profile = branding_profile_import.result
       render :show, status: :ok
@@ -14,6 +15,9 @@ module BrandingProfilesMethods
   end
 
   def export
-    render json: BrandingProfiles::Export.run!(branding_profile: @branding_profile).to_json, status: :ok
+    result = BrandingProfiles::Export.run!(branding_profile: @branding_profile).to_json
+    send_data result,
+              filename: "branding-profile-#{@branding_profile.profileable_type.underscore.split('/').last}-#{Date.today}.json",
+              status: :ok
   end
 end
