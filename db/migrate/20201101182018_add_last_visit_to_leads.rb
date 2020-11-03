ce4ae3e71a091abeb92874516a8407d9c37d955c
hanna-bautsiuk <hannabts@nitka.com>
2020-11-03 11:43:18 +0300

    Delete unused

--- db/migrate/20201101182018_add_last_visit_to_leads.rb
+++
@@ -1,5 +0,0 @@
-class AddLastVisitToLeads < ActiveRecord::Migration[5.2]
-  def change
-    Lead.where(last_visit: nil).update_all('last_visit=created_at')
-  end
-end
--- db/migrate/20201102133300_add_test_tracking_for_dev.rb
+++
@@ -1,8 +0,0 @@
-class AddTestTrackingForDev < ActiveRecord::Migration[5.2]
-  def change
-    leads = Lead.where(last_visit: '2020-10-01'..'2020-10-30')
-    agency = Agency.find_by(title: "Get Covered")
-    tr_url = TrackingUrl.create!(campaign_source: "test_s", campaign_medium: "test_m", campaign_term: "test_t", campaign_name: 'Test Campaign' ,agency_id: agency.id, landing_page: "rent_guarantee")
-    leads.update_all(tracking_url_id: tr_url.id)
-  end
-end
