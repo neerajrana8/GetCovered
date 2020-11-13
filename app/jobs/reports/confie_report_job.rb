ec3eec2493cc8a6dab2bf7da820732a1f853b28a
Joshua Brinsfield <sapphous@gmail.com>
2020-11-13 13:19:16 -0500

    Move credentials to cred file

--- app/jobs/reports/confie_report_job.rb
+++ app/jobs/reports/confie_report_job.rb
@@ -12,7 +12,7 @@ module Reports
       def set_policy_applications
         current_time = Time.current.beginning_of_hour
         @policy_applications = ::PolicyApplication.where(agency_id: ConfieService.agency_id)
-                                                  .where.not(status: ['complete']) # MOOSE WARNING: expand?
+                                                  .where.not(status: ['accepted']) # MOOSE WARNING: expand?
                                                   .where(created_at: (current_time - 1.hour)...(current_time))
       end
   end
