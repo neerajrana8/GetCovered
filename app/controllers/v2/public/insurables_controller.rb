211b058582d90dc09ff7fd5f4f6270f5374fa87e
Joshua Brinsfield <sapphous@gmail.com>
2020-11-16 19:51:11 -0500

    Implement the great hellbeast Insurable.get_or_create

--- app/controllers/v2/public/insurables_controller.rb
+++ app/controllers/v2/public/insurables_controller.rb
@@ -50,6 +50,92 @@ module V2
       end
       
       
+      
+      def get_or_create_params
+        params.permit(:address, :unit_title, :insurable_id, :insurable_type_id)
+      end
+      
+            
+      def get_or_create
+        # get a valid address model if possible
+        address = ::Address.from_string(params[:address])
+        unless address.errors.blank?
+          render json: standard_error(:invalid_address, 'Invalid address value', address.errors.full_messages),
+                 status: 422
+          return
+        end
+        # try to figure out unit title if applicable
+        seeking_unit = (!get_or_create_params[:unit_title].blank? || !get_or_create_params[:insurable_id].blank? || !address.street_two.blank?) ? true : false
+        unit_title = nil
+        if seeking_unit
+          unit_title = get_or_create_params[:unit_title]
+          if unit_title.blank? && !address.street_two.blank?
+            splat = address.street_two.gsub('#', ' ').gsub('.', ' ')
+                                      .gsub(/\s+/m, ' ').gsub(/^\s+|\s+$/m, '')
+                                      .split(" ").select do |strang|
+                                        ![
+                                          'apartment', 'apt', 'unit',
+                                          'flat', 'room', 'office',
+                                          'no', 'number'
+                                        ].include?(strang.lcase)
+                                      end
+            if splat.size == 1
+              unit_title = splat[0]
+            end
+          end
+        end
+        ###insurable_type_id = params[:insurable_type_id].to_i || (!address.street_two.blank? ? ::InsurableType.where(title: "Residential Community").take : ::InsurableType.where(title: "Residential Unit").take)
+
+        # search for the insurable
+        results = ::Insurable.references(:address).includes(:addresses).where({
+          addresses: {
+            primary: true, 
+            street_number: address.street_number,
+            street_name: address.street_name,
+            city: address.city,
+            state: address.state,
+            zip_code: address.zip_code
+          }
+        })
+        if results.blank?
+          # no results
+        end
+        if seeking_unit
+          if unit_title.
+        else
+        end
+        
+        
+        
+        results = ::Insurable.find_from_address(address, { enabled: true, insurable_type_id: ::InsurableType::RESIDENTIAL_COMMUNITIES_IDS | ::InsurableType::RESIDENTIAL_BUILDINGS_IDS }, allow_multiple: false)
+        
+        
+        
+        results = ::Insurable.find_from_address(address, { enabled: true, insurable_type_id: ::InsurableType::RESIDENTIAL_COMMUNITIES_IDS | ::InsurableType::RESIDENTIAL_BUILDINGS_IDS }, allow_multiple: false)
+        if results.nil? || (::InsurableType::RESIDENTIAL_BUILDINGS_IDS.include?(results.insurable_type_id) && (!results.parent_community.preferred_ho4 || !results.parent_community.enabled))
+          render json: { id: nil, preferred_ho4: false, units: nil },
+            status: 200
+          return
+        end
+        i = results
+        render json: {
+          id: i.id,
+          title: i.title,
+          enabled: i.enabled,
+          preferred_ho4: true,
+          account_id: i.account_id,
+          agency_id: i.account.agency_id,
+          insurable_type_id: i.insurable_type_id,
+          category: i.category,
+          covered: i.covered,
+          created_at: i.created_at,
+          updated_at: i.updated_at,
+          addresses: i.addresses,
+          units: i.units.select{|u| u.enabled }.map{|u| { id: u.id, title: u.title } }
+        }, status: 200
+      end
+      
+      
       private
       
         def view_path
--- app/models/insurable.rb
+++ app/models/insurable.rb
@@ -231,7 +231,243 @@ class Insurable < ApplicationRecord
       self.save
     end
   end
-  
+
+
+      
+            
+  def self.get_or_create(
+    address: nil,                 # an address string (required unless unit title and insurable_id provided)
+    unit: nil,                    # true to search for units, false to search for buildings/communities, a string to search for a specific unit title; nil means search for a unit if address has line 2 and a community otherwise
+    insurable_id: nil,            # optional; the community/building the sought insurable must belong to
+    create_if_ambiguous: false,   # pass true to force insurable creation if there's any ambiguity (example: if you've already called this and got 'multiple' type results, none of which were what you wanted)
+    disallow_creation: false,     # pass true to ONLY query, NOT create
+    created_community_title: nil, # optionally pass the title for the community in case we have to create it (defaults to combined_street_address)
+    account_id: nil               # optionally, the account id to use if we create anything
+  )
+    # validate params
+    if address.nil? && ([true,false,nil].include?(unit) || insurable_id.nil?)
+      raise ArgumentError.new("either 'address' or 'insurable_id' and a string 'unit' must be provided")
+    end
+    # if we have a unit title and an insurable id, get or create the unit without dealing with address nonsense
+    unit_title = [true,false,nil].include?(unit) ? nil : unit
+    if !unit_title.blank? && !insurable_id.nil?
+      results = ::Insurable.where(title: unit_title, insurable_id: insurable_id, insurable_type_id: ::InsurableType::RESIDENTIAL_UNITS_IDS)
+      case results.count
+        when 0
+          return nil if disallow_creation
+          results = ::Insurable.where(id: insurable_id, insurable_type_id: ::InsurableType::RESIDENTIAL_COMMUNITIES_IDS | ::InsurableType::RESIDENTIAL_BUILDINGS_IDS).take
+          if results.blank?
+            return { error_type: :invalid_community, message: "The requested residential building/community id does not exist" }
+          end
+          community = (results.parent_community || results)
+          if community.preferred_ho4
+            return { error_type: :invalid_unit, message: "The requested unit does not exist" }
+          end
+          unit = results.insurables.new(
+            title: unit_title,
+            insurable_type: ::InsurableType.where(title: "Residential Unit").take,
+            enabled: true, category: 'property', preferred_ho4: false,
+            account_id: result.account_id || community.account_id || nil # MOOSE WARNING: nil account id allowed?
+          )
+          unless unit.save
+            return { error_type: :invalid_unit, message: "Unable to create unit", details: unit.errors.full_messages }
+          end
+          return unit
+        when 1
+          return results.first
+        else
+          return results.to_a
+      end
+    end
+    # get a valid address model if possible
+    if address.class == ::Address
+      unless address.valid?
+        return { error_type: :invalid_address, message: "Invalid address value", details: address.errors.full_messages }
+      end
+    else
+      address = ::Address.from_string(address)
+      unless address.errors.blank?
+        return { error_type: :invalid_address, message: "Invalid address value", details: address.errors.full_messages }
+      end
+    end
+    # try to figure out unit title if applicable
+    seeking_unit = unit ? true : unit.nil? ? !address.street_two.blank? : false
+    if seeking_unit
+      if unit_title.blank? && !address.street_two.blank?
+        splat = address.street_two.gsub('#', ' ').gsub('.', ' ')
+                                  .gsub(/\s+/m, ' ').gsub(/^\s+|\s+$/m, '')
+                                  .split(" ").select do |strang|
+                                    ![
+                                      'apartment', 'apt', 'unit',
+                                      'flat', 'room', 'office',
+                                      'no', 'number'
+                                    ].include?(strang.lcase)
+                                  end
+        if splat.size == 1
+          unit_title = splat[0]
+        end
+      end
+    end
+    # search for the insurable
+    if seeking_unit # we want a unit
+      if unit_title.nil?
+        return { error_type: :invalid_address_line_two, message: "Unable to deduce unit title from address", details: "'#{address.line_two}' is not a standard format (e.g. 'Apartment #2, Unit 3, #5, etc.)" }
+      end
+      # query for units of the appropriate title, address, and, if provided, insurable_id
+      parent_ids = ::Insurable.references(:address).includes(:addresses).where(
+        {
+          addresses: {
+            primary: true, 
+            street_number: address.street_number,
+            street_name: address.street_name,
+            city: address.city,
+            state: address.state,
+            zip_code: address.zip_code
+          },
+          insurable_type_id: ::InsurableType::RESIDENTIAL_COMMUNITIES_IDS | ::InsurableType::RESIDENTIAL_BUILDINGS_IDS | ::InsurableType::RESIDENTIAL_UNITS_IDS
+        }
+      ).order(:id).group(:id).pluck(:id)
+      results = nil
+      if !insurable_id.nil?
+        parent_ids = parent_ids & [insurable_id]
+        results = ::Insurable.where({
+          title: unit_title,
+          insurable_type_id: ::InsurableType::RESIDENTIAL_UNITS_IDS,
+          insurable_id: parent_ids
+        })
+      else
+        results = ::Insurable.where({
+          title: unit_title,
+          insurable_type_id: ::InsurableType::RESIDENTIAL_UNITS_IDS,
+          insurable_id: parent_ids
+        }).or(::Insurable.where({ # gotta account for the possibility that a standalone-addressed unit came up in our initial query
+          id: parent_ids,
+          title: unit_title,
+          insurable_type_id: ::InsurableType::RESIDENTIAL_UNITS_IDS
+        }))
+      end
+      # handle the units returned by the query
+      case results.count
+        when 0
+          # unit does not exist; find a parent we can create on
+          return nil if disallow_creation
+          parents = ::Insurable.where(id: parent_ids, insurable_type_id: ::InsurableType::RESIDENTIAL_COMMUNITIES_IDS | ::InsurableType::RESIDENTIAL_BUILDINGS_IDS)
+          parent = parents.select{|p| (p.parent_community || p).preferred_ho4 == false }.sort{|a,b| (::InsurableType::RESIDENTIAL_BUILDINGS_IDS.include?(a) ? -1 : 1) <=> (::InsurableType::RESIDENTIAL_BUILDINGS_IDS.include?(b) ? -1 : 1) }.first
+          if parent.nil?
+            # flee if prevented from creating community
+            if disallow_creation
+              return nil
+            elsif !insurable_id.nil?
+              return { error_type: :invalid_community, message: "The requested residential building/community id does not exist" }
+            elsif parents.count > 0 # if parents.count > 0, we found the community/building but it was preferred
+              return { error_type: :invalid_unit, message: "The requested unit does not exist" }
+            end
+            # create community
+            address.id = nil
+            address.primary = true
+            address.street_two = nil
+            parent = ::Insurable.new(
+              title: address.combined_street_address,
+              insurable_type: ::InsurableType.where(title: "Residential Community").take,
+              enabled: true, preferred_ho4: false, category: 'property',
+              addresses: [ address ],
+              account_id: nil # MOOSE WARNING: nil account_id???
+            )
+            unless parent.save
+             return { error_type: :invalid_community, message: "Unable to create community from address", details: parent.errors.full_messages }
+            end
+          end
+          # create the unit
+          unit = results.insurables.new(
+            title: unit_title,
+            insurable_type: ::InsurableType.where(title: "Residential Unit").take,
+            enabled: true, category: 'property', preferred_ho4: false,
+            account_id: parent.account_id || nil # MOOSE WARNING: nil account id allowed?
+          )
+          unless unit.save
+            return { error_type: :invalid_unit, message: "Unable to create unit", details: unit.errors.full_messages }
+          end
+          return unit
+        when 1
+          # we found the unit
+          return results.first
+        else
+          # we found multiple candidate units
+          return results.to_a
+      end
+    else # we want a community (or building)
+      results = ::Insurable.references(:address).includes(:addresses).where(
+        {
+          addresses: {
+            primary: true, 
+            street_number: address.street_number,
+            street_name: address.street_name,
+            city: address.city,
+            state: address.state,
+            zip_code: address.zip_code
+          },
+          insurable_type_id: ::InsurableType::RESIDENTIAL_COMMUNITIES_IDS | ::InsurableType::RESIDENTIAL_BUILDINGS_IDS,
+          insurable_id: insurable_id
+        }.compact
+      )
+      unless address.street_two.blank?
+        with_street_two = results.select{|res| res.primary_address.street_two.strip == address.street_two.strip }
+        case with_street_two.count
+          when 0
+            # we can create a community with the exact specified line 2, or we can drop back to the blank street two block and return the partial matches
+            if create_if_ambiguous || results.count == 0
+              results = [] # force results count to be empty so we fall through and end up creating
+            elsif results.count == 1
+              return results.to_a # single result, but non-definitive, so still in an array
+            end
+          when 1
+            return with_street_two[0]
+          else
+            return with_street_two.to_a
+        end
+      end
+      case results.count
+        when 0
+          return nil if disallow_creation
+          # try to get parent if applicable
+          parent = nil
+          unless insurable_id.nil?
+            parent = ::Insurable.where(id: insurable_id, insurable_type_id: ::InsurableType::RESIDENTIAL_COMMUNITIES_IDS).take
+            if parent.nil?
+              return { error_type: :invalid_building, message: "Requested parent community does not exist" }
+            elsif parent.preferred_ho4
+              return { error_type: :invalid_building, message: "Requested building does not exist" }
+            else
+              parent_address = parent&.primary_address
+              if parent_address.state != address.state || parent_address.zip_code != address.zip_code || parent_address.city != address.city
+                return { error_type: :invalid_building, message: "Requested parent community is not in the same state/zip/city" }
+              end
+            end
+          end
+          # create community (or building, if there was a parent provided)
+          address.id = nil
+          address.primary = true
+          created = ::Insurable.new(
+              title: created_created_title || address.combined_street_address,
+              insurable_type: ::InsurableType.where(title: parent.nil? ? "Residential Community" : "Residential Building").take,
+              enabled: true, preferred_ho4: false, category: 'property',
+              addresses: [ address ],
+              account_id: parent&.account_id || nil # MOOSE WARNING: nil account_id???
+            )
+          unless created.save
+           return { error_type: :"invalid_#{parent.nil? ? 'community' : 'building'}", message: "Unable to create #{parent.nil? ? 'community' : 'building'} from address", details: created.errors.full_messages }
+          end
+          return created
+        when 1
+          return results.first
+        else
+          return results.to_a
+      end
+    end
+    # every possible case resulted in a return already
+    return { error_type: :internal_error, message: "Internal error occurred" }
+  end
+      
   
   private
 
