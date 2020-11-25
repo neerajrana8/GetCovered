256ae28b7e0a0eac0a5ef9f34fadda3f539abecf
Joshua Brinsfield <sapphous@gmail.com>
2020-11-25 15:17:29 -0500

    Fix nonpreferred additional interest

--- app/services/confie_service.rb
+++ app/services/confie_service.rb
@@ -250,7 +250,7 @@ class ConfieService
         additional_insured: policy.policy_users.select{|pu| !pu.primary }.map do |pu|
           pu.user.profile.full_name
         end,
-        additional_interest: ((policy.policy_type_id == ::PolicyType::RESIDENTIAL_ID && policy.carrier_id == MsiService.carrier_id && !policy.account.nil?) ?
+        additional_interest: ((policy.policy_type_id == ::PolicyType::RESIDENTIAL_ID && policy.carrier_id == MsiService.carrier_id && !policy.account.nil? && policy.account.title != "Nonpreferred Residential") ?
           policy.account.title
           : nil
         )
