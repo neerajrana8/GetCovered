89da507be848bb817f227565ca7e47fdaa806ccb
Joshua Brinsfield <sapphous@gmail.com>
2020-11-25 14:10:50 -0500

    Tweak repjob

--- app/jobs/reports/confie_report_job.rb
+++ app/jobs/reports/confie_report_job.rb
@@ -11,7 +11,8 @@ module Reports
     
       def set_policy_applications
         current_time = Time.current.beginning_of_hour
-        @policy_applications = ::PolicyApplication.where(agency_id: ConfieService.agency_id)
+        confie_id = ConfieService.agency_id
+        @policy_applications = confie_id.nil? ? [] : ::PolicyApplication.where(agency_id: confie_id)
                                                   .where.not(status: ['accepted']) # MOOSE WARNING: expand?
                                                   .where(created_at: (current_time - 1.hour)...(current_time))
       end
