9f7bae1dba5502d7334e90ee5fc512aae8ee2476
evanilukhin <evanilukhin@gmail.com>
2020-12-09 08:57:16 +0300

    GCV2-3131 Backend: Localization of the registration emails

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,10 +1,19 @@
 class CustomDeviseMailer < Devise::Mailer
   helper MailerHelper
+  before_action do
+    I18n.locale = @resource.profile&.language || I18n.default_locale
+    ap @resource
+  end
 
   def invitation_instructions(record, token, opts = {})
     if opts[:policy_application].present?
       @policy_application = opts[:policy_application]
-      opts[:subject] = "Finish Registering Your #{@policy_application.agency&.title} Account - #{@policy_application.policy_type.title}"
+      client_host = headers['client_host'] || Rails.application.credentials.uri[ENV["RAILS_ENV"].to_sym][:client]
+      @accept_link = "#{client_host}/auth/accept-invitation/#{@token}"
+
+      opts[:subject] = t('devise.mailer.product_invitation_instruction.subject',
+                         agency_title: '@policy_application.agency&.title',
+                         policy_type_title: @policy_application.policy_type.title)
       opts[:template_name] = 'product_invitation_instructions'
     end
 
