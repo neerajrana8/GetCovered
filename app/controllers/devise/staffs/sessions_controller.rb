7c419f89d5b177bc23d0a8590ac2ff5e96aeb984
hanna-bautsiuk <hannabts@nitka.com>
2020-12-10 22:22:28 +0300

    Another portion of translations

--- app/controllers/devise/staffs/sessions_controller.rb
+++ app/controllers/devise/staffs/sessions_controller.rb
@@ -1,20 +1,20 @@
 class Devise::Staffs::SessionsController < DeviseTokenAuth::SessionsController
   include SessionsMethods
-  
+
   protected
 
   def render_create_success
     # @resource will have been set by set_user_by_token concern
     if @resource
       @staff = @resource
-      
+
       render template: 'v2/shared/staffs/show.json', status: :ok
     else
       render json: {
         success: false,
-        errors: ['Invalid login credentials']
+        errors: [I18n.t('user_users_controler.invalid_login_credentials')]
       }, status: 401
-    end        
-  end 
-    
+    end
+  end
+
 end
