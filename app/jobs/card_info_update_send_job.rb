3cf9a5cb3a6f6745297808104a99d64387c2f35a
evanilukhin <evanilukhin@gmail.com>
2020-12-18 14:28:04 +0300

    GCV2-2623 - Billing Notifications - fix mail

--- app/jobs/card_info_update_send_job.rb
+++ app/jobs/card_info_update_send_job.rb
@@ -4,7 +4,7 @@ class CardInfoUpdateSendJob < ApplicationJob
   def perform(charge)
     user = charge.invoice.payer
     if user.is_a? User
-      policy = charge.invoice.invoiceable
+      policy = charge.invoice.invoiceable.policy
 
       CardInfoUpdateMailer.please_update_card_info(user: user, policy: policy).deliver
     end
