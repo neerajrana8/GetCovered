d156e7b8f90b1275fa29faa4ccd5c7e8612bda36
evanilukhin <evanilukhin@gmail.com>
2020-12-23 14:26:53 +0300

    GCV2-3164 Backend: Implement notification settings - remove unsued setting

--- app/models/notification_setting.rb
+++ app/models/notification_setting.rb
@@ -1,5 +1,5 @@
 class NotificationSetting < ApplicationRecord
-  USERS_NOTIFICATIONS = %w[upcoming_invoice update_credit_card warning_emails].freeze
+  USERS_NOTIFICATIONS = %w[upcoming_invoice update_credit_card].freeze
 
   belongs_to :notifyable, polymorphic: true # User, Staff or other object/person who can manipulate notifications
 
