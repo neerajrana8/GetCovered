a9675aca92ebc93f777f00e3288d4441bd648754
evanilukhin <evanilukhin@gmail.com>
2020-12-29 19:55:50 +0300

    GCV2-3198 Warnings for missed payments - fix job

--- app/jobs/bill_past_due_invoices_job.rb
+++ app/jobs/bill_past_due_invoices_job.rb
@@ -7,7 +7,7 @@ class BillPastDueInvoicesJob < ApplicationJob
   def perform(*_args)
     @invoices.each do |invoice|
       invoice.pay(allow_missed: true, stripe_source: :default) if invoice.charges.failed.where('created_at >= ?', invoice.due_date.midnight + 1.day).count < PAYMENT_ATTEMPTS
-      reload
+      invoice.reload
       next unless rent_guarantee_notify?(invoice)
 
       rent_guarantee_notify(invoice)
@@ -19,12 +19,12 @@ class BillPastDueInvoicesJob < ApplicationJob
   def set_invoices
     # WARNING: we take Policies/PolicyGroups which are BEHIND and have been so for 1-29 days... we check each invoice's charges manually to count the number of tries in perform
     curdate = Time.current.to_date
-    @invoices = Invoice.where(invoiceable_type: 'PolicyQuote', invoiceable_id: PolicyQuote.select(:id).where(status: 'accepted', policy_id: Policy.select(:id).policy_in_system(true).current.where(auto_pay: true, billing_status: 'BEHIND', billing_behind_since: (curdate - 30.days)...curdate)))
-      .or(
+    @invoices = Invoice.where(invoiceable_type: 'PolicyQuote', invoiceable_id: PolicyQuote.select(:id).where(status: 'accepted', policy_id: Policy.select(:id).policy_in_system(true).current.where(auto_pay: true, billing_status: 'BEHIND', billing_behind_since: (curdate - 30.days)...curdate))).
+      or(
             Invoice.where(invoiceable_type: 'PolicyGroupQuote', invoiceable_id: PolicyGroupQuote.select(:id).where(status: 'accepted', policy_group_id: PolicyGroup.select(:id).policy_in_system(true).current.where(auto_pay: true, billing_status: 'BEHIND', billing_behind_since: (curdate - 30.days)...curdate)))
-          )
-      .where("due_date < '#{curdate.to_s(:db)}'")
-      .where(status: 'missed', external: false)
+          ).
+      where("due_date < '#{curdate.to_s(:db)}'").
+      where(status: 'missed', external: false)
   end
 
   def rent_guarantee_notify?(invoice)
