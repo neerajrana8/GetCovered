ce3efce655052684b9e11489627a20a40b68eb3e
Joshua Brinsfield <sapphous@gmail.com>
2021-01-18 13:00:50 -0500

    Add account id setter from dev

--- app/controllers/v2/user/policy_applications_controller.rb
+++ app/controllers/v2/user/policy_applications_controller.rb
@@ -325,6 +325,7 @@ module V2
         @policy_application = PolicyApplication.find(params[:id])
 
         if @policy_application.policy_type.title == 'Residential'
+          @policy_application.account_id = @policy_application.primary_insurable&.account_id
           @policy_application.policy_rates.destroy_all
           # try to update
           @policy_application.assign_attributes(update_residential_params)
