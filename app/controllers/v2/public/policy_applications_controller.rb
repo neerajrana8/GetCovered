9c5d1e683687eb4f378c6418e33d762595c8c53e
Joshua Brinsfield <sapphous@gmail.com>
2021-01-18 12:58:39 -0500

    Fix papp#update

--- app/controllers/v2/public/policy_applications_controller.rb
+++ app/controllers/v2/public/policy_applications_controller.rb
@@ -586,7 +586,7 @@ module V2
                 @quote = @policy_application.policy_quotes.order("updated_at DESC").limit(1).first
 
                 if @policy_application.status == "quote_failed"
-                  render json: standard_error(:policy_application_unavailable, @policy_application.error_message || I18n.t('policy_application_contr.create_security_deposit_replacement.policy_application_unavailable')),
+                  render json: standard_error(:policy_application_unavailable, I18n.t('policy_application_contr.create_security_deposit_replacement.policy_application_unavailable') + " #{@policy_application.error_message}"),
                          status: 400
                 elsif @policy_application.status == "quoted"
                   render json: standard_error(:policy_application_unavailable, I18n.t('policy_application_contr.create_security_deposit_replacement.policy_application_unavailable')),
