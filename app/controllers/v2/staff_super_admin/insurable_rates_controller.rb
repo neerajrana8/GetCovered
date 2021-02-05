30b9fe8d388ea8545c03cc0e7c92ccd6d1265fe0
evanilukhin <evanilukhin@gmail.com>
2021-02-05 11:51:16 +0300

    GCV2-3481 API Insurable_rates for super_admin returns 404

--- app/controllers/v2/staff_super_admin/insurable_rates_controller.rb
+++ app/controllers/v2/staff_super_admin/insurable_rates_controller.rb
@@ -5,43 +5,41 @@
 module V2
   module StaffSuperAdmin
     class InsurableRatesController < StaffSuperAdminController
-
-      before_action :set_agency
       before_action :set_insurable
       before_action :set_insurable_rate, only: [:update]
 
       def index
-	      @rates = {}
+        @rates = {}
 
-	      @rates["coverage_c"] = @insurable.insurable_rates
-		  										 							 .coverage_c
-													 							 .activated
+        @rates['coverage_c'] = @insurable.insurable_rates
+          .coverage_c
+          .activated
 
-		  	@rates["liability"] = @insurable.insurable_rates
-									  										.liability
-									  									  .activated
+        @rates['liability'] = @insurable.insurable_rates
+          .liability
+          .activated
 
-		  	@rates["optional"] = @insurable.insurable_rates
- 	  							  									 .optional
-  								  									 .activated
-								  										 .where.not(sub_schedule: "policy_fee")
+        @rates['optional'] = @insurable.insurable_rates
+          .optional
+          .activated
+          .where.not(sub_schedule: 'policy_fee')
 
-	      render json: @rates.to_json,
-	             status: :ok
+        render json: @rates.to_json,
+               status: :ok
       end
 
       def update
         if update_allowed?
           if @insurable_rate.update(update_params)
             render json: { success: true },
-              status: :ok
+                   status: :ok
           else
             render json: @insurable_rate.errors,
-              status: :unprocessable_entity
+                   status: :unprocessable_entity
           end
         else
           render json: { success: false, errors: ['Unauthorized Access'] },
-            status: :unauthorized
+                 status: :unauthorized
         end
       end
 
@@ -51,40 +49,35 @@ module V2
 
       private
 
-        def view_path
-          super + "/insurable_rates"
-        end
+      def view_path
+        super + '/insurable_rates'
+      end
 
-        def update_allowed?
-          true
-        end
+      def update_allowed?
+        true
+      end
 
-        def set_insurable
-          @insurable = @agency.insurables.find(params[:insurable_id])
-        end
+      def set_insurable
+        @insurable = Insurable.find(params[:insurable_id])
+      end
 
-        def set_agency
-          @agency = Agency.find(params[:agency_id])
-        end
+      def set_insurable_rate
+        @insurable_rate = @insurable.insurable_rates.find(params[:id])
+      end
 
-        def set_insurable_rate
-          @insurable_rate = @insurable.insurable_rates.find(params[:id])
-        end
+      def update_params
+        params.require(:insurable_rate).permit(:enabled, :mandatory)
+      end
 
-        def update_params
-          params.require(:insurable_rate).permit(:enabled, :mandatory)
-        end
+      def supported_filters(called_from_orders = false)
+        @calling_supported_orders = called_from_orders
+        {
+        }
+      end
 
-        def supported_filters(called_from_orders = false)
-          @calling_supported_orders = called_from_orders
-          {
-          }
-        end
-
-        def supported_orders
-          supported_filters(true)
-        end
-
+      def supported_orders
+        supported_filters(true)
+      end
     end
-  end # module StaffAgency
+  end
 end
