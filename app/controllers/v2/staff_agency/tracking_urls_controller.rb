de86836cf6a7d4f53ac42b87000da4fa1b31648f
evanilukhin <evanilukhin@gmail.com>
2021-02-25 01:52:41 +0300

    GCV2-3586 API: Add branding profile for tracking url - select profiles for agencies

--- app/controllers/v2/staff_agency/tracking_urls_controller.rb
+++ app/controllers/v2/staff_agency/tracking_urls_controller.rb
@@ -23,13 +23,13 @@ module V2
         sub_agencies = agency.agencies.select(required_fields)
 
         result << if sub_agencies.any?
-                      sub_agencies_attr = sub_agencies.map{|el| el.attributes.merge("branding_url"=> el.branding_url)}
-                      agency_attr = agency.attributes.reverse_merge("agencies"=> sub_agencies_attr)
-                      agency_attr.merge("branding_url"=> agency.branding_url)
-                    else
-                      agency_attr = agency.attributes
-                      agency_attr.merge("branding_url"=> agency.branding_url)
-                    end
+                    sub_agencies_attr = sub_agencies.map{|el| el.attributes.merge("branding_profiles"=> el.branding_profiles.map{|bp| {id: bp.id, url: bp.url}})}
+                    agency_attr = agency.attributes.reverse_merge("agencies"=> sub_agencies_attr)
+                    agency_attr.merge("branding_profiles"=> agency.branding_profiles.map{|bp| {id: bp.id, url: bp.url}})
+                  else
+                    agency_attr = agency.attributes
+                    agency_attr.merge("branding_profiles"=> agency.branding_profiles.map{|bp| {id: bp.id, url: bp.url}})
+                  end
 
         render json: result.to_json
       end
