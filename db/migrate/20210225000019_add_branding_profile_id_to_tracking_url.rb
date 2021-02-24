6e3e3ae1c5f6a8d9c79061f965a5e16444d2bf34
evanilukhin <evanilukhin@gmail.com>
2021-02-25 00:57:35 +0300

    GCV2-3586 API: Add branding profile for tracking url - fix migration

--- db/migrate/20210225000019_add_branding_profile_id_to_tracking_url.rb
+++ db/migrate/20210225000019_add_branding_profile_id_to_tracking_url.rb
@@ -2,7 +2,7 @@ class AddBrandingProfileIdToTrackingUrl < ActiveRecord::Migration[5.2]
   def up
     add_column :tracking_urls, :branding_profile_id, :integer
 
-    TrackingUrl.each do |tracking_url|
+    TrackingUrl.all.each do |tracking_url|
       tracking_url.update(branding_profile_id: tracking_url&.agency&.branding_profiles&.first&.id)
     end
   end
