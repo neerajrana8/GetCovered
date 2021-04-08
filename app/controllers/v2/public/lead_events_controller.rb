2412bf477d8945cec848f9e9277c81557e8ed943
evanilukhin <evanilukhin@gmail.com>
2021-04-08 09:13:53 +0300

    GCV2-3708 Lead Tracking improvements

--- app/controllers/v2/public/lead_events_controller.rb
+++ app/controllers/v2/public/lead_events_controller.rb
@@ -1,22 +1,20 @@
-#require 'lib/klaviyo/redis_client'
+# require 'lib/klaviyo/redis_client'
 
 module V2
   module Public
     class LeadEventsController < PublicController
-
       include Leads::CreateMethods
 
       before_action :set_lead, only: :create
 
       def create
-          if @lead.errors.any?
-            render json: standard_error(:lead_creation_error, nil, @lead.errors.full_messages)
-          else
-            create_lead_event
-            render template: 'v2/shared/leads/full'
+        if @lead.errors.any?
+          render json: standard_error(:lead_creation_error, nil, @lead.errors.full_messages)
+        else
+          create_lead_event
+          render template: 'v2/shared/leads/full'
         end
       end
-
     end
   end
 end
