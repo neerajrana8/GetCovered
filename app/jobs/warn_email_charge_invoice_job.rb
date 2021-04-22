362241bddfacd6bf7d6cd34ec2512c8753c15340
Joshua Brinsfield <sapphous@gmail.com>
2021-04-22 18:27:35 -0400

    Fix ternary boi

--- app/jobs/warn_email_charge_invoice_job.rb
+++ app/jobs/warn_email_charge_invoice_job.rb
@@ -2,7 +2,7 @@ class WarnEmailChargeInvoiceJob < ApplicationJob
   queue_as :default
 
   def perform(invoice)
-    policy = invoice.invoiceable_type == 'Policy' ? invoice.invoiceable : invoice.invoiceable_type == 'PolicyQuote' ? invoice.invoiceable.policy
+    policy = invoice.invoiceable_type == 'Policy' ? invoice.invoiceable : invoice.invoiceable_type == 'PolicyQuote' ? invoice.invoiceable.policy : nil
     WarnUpcomingChargeMailer.send_warn_upcoming_invoice(invoice).deliver unless policy.nil?
   end
 end
