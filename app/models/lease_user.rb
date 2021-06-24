f88bad4511a93f59f95d81abfb01fdba262ec8be
evanilukhin <evanilukhin@gmail.com>
2021-06-24 12:39:14 +0300

    GCV2-3848 remove validation on Lease Creation from Users' address-attributes fields - change lease user primary validation

--- app/models/lease_user.rb
+++ app/models/lease_user.rb
@@ -6,32 +6,38 @@ class LeaseUser < ApplicationRecord
   # Callbacks
   before_create :set_first_as_primary
   after_create :set_account_user
+  after_save :check_default
   
   # Relationships
   belongs_to :lease
   belongs_to :user
     
   def related_records_list
-    return ['lease', 'user']  
+    %w[lease user]  
   end
   
   private
     
-    def set_first_as_primary
-      if lease.lease_users.count == 0
-        self.primary = true  
-      end  
-    end
+  def set_first_as_primary
+    self.primary = true if lease.lease_users.count == 0  
+  end
    
-    def set_account_user
-      acct = AccountUser.where(user_id: user.id, account_id: lease.account_id).take
-      if acct.nil?
-        AccountUser.create!(user: user, account: lease.account)
-      elsif acct.status != 'enabled'
-        acct.update(status: 'enabled')
-      else
-        # do nothing
-      end
+  def set_account_user
+    acct = AccountUser.where(user_id: user.id, account_id: lease.account_id).take
+    if acct.nil?
+      AccountUser.create!(user: user, account: lease.account)
+    elsif acct.status != 'enabled'
+      acct.update(status: 'enabled')
     end
+  end
 
+  def check_default
+    if profileable.branding_profiles.count > 1 && default?
+
+      profileable.branding_profiles
+                 .where(default: true)
+                 .where.not(id: id)
+                 .update default: false
+    end
+  end
 end
