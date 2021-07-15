7055bb984003eedbf7209bc24a03045e107cc966
Joshua Brinsfield <sapphous@gmail.com>
2021-07-15 15:35:23 -0400

    Add MSI upgrade to migration

--- db/migrate/20210715045543_add_support_for_hidden_fees.rb
+++ db/migrate/20210715045543_add_support_for_hidden_fees.rb
@@ -1,8 +1,21 @@
 class AddSupportForHiddenFees < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :policy_premia, :total_hidden_fee, :integer, null: false, default: 0
     add_column :policy_premia, :total_hidden_tax, :integer, null: false, default: 0
     add_column :policy_premium_items, :hidden, :boolean, null: false, default: false
     add_column :line_items, :hidden, :boolean, null: false, default: false
+    
+    PolicyPremiumItem.where(title: "Policy Fee").each do |ppi|
+      ppi.policy_premium.update_columns(total_hidden_fee: ppi.original_total_due)
+      ppi.line_items.update_all(hidden: true)
+      ppi.update_columns(hidden: true)
+    end
+  end
+  
+  def down
+    remove_column :line_items, :hidden
+    remove_column :policy_premium_items, :hidden
+    remove_column :policy_premia, :total_hidden_tax
+    remove_column :policy_premia, :total_hidden_fee
   end
 end
