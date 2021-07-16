32e53a949143f8390d5003f075440151e33a3ca1
Joshua Brinsfield <sapphous@gmail.com>
2021-07-16 17:09:05 -0400

    Change purchase mailer email

--- app/mailers/policies/purchase_mailer.rb
+++ app/mailers/policies/purchase_mailer.rb
@@ -5,7 +5,7 @@ module Policies
     before_action :set_variables
     before_action :set_address
 
-    default to: -> { "policysold@getcoveredllc.com" },
+    default to: -> { ENV["RAILS_ENV"] == "production" ? "policysold@getcovered.io" : "testing@getcovered.io" },
             from: -> { "purchase-notifier-#{ENV["RAILS_ENV"]}@getcoveredinsurance.com" }
 
     def get_covered
