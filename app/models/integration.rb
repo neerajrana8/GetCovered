3520ba361eeeac6ce171bc6d47d60441d6921584
Dylan Gaines <dylan@dylangaines.com>
2021-08-20 09:12:48 -0700

    continueing base model work, committing to switch computers

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -6,4 +6,11 @@ class Integration < ApplicationRecord
   has_many :integration_profiles
 
   enum provider: %w[yardi realpage engrain]
+
+  validate :one_provider_per_integratable
+
+  private
+  def one_provider_per_integratable
+    errors.add(:integratable, "#{ integratable.title } already has a #{ self.provider.titlecase } integration") if integratable.integrations.exists?(provider: self.provider)
+  end
 end
