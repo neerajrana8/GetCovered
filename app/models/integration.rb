4f3af9c5c4f0d481f8cf6df5deba8e46bee4335a
Dylan Gaines <dylan@dylangaines.com>
2021-08-20 08:33:56 -0700

    Setting up base models for integration & integration profile

--- app/models/integration.rb
+++ app/models/integration.rb
@@ -1,3 +1,9 @@
+# Integration Model
+# file: app/models/integration.rb
+
 class Integration < ApplicationRecord
   belongs_to :integratable, polymorphic: true
+  has_many :integration_profiles
+
+  enum provider: %w[yardi realpage engrain]
 end
