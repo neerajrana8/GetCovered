bfac9ae7da97ffc056be3fb2a910c61259902702
evanilukhin <evanilukhin@gmail.com>
2021-09-20 17:00:06 +0300

    GCV2-3962 - Create an internal Daily Partner Sales Report (email) - fix mailer

--- app/jobs/reports/daily_sales_send_emails_job.rb
+++ app/jobs/reports/daily_sales_send_emails_job.rb
@@ -4,7 +4,7 @@ module Reports
 
     def perform
       range_start = Time.zone.now
-      report_path = Reports::DailySalesAggregate.new(range_start: range_start).generate_csv
+      report_path = Reports::DailySalesAggregate.new(range_start: range_start).generate.generate_csv
       recipients = Agency.get_covered.staff.pluck(:email)
       DailySalesReportMailer.send_report(recipients, report_path, 'All partners', range_start.to_date.to_s)
     end
--- app/mailers/daily_sales_report_mailer.rb
+++ app/mailers/daily_sales_report_mailer.rb
@@ -4,6 +4,7 @@ class DailySalesReportMailer < ApplicationMailer
   def send_report(recipients, report_path, partner, date)
     @partner = partner
     @date = date
+    @branding_profile = BrandingProfile.global_default
 
     attachments["Daily Sales Report - #{partner} - Renters Insurance - #{date}.csv"] = open(report_path).read
 
