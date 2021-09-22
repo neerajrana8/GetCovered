bb321ba79fb46f3463903e68fae66f8fce1bcca0
Joshua Brinsfield <sapphous@gmail.com>
2021-09-22 16:28:59 -0500

    Fix stuff

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,19 @@
+
+# hideous monkey patch
+module ActiveRecord
+  module ConnectionAdapters
+    module DatabaseStatements
+      alias :old_trans :transaction
+    
+      def transaction(*largs, **kargs, &barg)
+        ApplicationRecord.increment_transaction_id if ActiveRecord::Base.connection.open_transactions == 0
+        old_trans(*largs, **kargs, &barg)
+      end
+    end
+  end
+end
+
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   
@@ -5,30 +21,46 @@ class ApplicationRecord < ActiveRecord::Base
   #  1) The AR bug where locking a record destroys its previous_changes dirty data, and
   #  2) The AR design oversight where previous_changes isn't cumulative within a transaction (and thus after_commit can't determine what was changed over the course of a transaction)
   
+  def self.transaction_id
+    connection.open_transactions == 0 ? nil : @gc_ar_base_correct_dirty_transaction_id
+  end
+  
+  def self.increment_transaction_id
+    @gc_ar_base_correct_dirty_mask ||= 2**64 - 1
+    @gc_ar_base_correct_dirty_transaction_id ||= 0
+    @gc_ar_base_correct_dirty_transaction_id = (@gc_ar_base_correct_dirty_transaction_id + 1) & @gc_ar_base_correct_dirty_mask
+  end
+  
   after_save :gc_ar_base_correct_dirty_for_transaction
   after_commit :gc_ar_base_correct_dirty_after_transaction
   after_rollback :gc_ar_base_correct_dirty_after_transaction
   
+  def gc_ar_base_correct_dirty_before_transaction
+    # MOOSE WARNING: if we are in a new transaction we want to push a new mbls hash onto the stack
+    if ActiveRecord::Base.transaction_id != @gc_ar_base_correct_dirty_tid
+      @gc_ar_base_correct_dirty_tid = ActiveRecord::Base.transaction_id
+      (@gc_ar_base_correct_dirty_mbls ||= []).push({})
+    end
+  end
+  
   def gc_ar_base_correct_dirty_for_transaction
-    puts "PRESERVING DIRTY INFO"
-    @gc_ar_base_correct_dirty_mbls ||= {}
     self.previous_changes.each do |field, changez|
       # correct field history in our internal moosehopper
-      if @gc_ar_base_correct_dirty_mbls.has_key?(field)
-        @gc_ar_base_correct_dirty_mbls[field][1] = changez[1]
+      if @gc_ar_base_correct_dirty_mbls.last.has_key?(field)
+        @gc_ar_base_correct_dirty_mbls.last[field][1] = changez[1]
       else
-        @gc_ar_base_correct_dirty_mbls[field] = [changez[0], changez[1]]
+        @gc_ar_base_correct_dirty_mbls.last[field] = [changez[0], changez[1]]
       end
     end
     gc_ar_base_correct_dirty_restoration_time
   end
   
   def gc_ar_base_correct_dirty_restoration_time
-    if @gc_ar_base_correct_dirty_mbls
+    if @gc_ar_base_correct_dirty_mbls && @gc_ar_base_correct_dirty_mbls.last
     
       # THIS IS A BETTER WAY OF DOING IT -- but setting @attributes this way doesn't solve our problem. We need to make a proper mutationtracker when mutations_before_last_save is a NullMutationTracker. And I don't know how yet. Workaround below.
       #@attributes = ActiveModel::AttributeSet.new({}) if self.send(:mutations_before_last_save).class == ActiveModel::NullMutationTracker
-      #@gc_ar_base_correct_dirty_mbls.each do |field, changez|
+      #@gc_ar_base_correct_dirty_mbls.last.each do |field, changez|
       #  if self.send(:mutations_before_last_save).send(:attributes)[field].instance_variable_get(:@original_attribute).nil?
       #    self.send(:mutations_before_last_save).send(:attributes)[field].instance_variable_set(:@original_attribute, self.send(:mutations_from_database).send(:attributes)[field].dup)
       #  end
@@ -38,7 +70,7 @@ class ApplicationRecord < ActiveRecord::Base
       #  self.send(:mutations_before_last_save).send(:attributes)[field].instance_variable_set(:@value, changez[1])
       #end
       
-      @gc_ar_base_correct_dirty_mbls.each do |field, changez|
+      @gc_ar_base_correct_dirty_mbls.last.each do |field, changez|
         if self.send(:mutations_from_database).send(:attributes)[field].instance_variable_get(:@original_attribute).nil?
           self.send(:mutations_from_database).send(:attributes)[field].instance_variable_set(:@original_attribute, self.send(:mutations_from_database).send(:attributes)[field].dup)
         end
@@ -48,19 +80,16 @@ class ApplicationRecord < ActiveRecord::Base
         self.send(:mutations_from_database).send(:attributes)[field].instance_variable_set(:@value, changez[1])
       end
       self.send(:changes_applied)
-      
     end
   end
   
   def gc_ar_base_correct_dirty_after_transaction
-    @gc_ar_base_correct_dirty_mbls = nil
+    @gc_ar_base_correct_dirty_mbls.pop
   end
   
-  def lock!(*largs, **kargs, &barg)
-    super(*largs, **kargs, &barg)
+  def lock!(*meth, **am, &phetamine)
+    super(*meth, **am, &phetamine)
     gc_ar_base_correct_dirty_restoration_time
   end
-  
-  
   
 end
