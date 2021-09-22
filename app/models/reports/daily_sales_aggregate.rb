ffcbcc75310b25ba5f9fe4b51b0dee5a82484548
evanilukhin <evanilukhin@gmail.com>
2021-09-22 21:37:28 +0300

    GCV2-3962 - Create an internal Daily Partner Sales Report (email) - update csv

--- app/models/reports/daily_sales_aggregate.rb
+++ app/models/reports/daily_sales_aggregate.rb
@@ -11,6 +11,7 @@ module Reports
         add_item_report(reportable)
       end
 
+      calculate_total_conversion_percentages
       self
     end
 
@@ -40,30 +41,29 @@ module Reports
           csv << [
             'TOTAL', '', '', '',
             *data['totals']['yesterday'].values_at(*daily_sales_columns[:without_formatting]),
-            *data['totals']['yesterday'].values_at(*daily_sales_columns[:percent]).map { |value| "%#{value}" },
-            *data['totals']['yesterday'].values_at(*daily_sales_columns[:money]).map { |value| "$#{value}" },
+            *data['totals']['yesterday'].values_at(*daily_sales_columns[:percent]).map { |value| "#{value}%" },
+            *data['totals']['yesterday'].values_at(*daily_sales_columns[:money]).map { |value| "$#{(value / 100.0).to_s(:delimited)}" },
             *data['totals']['prior_seven_days'].values_at(*daily_sales_columns[:without_formatting]),
-            *data['totals']['prior_seven_days'].values_at(*daily_sales_columns[:percent]).map { |value| "%#{value}" },
-            *data['totals']['prior_seven_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{value}" },
+            *data['totals']['prior_seven_days'].values_at(*daily_sales_columns[:percent]).map { |value| "#{value}%" },
+            *data['totals']['prior_seven_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{(value / 100.0).to_s(:delimited)}" },
             *data['totals']['prior_thirty_days'].values_at(*daily_sales_columns[:without_formatting]),
-            *data['totals']['prior_thirty_days'].values_at(*daily_sales_columns[:percent]).map { |value| "%#{value}" },
-            *data['totals']['prior_thirty_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{value}" }
-
+            *data['totals']['prior_thirty_days'].values_at(*daily_sales_columns[:percent]).map { |value| "#{value}%" },
+            *data['totals']['prior_thirty_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{(value / 100.0).to_s(:delimited)}" }
           ]
         end
 
         data['rows'].each do |row|
           csv << [
-            *row.values_at(*%w[title type parent_agency status]),
+            *row.values_at('title', 'type', 'parent_agency', 'status'),
             *row['data']['yesterday'].values_at(*daily_sales_columns[:without_formatting]),
-            *row['data']['yesterday'].values_at(*daily_sales_columns[:percent]).map { |value| "%#{value}" },
-            *row['data']['yesterday'].values_at(*daily_sales_columns[:money]).map { |value| "$#{value}" },
+            *row['data']['yesterday'].values_at(*daily_sales_columns[:percent]).map { |value| "#{value}%" },
+            *row['data']['yesterday'].values_at(*daily_sales_columns[:money]).map { |value| "$#{(value / 100.0).to_s(:delimited)}" },
             *row['data']['prior_seven_days'].values_at(*daily_sales_columns[:without_formatting]),
-            *row['data']['prior_seven_days'].values_at(*daily_sales_columns[:percent]).map { |value| "%#{value}" },
-            *row['data']['prior_seven_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{value}" },
+            *row['data']['prior_seven_days'].values_at(*daily_sales_columns[:percent]).map { |value| "#{value}%" },
+            *row['data']['prior_seven_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{(value / 100.0).to_s(:delimited)}" },
             *row['data']['prior_thirty_days'].values_at(*daily_sales_columns[:without_formatting]),
-            *row['data']['prior_thirty_days'].values_at(*daily_sales_columns[:percent]).map { |value| "%#{value}" },
-            *row['data']['prior_thirty_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{value}" }
+            *row['data']['prior_thirty_days'].values_at(*daily_sales_columns[:percent]).map { |value| "#{value}%" },
+            *row['data']['prior_thirty_days'].values_at(*daily_sales_columns[:money]).map { |value| "$#{(value / 100.0).to_s(:delimited)}" }
           ]
         end
       end
@@ -87,9 +87,13 @@ module Reports
     def daily_sales_columns
       {
         without_formatting: %w[site_visits total_visitors applications_started submissions leads conversions],
-        percent:            %w[conversions_percentage],
-        money:              %w[total_premiums partner_commissions get_covered_commissions]
+        percent: %w[conversions_percentage],
+        money: %w[total_premiums partner_commissions get_covered_commissions]
       }
+    end
+
+    def periods
+      %w[yesterday prior_seven_days prior_thirty_days]
     end
 
     def aggregate_report
@@ -101,21 +105,21 @@ module Reports
 
     def line_data(line_reportable)
       {
-        'yesterday'         =>
+        'yesterday' =>
           DailySales.find_by(
-            reportable:  line_reportable,
+            reportable: line_reportable,
             range_start: range_start.yesterday.all_day,
-            range_end:   range_start.yesterday.all_day
+            range_end: range_start.yesterday.all_day
           )&.data,
-        'prior_seven_days'  => DailySales.find_by(
-          reportable:  line_reportable,
+        'prior_seven_days' => DailySales.find_by(
+          reportable: line_reportable,
           range_start: (range_start - 7.days).all_day,
-          range_end:   range_start.yesterday.all_day
+          range_end: range_start.yesterday.all_day
         )&.data,
         'prior_thirty_days' => DailySales.find_by(
-          reportable:  line_reportable,
+          reportable: line_reportable,
           range_start: (range_start - 30.days).all_day,
-          range_end:   range_start.yesterday.all_day
+          range_end: range_start.yesterday.all_day
         )&.data
       }
     end
@@ -141,12 +145,12 @@ module Reports
       return if item_data.value?(nil) || (!item.enabled? && item_data.values.map(&:values).flatten.uniq == [0])
 
       data['rows'] << {
-        'title'         => item.title,
-        'type'          => (item.is_a?(Agency) && item.agency_id.present?) ? 'Sub-Agency' : item.class.to_s,
-        'id'            => item.id,
+        'title' => item.title,
+        'type' => item.is_a?(Agency) && item.agency_id.present? ? 'Sub-Agency' : item.class.to_s,
+        'id' => item.id,
         'parent_agency' => item.agency&.title,
-        'status'        => item.enabled? ? 'Active' : 'Inactive',
-        'data'          => item_data
+        'status' => item.enabled? ? 'Active' : 'Inactive',
+        'data' => item_data
       }
 
       update_totals(item_data) unless item.is_a?(Account)
@@ -155,12 +159,20 @@ module Reports
     def update_totals(line_data)
       line_data.each do |period, period_data|
         period_data.each do |key, value|
-          if key == 'conversions_percentage'
-            data['totals'][period][key] = ((data['totals'][period][key] + value) / 2.0).round(2)
-          else
-            data['totals'][period][key] += value
-          end
+          data['totals'][period][key] += value unless key == 'conversions_percentage'
         end
+      end
+    end
+
+    def calculate_total_conversion_percentages
+      periods.each do |period|
+        data['totals'][period]['conversions_percentage'] =
+          if data['totals'][period]['total_visitors'] != 0
+            (data['totals'][period]['conversions'].to_f / data['totals'][period]['total_visitors'].to_f * 100).
+              round(1)
+          else
+            0
+          end
       end
     end
 
@@ -168,44 +180,44 @@ module Reports
       self.duration = 'day'
       self.data     = {
         'totals' => {
-          'yesterday'         => {
-            'site_visits'             => 0,
-            'total_visitors'          => 0,
-            'applications_started'    => 0,
-            'submissions'             => 0,
-            'leads'                   => 0,
-            'conversions'             => 0,
-            'total_premiums'          => 0,
-            'partner_commissions'     => 0,
+          'yesterday' => {
+            'site_visits' => 0,
+            'total_visitors' => 0,
+            'applications_started' => 0,
+            'submissions' => 0,
+            'leads' => 0,
+            'conversions' => 0,
+            'total_premiums' => 0,
+            'partner_commissions' => 0,
             'get_covered_commissions' => 0,
-            'conversions_percentage'  => 0
+            'conversions_percentage' => 0
           },
-          'prior_seven_days'  => {
-            'site_visits'             => 0,
-            'total_visitors'          => 0,
-            'applications_started'    => 0,
-            'submissions'             => 0,
-            'leads'                   => 0,
-            'conversions'             => 0,
-            'total_premiums'          => 0,
-            'partner_commissions'     => 0,
+          'prior_seven_days' => {
+            'site_visits' => 0,
+            'total_visitors' => 0,
+            'applications_started' => 0,
+            'submissions' => 0,
+            'leads' => 0,
+            'conversions' => 0,
+            'total_premiums' => 0,
+            'partner_commissions' => 0,
             'get_covered_commissions' => 0,
-            'conversions_percentage'  => 0
+            'conversions_percentage' => 0
           },
           'prior_thirty_days' => {
-            'site_visits'             => 0,
-            'total_visitors'          => 0,
-            'applications_started'    => 0,
-            'submissions'             => 0,
-            'leads'                   => 0,
-            'conversions'             => 0,
-            'total_premiums'          => 0,
-            'partner_commissions'     => 0,
+            'site_visits' => 0,
+            'total_visitors' => 0,
+            'applications_started' => 0,
+            'submissions' => 0,
+            'leads' => 0,
+            'conversions' => 0,
+            'total_premiums' => 0,
+            'partner_commissions' => 0,
             'get_covered_commissions' => 0,
-            'conversions_percentage'  => 0
+            'conversions_percentage' => 0
           }
         },
-        'rows'   => []
+        'rows' => []
       }
     end
   end
