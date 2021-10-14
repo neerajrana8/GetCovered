c55a10906f45a8a2d7a1a06c126830c5831995f0
evanilukhin <evanilukhin@gmail.com>
2021-10-14 15:43:06 +0300

    GCV2-4180 BACKEND - Update a devise configuration to support remember me functional - fix header

--- app/controllers/concerns/sessions_methods.rb
+++ app/controllers/concerns/sessions_methods.rb
@@ -24,7 +24,7 @@ module SessionsMethods
         end
 
         # Begin of the modified fragment
-        lifespan = request.headers['remember-me'] == 'true' ? Devise.remember_for : DeviseTokenAuth.token_lifespan
+        lifespan = request.headers['HTTP_REMEMBER_ME'] == 'true' ? Devise.remember_for : DeviseTokenAuth.token_lifespan
         @token = @resource.create_token(lifespan: lifespan)
         # End of the modified fragment
 
