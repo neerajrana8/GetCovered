491301cf577de6d3e25e417d83233a3a562402a5
Joshua Brinsfield <sapphous@gmail.com>
2021-11-05 12:54:28 -0500

    Improve gsub

--- app/models/concerns/carrier_qbe_insurable.rb
+++ app/models/concerns/carrier_qbe_insurable.rb
@@ -909,8 +909,7 @@ module CarrierQbeInsurable
   # used for comparing county strings that may come from sources other than QBE
   def qbe_standardize_county_string(county_string)
     county_string.upcase.chomp(" COUNTY").chomp(" PARISH")
-                 .gsub("SAINT", "ST") # since we remove all non-alpha characters this works just as well as .gsub(/St\s|St\./, Hash.new("Saint").merge({ 'St ' => 'Saint '  }))
-                 .gsub("MOUNT", "MT")
+                 .gsub(/SAINT|STREET|ROAD|MOUNT/, { "SAINT" => "ST", "STREET" => "ST", "ROAD" => "RD", "MOUNT" => "MT" })
                  .gsub(/[^a-z]/i, '')
   end
 end
