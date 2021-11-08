900f5dc767c9ce03803c2dac6bf27cd2fca55ca8
Dylan Gaines <dylan@dylangaines.com>
2021-11-08 08:33:05 -0800

    Fixes for update method and more restrictions on which policies can be displayed in namespace

--- app/controllers/v2/staff_policy_support/policies_controller.rb
+++ app/controllers/v2/staff_policy_support/policies_controller.rb
@@ -18,6 +18,16 @@ module V2
       end
 
       def show; end
+
+      def update
+        if @policy.update(update_policy_attributes)
+          puts "Hooray!  God praise the Queen."
+        else
+          render json: @policy.errors.to_json,
+                 status: 422
+        end
+      end
+
       private
 
       def view_path
@@ -26,12 +36,15 @@ module V2
 
       def set_policy
         @policy = access_model(::Policy, params[:id])
+        unless @policy.policy_in_system == false && @policy.status == "EXTERNAL_UNVERIFIED"
+          render json: standard_error(error: "Invalid Selection", message: "This policy does not meet the criteria for review")
+        end
       end
 
       def set_substrate
         super
         if @substrate.nil?
-          @substrate = access_model(::Policy.where(policy_in_system: false))
+          @substrate = access_model(::Policy.where(policy_in_system: false, status: "EXTERNAL_UNVERIFIED"))
         elsif !params[:substrate_association_provided]
           @substrate = @substrate.policies
         end
@@ -51,6 +64,10 @@ module V2
 
           @substrate = @substrate.joins(:insurables).where(insurables: { id: insurable_units_ids })
         end
+      end
+
+      def update_policy_attributes
+
       end
 
     end
