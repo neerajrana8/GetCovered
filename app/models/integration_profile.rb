91bae9fadc228cdbd226d4a556f0aedc35774c10
Joshua Brinsfield <sapphous@gmail.com>
2021-12-09 16:22:57 -0600

    Fix convenience assocs

--- app/models/integration_profile.rb
+++ app/models/integration_profile.rb
@@ -5,9 +5,10 @@ class IntegrationProfile < ApplicationRecord
   belongs_to :integration
   belongs_to :profileable, polymorphic: true
   
-  # profileable expansions
-  belongs_to :insurable, optional: true, foreign_key: 'profileable_id', -> { where(integration_profiles: { profileable_type: 'Insurable' }) }
-  belongs_to :lease, optional: true, foreign_key: 'profileable_id', -> { where(integration_profiles: { profileable_type: 'Lease' }) }
-  belongs_to :policy, optional: true, foreign_key: 'profileable_id', -> { where(integration_profiles: { profileable_type: 'Policy' }) }
-  belongs_to :user, optional: true, foreign_key: 'profileable_id', -> { where(integration_profiles: { profileable_type: 'User' }) }
+  # profileable expansions for convenient joins
+  has_one :self_reference, class_name: 'IntegrationProfile', foreign_key: :id
+  has_one :insurable, through: :self_reference, source: :profileable, source_type: 'Insurable'
+  has_one :lease, through: :self_reference, source: :profileable, source_type: 'Lease'
+  has_one :policy, through: :self_reference, source: :profileable, source_type: 'Policy'
+  has_one :user, through: :self_reference, source: :profileable, source_type: 'User'
 end
