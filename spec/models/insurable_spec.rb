38c24ee839624d5099819a2c1bc16894e53987e7
Dylan Gaines <dylan@dylangaines.com>
2022-01-03 12:34:18 -0800

    Cleaning up elasticsearch spec issues

--- spec/models/insurable_spec.rb
+++ spec/models/insurable_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Insurable, elasticsearch: true, type:  :model do
-  it 'Insurable Test title should be indexed' do
-    insurable = FactoryBot.create(:insurable)
-    Insurable.__elasticsearch__.refresh_index!
-    expect(Insurable.search(insurable.title).records.length).to eq(1)
-  end
-
-  it 'Insurable Wrong Name should not be indexed' do
-    FactoryBot.create(:insurable)
-    Insurable.__elasticsearch__.refresh_index!
-    expect(Insurable.search('Wrong Name').records.length).to eq(0)
-  end
-
   it 'should belong to same Account if it has insurable parent' do
     account = FactoryBot.create(:account)
     insurable = FactoryBot.create(:insurable)
