1232339efaa09888ad1d9303066ab13dc4a74d83
Dylan Gaines <dylan@dylangaines.com>
2022-01-04 13:02:59 -0800

    Fixing agency model spec

--- spec/models/agency_spec.rb
+++ spec/models/agency_spec.rb
@@ -3,9 +3,9 @@
 RSpec.describe Agency, elasticsearch: true, type: :model do
   it 'should allow more then one branding profile' do
     agency = FactoryBot.create(:agency)
-    agency.branding_profiles.create(title: 'new', url: 'new_agency.com')
+    agency.branding_profiles.create(url: 'new_agency.com')
     expect(agency.branding_profiles.count).to eq(1)
-    new_branding = agency.branding_profiles.create(title: 'new2', url: 'new_agency2.com')
+    new_branding = agency.branding_profiles.create(url: 'new_agency2.com')
     expect(new_branding.persisted?).to eq(true)
     expect(agency.branding_profiles.count).to eq(2)
   end
