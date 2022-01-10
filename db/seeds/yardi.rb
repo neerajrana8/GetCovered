9309dcf2d5b369464084e60cb6b8ae2ee6dedb35
Joshua Brinsfield <sapphous@gmail.com>
2022-01-09 20:10:32 -0600

    Make some unrelated importer changes to support community names

--- db/seeds/yardi.rb
+++ db/seeds/yardi.rb
@@ -6,60 +6,86 @@ require 'socket'
 @account = Account.all.first
 
 
-=begin
-
-Integration.create!(
-  integratable: @account,
-  provider: 'yardi',
-  enabled: true,
-  credentials: {
-    'voyager' => {
-      'username' => 'getcovereddb',
-      'password' => '101912',
-      'database_server' => 'afqoml_itf70dev7',
-      'database_name' => 'afqoml_itf70dev7'
-    },
-    'billing' => {
-      'username' => 'getcoveredws',
-      'password' => '101912',
-      'database_server' => 'afqoml_itf70dev7',
-      'database_name' => 'afqoml_itf70dev7'
-    },
-    'urls' => {
-      'billing_and_payments' => 'https://www.yardipcv.com/8223tp7s7dev/Webservices/ItfResidentTransactions20.asmx',
-      'renters_insurance' => 'https://www.yardipcv.com/8223tp7s7dev/Webservices/ItfRentersinsurance.asmx',
-      'system_batch' => 'https://www.yardipcv.com/8223tp7s7dev/Webservices/ItfResidentTransactions20_SysBatch.asmx',
-      'common_data' => 'https://www.yardipcv.com/8223tp7s7dev/webservices/itfCommonData.asmx',
-      'resident_data' => 'https://www.yardipcv.com/8223tp7s7dev/webservices/itfresidentdata.asmx'
-    }
-  }
-)
-
-=end
-
-Integration.create!(
-  integratable: @account,
-  provider: 'yardi',
-  enabled: true,
-  credentials: {
-    'voyager' => {
-      'username' => 'getcoveredqa',
-      'password' => '101915',
-      'database_server' => 'afqoml_itf_70QA',
-      'database_name' => 'afqoml_itf_70QA'
-    },
-    'billing' => {
-      'username' => 'getcoveredqa',
-      'password' => '101915',
-      'database_server' => 'afqoml_itf_70QA',
-      'database_name' => 'afqoml_itf_70QA'
-    },
-    'urls' => {
-      'billing_and_payments' => 'https://www.yardipcv.com/8223tp7s7qa/Webservices/ItfResidentTransactions20.asmx',
-      'renters_insurance' => 'https://www.yardipcv.com/8223tp7s7qa/Webservices/ItfRentersinsurance.asmx',
-      'system_batch' => 'https://www.yardipcv.com/8223tp7s7qa/Webservices/ItfResidentTransactions20_SysBatch.asmx'
-    }
-  }
-)
+usage_mode = :orion # could also be :yardi or :qa
 
 
+
+case usage_mode
+  when :yardi
+    Integration.create!(
+      integratable: @account,
+      provider: 'yardi',
+      enabled: true,
+      credentials: {
+        'voyager' => {
+          'username' => 'getcovereddb',
+          'password' => '101912',
+          'database_server' => 'afqoml_itf70dev7',
+          'database_name' => 'afqoml_itf70dev7'
+        },
+        'billing' => {
+          'username' => 'getcoveredws',
+          'password' => '101912',
+          'database_server' => 'afqoml_itf70dev7',
+          'database_name' => 'afqoml_itf70dev7'
+        },
+        'urls' => {
+          'billing_and_payments' => 'https://www.yardipcv.com/8223tp7s7dev/Webservices/ItfResidentTransactions20.asmx',
+          'renters_insurance' => 'https://www.yardipcv.com/8223tp7s7dev/Webservices/ItfRentersinsurance.asmx',
+          'system_batch' => 'https://www.yardipcv.com/8223tp7s7dev/Webservices/ItfResidentTransactions20_SysBatch.asmx',
+          'common_data' => 'https://www.yardipcv.com/8223tp7s7dev/webservices/itfCommonData.asmx',
+          'resident_data' => 'https://www.yardipcv.com/8223tp7s7dev/webservices/itfresidentdata.asmx'
+        }
+      }
+    )
+  when :qa
+    Integration.create!(
+      integratable: @account,
+      provider: 'yardi',
+      enabled: true,
+      credentials: {
+        'voyager' => {
+          'username' => 'getcoveredqa',
+          'password' => '101915',
+          'database_server' => 'afqoml_itf_70QA',
+          'database_name' => 'afqoml_itf_70QA'
+        },
+        'billing' => {
+          'username' => 'getcoveredqa',
+          'password' => '101915',
+          'database_server' => 'afqoml_itf_70QA',
+          'database_name' => 'afqoml_itf_70QA'
+        },
+        'urls' => {
+          'billing_and_payments' => 'https://www.yardipcv.com/8223tp7s7qa/Webservices/ItfResidentTransactions20.asmx',
+          'renters_insurance' => 'https://www.yardipcv.com/8223tp7s7qa/Webservices/ItfRentersinsurance.asmx',
+          'system_batch' => 'https://www.yardipcv.com/8223tp7s7qa/Webservices/ItfResidentTransactions20_SysBatch.asmx'
+        }
+      }
+    )
+  when :orion
+    Integration.create!(
+      integratable: @account,
+      provider: 'yardi',
+      enabled: true,
+      credentials: {
+        'voyager' => {
+          'username' => 'getcovered',
+          'password' => '3094asjd!',
+          'database_server' => 'bfraobtpd_test',
+          'database_name' => 'bfraobtpd_test'
+        },
+        'billing' => {
+          'username' => 'getcovered',
+          'password' => '3094asjd!',
+          'database_server' => 'bfraobtpd_test',
+          'database_name' => 'bfraobtpd_test'
+        },
+        'urls' => {
+          'billing_and_payments' => 'https://www.yardiasptx10.com/02667regency/Webservices/ItfResidentTransactions20.asmx',
+          'renters_insurance' => 'https://www.yardiasptx10.com/02667regency/Webservices/ItfRentersinsurance.asmx',
+          'system_batch' => 'https://www.yardiasptx10.com/02667regency/Webservices/ItfResidentTransactions20_SysBatch.asmx'
+        }
+      }
+    )
+end
