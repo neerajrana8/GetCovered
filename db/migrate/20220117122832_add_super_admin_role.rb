5004ca72bc1a5d5dd7dde0f7758f334c7a41a047
Neeraj Rana <neeraj.rana@carmatec.in>
2022-01-17 18:05:41 +0530

    Added migration for super admin

---
+++ db/migrate/20220117122832_add_super_admin_role.rb
@@ -0,0 +1,11 @@
+class AddSuperAdminRole < ActiveRecord::Migration[6.1]
+  def change
+    staffs = Staff.where(role: 'super_admin')
+    staffs.each do |staff|
+      if staff.staff_roles.where(role: 'super_admin').count === 0
+        staff.staff_roles.update(primary: false)
+        staff.create_staff_role(role: 'super_admin', primary: true)
+      end
+    end
+  end
+end
