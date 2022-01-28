e0729a450da7d2391e5dedd1cb795bd8b72ebb7b
Neeraj Rana <neeraj.rana@carmatec.in>
2022-01-28 19:19:51 +0530

    Changed units endpoint to include units only from a Community or a Building

--- app/controllers/v2/public/units_controller.rb
+++ app/controllers/v2/public/units_controller.rb
@@ -2,8 +2,9 @@ module V2
   module Public
     class UnitsController < PublicController
       def communities
-        community = Insurable.find(params[:id])
-        @account_units = community.insurables.units
+        # Fetch Community or Building
+        community = Insurable.where(id: params[:id], insurable_type_id: InsurableType::COMMUNITIES_IDS + InsurableType::BUILDINGS_IDS).take
+        @account_units = community.units
         render '/v2/staff_super_admin/accounts/account_units', status: :ok
       end
     end
