884c4a95a78dba03392ee6459fcee455cb7f0947
Neeraj Rana <neeraj.rana@carmatec.in>
2022-01-31 19:58:44 +0530

    Recording staff reports mailer

--- app/mailers/staff_reports_mailer.rb
+++ app/mailers/staff_reports_mailer.rb
@@ -1,10 +1,24 @@
 class StaffReportsMailer < ApplicationMailer
   before_action { @notifiable = params[:notifiable] }
   before_action { @report_path = params[:report_path] }
+  after_action :record_mail
 
   def daily_purchase_activity
     attachments["DailyPurchaseActivity.csv"] = open(@report_path).read
 
     mail(:subject => "Daily Purchase Activity Report")
+  end
+
+  private
+
+  def record_mail
+    contact_record = ContactRecord.new(
+      approach: 'email',
+      direction: 'outgoing',
+      status: 'sent',
+      contactable: @notifiable
+    )
+
+    contact_record.save
   end
 end
