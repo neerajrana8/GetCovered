870d109179ee39ff559b782f63cb2fd76c7359b4
Neeraj Rana <neeraj.rana@carmatec.in>
2022-01-31 19:50:00 +0530

    Recording master policies mailer

--- app/mailers/master_policies_mailer.rb
+++ app/mailers/master_policies_mailer.rb
@@ -5,7 +5,22 @@ class MasterPoliciesMailer < ApplicationMailer
     @invoice = params[:invoice]
   end
 
+  after_action :record_mail
+
   def bill_master_policy
     mail(from: 'support@getcoveredinsurance.com', to: @staff.email, subject: "Master Policy #{@master_policy.number} Invoice")
+  end
+
+  private
+
+  def record_mail
+    contact_record = ContactRecord.new(
+      approach: 'email',
+      direction: 'outgoing',
+      status: 'sent',
+      contactable: @staff
+    )
+
+    contact_record.save
   end
 end
