abc088f9c5f400687c4efdfae84e678deec5c30a
Joshua Brinsfield <sapphous@gmail.com>
2022-01-31 21:03:23 -0600

    Prevent 500 error on old effective date for MSI; improve fetch rates job for qbe

--- app/jobs/fetch_qbe_rates_job.rb
+++ app/jobs/fetch_qbe_rates_job.rb
@@ -5,6 +5,7 @@ class FetchQbeRatesJob < ApplicationJob
   def perform(target, number_insured: [1,2,3,4,5], effective_date: nil, traits_override: {}, delay: nil, igc: :unset)
     # make target point at an insurable, make igc point at an igc or nil, flee if invalid target was provided
     if igc == :unset # when recursing we get to skip these time-wasting checks becuase we will always explicitly specify igc as nil or as an IGC
+      # figure out issues with parameters
       number_insured = [number_insured] unless number_insured.class == ::Array
       effective_date = Time.current + 1.day if effective_date.nil?
       igc = nil
@@ -17,6 +18,14 @@ class FetchQbeRatesJob < ApplicationJob
         return
       end
       return if !::InsurableType::RESIDENTIAL_COMMUNITIES_IDS.include?(target.insurable_type_id)
+      # perform other steps first if needed
+      cip = target.carrier_profile(QbeService.carrier_id)
+      unless cip
+        community.create_carrier_profile(QbeService.carrier_id)
+        cip = community.carrier_profile(QbeService.carrier_id)
+      end
+      return unless cip.data['county_resolved'] || (target.get_qbe_zip_code && cip.reload.data['county_resolved'])
+      return unless cip.data['property_info_resolved'] || (target.get_qbe_property_info && target.reload.data['property_info_resolved'])
     end
     # get the rates
     target.get_qbe_rates(number_insured.first, traits_override: traits_override, irc_configurable_override: igc)
