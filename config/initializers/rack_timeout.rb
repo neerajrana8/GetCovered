8325403a2ea457aa5863c473da30d1c723d6e554
Joshua Brinsfield <sapphous@gmail.com>
2022-02-05 18:28:07 -0600

    Extend rack timeout

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,1 +1,1 @@
-Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 90
+Rails.application.config.middleware.insert_before Rack::Runtime, Rack::Timeout, service_timeout: 90, wait_timeout: 90
