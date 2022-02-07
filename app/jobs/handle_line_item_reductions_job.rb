8f1e7eb7d347ba979b96587c09911a247abdbb05
Joshua Brinsfield <sapphous@gmail.com>
2022-02-07 15:40:12 -0600

    Add delay to keep LIRs on the same Refund when possible

---
+++ app/jobs/handle_line_item_reductions_job.rb
@@ -0,0 +1,16 @@
+class HandleLineItemReductionsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args, invoice_id: nil, invoice_ids: nil)
+    invoice_ids = if !invoice_id.nil?
+      [invoice_id]
+    else
+      LineItemReduction.references(:line_items).includes(:line_item).pending.order("line_items.invoice_id ASC").group("line_items.invoice_id").pluck("line_items.invoice_id")
+    end if invoice_ids.nil?
+    
+    @invoice_ids.each do |invid|
+      ::Invoice.where(id: invid).take&.process_reductions
+    end
+  end
+  
+end
