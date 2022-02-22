b17cffe0ada5719e7882534f4cf9e75d84ad860d
Neeraj Rana <neeraj.rana@carmatec.in>
2022-02-22 15:39:45 +0530

    Added address attribute to public APIs

--- app/controllers/v2/public/buildings_controller.rb
+++ app/controllers/v2/public/buildings_controller.rb
@@ -2,9 +2,10 @@ module V2
   module Public
     class BuildingsController < PublicController
       def community
-        community = Insurable.where(id: params[:id], insurable_type_id: InsurableType::BUILDINGS_IDS).first
-        @buildings = []
-        @buildings = community.buildings if community
+        community = Insurable.where(id: params[:id], insurable_type_id: InsurableType::COMMUNITIES_IDS).first
+        @account_communities = []
+        @account_communities = community.buildings if community
+        render '/v2/public/communities/accounts', status: :ok
       end
     end
   end
--- app/controllers/v2/public/units_controller.rb
+++ app/controllers/v2/public/units_controller.rb
@@ -4,8 +4,8 @@ module V2
       def communities
         # Fetch Community or Building
         community = Insurable.where(id: params[:id], insurable_type_id: InsurableType::COMMUNITIES_IDS + InsurableType::BUILDINGS_IDS).take
-        @account_units = community.units
-        render '/v2/staff_super_admin/accounts/account_units', status: :ok
+        @account_communities = community.units
+        render '/v2/public/communities/accounts', status: :ok
       end
     end
   end
