1c417fca9cf66b0f8bc9d8ac2aebf416db7fa307
Dylan Gaines <dylan@dylangaines.com>
2022-02-24 10:22:33 -0800

    Adding additional interest and preferred to insurable and additional interest to account

---
+++ db/migrate/20220224175744_add_preferred_and_additional_interest_to_insurable.rb
@@ -0,0 +1,16 @@
+class AddPreferredAndAdditionalInterestToInsurable < ActiveRecord::Migration[6.1]
+  def change
+    add_column :insurables, :preferred, :jsonb, default: {}
+    add_column :insurables, :additional_interest, :boolean, default: false
+
+    Insurable.reset_column_information
+
+    Insurable.find_each do |insurable|
+      preferred_hash = {}
+      [1, 5].each do |carrier_id|
+        preferred_hash[carrier_id] = insurable.get_carrier_status(carrier_id) == :preferred ? true : false
+      end
+      insurable.update preferred: preferred_hash
+    end
+  end
+end
