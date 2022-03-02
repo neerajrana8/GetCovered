a1d9e74c9ddc5338ff94b369e56c0eb687a62add
hanna-bautsiuk <hannabts@nitka.com>
2022-03-02 18:41:23 +0100

    GCV2-3990: update missed field

--- app/mailers/pm_tenant_portal/invitation_to_pm_tenant_portal_mailer.rb
+++ app/mailers/pm_tenant_portal/invitation_to_pm_tenant_portal_mailer.rb
@@ -16,6 +16,7 @@ module PmTenantPortal
       #TODO: need to remove after test
       @master_policy = @community.policies.where(policy_type_id: 2).take || Policy.last
 
+      set_liabilities(@community)
       #@agency = master_policy.agency
 
       @from = @pm_account.contact_info["contact_email"]
@@ -33,6 +34,8 @@ module PmTenantPortal
       @user = user
       @community = community
       @tenant_onboarding_url = tenant_onboarding_url
+
+      set_liabilities(@community)
 
       @pm_account = @community.account
       #TODO: need to remove after test
@@ -123,6 +126,24 @@ module PmTenantPortal
       str_to_encrypt = "user #{user_id} community #{community.id}" #user 1443 community 10035
       auth_token_for_email = EncryptionService.encrypt(str_to_encrypt)
       "https://#{branding_profile_url}/pma-tenant-onboarding?token=#{auth_token_for_email}"
+    end
+
+    def set_liabilities(insurable)
+      account = insurable.account
+      carrier_id = account.agency.providing_carrier_id(PolicyType::RESIDENTIAL_ID, insurable){|cid| (insurable.get_carrier_status(carrier_id) == :preferred) ? true : nil }
+      carrier_policy_type = CarrierPolicyType.where(carrier_id: carrier_id, policy_type_id: PolicyType::RESIDENTIAL_ID).take
+      uid = (carrier_id == ::MsiService.carrier_id ? '1005' : carrier_id == ::QbeService.carrier_id ? 'liability' : nil)
+      liability_options = ::InsurableRateConfiguration.get_inherited_irc(carrier_policy_type, account, insurable).configuration['coverage_options']&.[](uid)&.[]('options')
+      @max_liability = liability_options&.map{|opt| opt['value'].to_i }&.max
+      @min_liability = liability_options&.map{|opt| opt['value'].to_i }&.min
+
+      if @min_liability.nil? || @min_liability == 0 || @min_liability == "null"
+        @min_liability = 1000000
+      end
+
+      if @max_liability.nil? || @max_liability == 0 || @max_liability == "null"
+        @max_liability = 30000000
+      end
     end
   end
 end
