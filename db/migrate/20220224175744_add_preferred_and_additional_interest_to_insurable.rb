78b56f7c5ccf070862b9ddcacda886906c13308b
Dylan Gaines <dylan@dylangaines.com>
2022-03-03 13:12:09 -0800

    removing resource heavy insurable loop from migration

--- db/migrate/20220224175744_add_preferred_and_additional_interest_to_insurable.rb
+++ db/migrate/20220224175744_add_preferred_and_additional_interest_to_insurable.rb
@@ -3,14 +3,14 @@ class AddPreferredAndAdditionalInterestToInsurable < ActiveRecord::Migration[6.1
     add_column :insurables, :preferred, :jsonb, default: {}
     add_column :insurables, :additional_interest, :boolean, default: false
 
-    Insurable.reset_column_information
-
-    Insurable.find_each do |insurable|
-      preferred_hash = {}
-      [1, 5].each do |carrier_id|
-        preferred_hash[carrier_id] = insurable.get_carrier_status(carrier_id) == :preferred ? true : false
-      end
-      insurable.update preferred: preferred_hash
-    end
+    #    Insurable.reset_column_information
+    #
+    #    Insurable.find_each do |insurable|
+    #      preferred_hash = {}
+    #      [1, 5].each do |carrier_id|
+    #        preferred_hash[carrier_id] = insurable.get_carrier_status(carrier_id) == :preferred ? true : false
+    #      end
+    #      insurable.update preferred: preferred_hash
+    #    end
   end
 end
