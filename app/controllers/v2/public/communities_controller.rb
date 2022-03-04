07b9cd9bf47a0cf0b6f481ac3990fa76e787d1d1
hanna-bautsiuk <hannabts@nitka.com>
2022-03-04 10:58:04 +0100

    GCV2-4529: sort communities by account

--- app/controllers/v2/public/communities_controller.rb
+++ app/controllers/v2/public/communities_controller.rb
@@ -11,10 +11,31 @@ module V2
           render json: standard_error(:state_param_blank,'State parameter can\'t be blank'),
                  status: :unprocessable_entity
         else
-          @communities = Insurable.communities.joins(:addresses).where(addresses: { state: params[:state] } )
+          if params[:branding_profile_id].blank?
+            render json: standard_error(:branding_profile_id_param_blank,'branding_profile_id parameter can\'t be blank'),
+                   status: :unprocessable_entity
+          else
+            account_id = BrandingProfile.find_by_id(params[:branding_profile_id])&.profileable_id
+            account = Account.find_by_id(account_id)
+
+            if account.blank?
+              render json: standard_error(:account_not_found,'Account for this branding_id not found'),
+                     status: :unprocessable_entity
+            else
+              begin
+                @communities = Insurable.communities.where(account_id: account.id)
+                                   .joins(:addresses).where(addresses: { state: params[:state] } )
+              rescue StandardError => e
+                render json: standard_error(:query_failed,"#{e.message}"),
+                       status: :unprocessable_entity
+              end
+            end
+
+          end
         end
       end
 
+      #TODO: think about validations with dry components
       def account_states
         if params[:branding_profile_id].blank?
           render json: standard_error(:branding_profile_id_param_blank,'branding_profile_id parameter can\'t be blank'),
