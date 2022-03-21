f8313f91c38e7489215be03e821607266074b9f2
Dylan Gaines <dylan@dylangaines.com>
2022-03-21 10:40:44 -0700

    One more tweak to only send bind warnings to new address in production

--- app/jobs/policy_bind_warning_notification_job.rb
+++ app/jobs/policy_bind_warning_notification_job.rb
@@ -3,7 +3,7 @@ class PolicyBindWarningNotificationJob < ApplicationJob
 
   def perform(message: )
     return if message.nil?
-    emails = ['bindwarning@getcovered.io']
-	  ActionMailer::Base.mail(from: 'no-reply@getcoveredinsurance.com', to: emails, subject: I18n.t('policy_bind_warning_notification_job.get_covered_bind_warning'), body: message).deliver
+    notification_email = ENV["RAILS_ENV"] == "production" ? "bindwarning@getcovered.io" : "dev@getcoveredllc.com"
+	  ActionMailer::Base.mail(from: 'no-reply@getcoveredinsurance.com', to: notification_email, subject: I18n.t('policy_bind_warning_notification_job.get_covered_bind_warning'), body: message).deliver
   end
 end
