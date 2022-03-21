44f5330a649f7187838d8c55c61f2bd98a0918e4
Dylan Gaines <dylan@dylangaines.com>
2022-03-21 10:37:17 -0700

    Correcting bind warning notification email address

--- app/jobs/policy_bind_warning_notification_job.rb
+++ app/jobs/policy_bind_warning_notification_job.rb
@@ -3,7 +3,7 @@ class PolicyBindWarningNotificationJob < ApplicationJob
 
   def perform(message: )
     return if message.nil?
-    emails = ['dev@getcoveredllc.com'] #, 'QBE-FPS-Production-Support.US-BOX@us.qbe.com', 'US-QBE-Renters-Support-Team@us.qbe.com']
+    emails = ['bindwarning@getcovered.io']
 	  ActionMailer::Base.mail(from: 'no-reply@getcoveredinsurance.com', to: emails, subject: I18n.t('policy_bind_warning_notification_job.get_covered_bind_warning'), body: message).deliver
   end
 end
