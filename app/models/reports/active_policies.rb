f15ccea6f40180726d8be933622a7dcd3da296b4
Dylan Gaines <dylan@dylangaines.com>
2022-04-04 14:17:39 -0700

    Disabling all but 2 reporting jobs and adding coverage_contents and coverage_liability to active policies report

--- app/models/reports/active_policies.rb
+++ app/models/reports/active_policies.rb
@@ -15,6 +15,8 @@ module Reports
           'property_phone' => nil,
           'policy_type' => 'H04',
           'policy_number' => policy.number,
+          'coverage_contents' => policy.policy_coverages.exists?(designation: 'coverage_c') ? ActionController::Base.helpers.number_to_currency policy.policy_coverages.where(designation: 'coverage_c').take.limit.to_f / 100 : "N/A",
+          'coverage_liability' => policy.policy_coverages.exists?(designation: 'liability') ? ActionController::Base.helpers.number_to_currency policy.policy_coverages.where(designation: 'liability').take.limit.to_f / 100 : "N/A",
           'primary_insured_name' => primary_policy_user&.full_name,
           'primary_insured_phone' => primary_policy_user&.contact_phone,
           'primary_insured_email' => primary_policy_user&.contact_email,
@@ -36,6 +38,8 @@ module Reports
         'property_phone' => 'Property Phone',
         'policy_type' => 'Policy Type',
         'policy_number' => 'Policy Number',
+        'coverage_contents' => 'Contents Coverage',
+        'coverage_liability' => 'Liability Coverage',
         'primary_insured_name' => 'Primary Insured Name',
         'primary_insured_phone' => 'Primary Insured Phone',
         'primary_insured_email' => 'Primary Insured Email',
@@ -48,8 +52,8 @@ module Reports
 
     def headers
       %w[management_company property_name property_city property_state property_phone policy_type
-         policy_number primary_insured_name primary_insured_phone primary_insured_email
-         primary_insurance_location effective_date expiration_date finished_date]
+         policy_number coverage_contents coverage_liability primary_insured_name primary_insured_phone
+         primary_insured_email primary_insurance_location effective_date expiration_date finished_date]
     end
 
     private
