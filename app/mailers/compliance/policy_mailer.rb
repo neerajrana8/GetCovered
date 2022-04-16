218d857ae1997344e2e0fa610371d230e617dad5
Dylan Gaines <dylan@dylangaines.com>
2022-04-16 16:03:00 -0700

    More mailer fixes

--- app/mailers/compliance/policy_mailer.rb
+++ app/mailers/compliance/policy_mailer.rb
@@ -11,13 +11,12 @@ module Compliance
                   Your insurance policy on file with us is set to expire on #{ policy.expiration_date.strftime('%B %d, %Y') }.
                   Please submit your new insurance policy or renewal documents before the expiration date.Thank you!"
 
-      from = @pm_account&.contact_info&.has_key?("contact_email") &&
-             !@pm_account&.contact_info["contact_email"].nil? ? @pm_account&.contact_info["contact_email"] :
-                                                                "policyverify@getcovered.io"
+      @from = @pm_account&.contact_info&.has_key?("contact_email") && !@pm_account&.contact_info["contact_email"].nil? ? @pm_account&.contact_info["contact_email"] : "policyverify@getcovered.io"
+
       subject = "Your insurance will expire soon!"
 
       mail(to: @user.email,
-           from: from,
+           from: @from,
            subject: subject,
            template_path: 'compliance/policy',
            template_name: 'text_only')
@@ -26,25 +25,23 @@ module Compliance
     def policy_lapsed(policy:)
       @user = policy.primary_user()
       @pm_account = policy.account
-      @content = "Hi #{ @user.profile.first_name },<br><br><strong>Your policy has lapsed or is canceled.</strong>  You
-                  are out of compliance with your lease agreement and subject to being enrolled into the default community
-                  policy. Please reinstate your insurance policy or upload your new policy information
-                  <a href=\"https://#{ @branding_profile&.url }/pma-tenant-onboarding\">on our portal</a> for review."
+      @policy = policy
+      @community = @policy&.primary_insurable&.parent_community
+      @from = @pm_account&.contact_info&.has_key?("contact_email") && !@pm_account&.contact_info["contact_email"].nil? ? @pm_account&.contact_info["contact_email"] : "policyverify@getcovered.io"
 
-      from = @pm_account&.contact_info&.has_key?("contact_email") &&
-             !@pm_account&.contact_info["contact_email"].nil? ? @pm_account&.contact_info["contact_email"] :
-                                                                "policyverify@getcovered.io"
+      set_master_policy_and_configuration(@community, 2)
 
-      subject = "You are out of compliance"
       mail(to: @user.email,
-           from: from,
-           subject: subject,
-           template_path: 'compliance/policy',
-           template_name: 'text_only')
+           from: @from,
+           subject: "You are out of compliance")
     end
 
     def enrolled_in_master(user:, community:, force:)
       set_master_policy_and_configuration(community, 2)
+
+      @user = policy.primary_user()
+      @pm_account = policy.account
+      @policy = policy
 
       liability_coverage = @master_policy.policy_coverages.where(designation: "liability_coverage").take
       contents_coverage = @master_policy.policy_coverages.where(designation: "tenant_contingent_contents").take
@@ -56,9 +53,7 @@ module Compliance
                   liability coverage and #{ ActionController::Base.helpers.number_to_currency contents_coverage.limit.to_f / 100 } in contents coverage
                   <strong>(contents if applicable)</strong>."
 
-      from = @master_policy&.account&.contact_info&.has_key?("contact_email") &&
-             !@master_policy&.account&.contact_info["contact_email"].nil? ? @master_policy&.account&.contact_info["contact_email"] :
-                                                                            "policyverify@getcovered.io"
+      @from = @pm_account&.contact_info&.has_key?("contact_email") && !@pm_account&.contact_info["contact_email"].nil? ? @pm_account&.contact_info["contact_email"] : "policyverify@getcovered.io"
 
       subject = "Default Policy Enrollment"
       mail(to: user.email,
