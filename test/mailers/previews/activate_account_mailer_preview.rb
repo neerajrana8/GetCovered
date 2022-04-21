155251b4316cc975568d864660ba12a458f835e8
hanna-bautsiuk <hannabts@nitka.com>
2022-04-21 16:35:32 +0200

    GCVR2-32: add locales to preview

--- test/mailers/previews/activate_account_mailer_preview.rb
+++ test/mailers/previews/activate_account_mailer_preview.rb
@@ -1,18 +1,25 @@
 class ActivateAccountMailerPreview < ActionMailer::Preview
   def third_party_insurance
     policy = Policy.where(policy_type_id: 1, carrier_id: 1).last
-    #policy = Policy.where(status: "EXTERNAL_VERIFIED").firs
+    user_language = policy.primary_user.profile&.language
+    policy.primary_user.profile&.update(language: params[:locale] || user_language)
+
     ActivateAccountMailer.third_party_insurance(policy: policy, user: policy.primary_user)
   end
 
   def master_policy_enrollment
     policy = Policy.where(policy_type_id: 1, carrier_id: 1).last
-    #policy = Policy.where(status: "EXTERNAL_VERIFIED").firs
+    user_language = policy.primary_user.profile&.language
+    policy.primary_user.profile&.update(language: params[:locale] || user_language)
+
     ActivateAccountMailer.master_policy_enrollment(policy: policy, user: policy.primary_user)
   end
 
   def renters_insurance_policy_purchase
     policy = Policy.where(policy_type_id: 1, carrier_id: 1).last
+    user_language = policy.primary_user.profile&.language
+    policy.primary_user.profile&.update(language: params[:locale] || user_language)
+
     ActivateAccountMailer.renters_insurance_policy_purchase(policy: policy, user: policy.primary_user)
   end
 end
