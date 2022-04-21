396e4a326834545a12fbec690145525a2c477620
Joshua Brinsfield <sapphous@gmail.com>
2022-04-21 18:26:53 -0500

    Add parentheses for scoping

--- app/controllers/v2/public/accounts_controller.rb
+++ app/controllers/v2/public/accounts_controller.rb
@@ -1,6 +1,3 @@
-##
-# V2 StaffAccount Accounts Controller
-# File: app/controllers/v2/staff_account/accounts_controller.rb
 
 module V2
   module Public
@@ -9,13 +6,13 @@ module V2
       def agency_accounts
         @accounts = Account.where(agency_id: params[:agency_id].to_i).order(:title)
         
-        render json: @accounts.map do |account|
+        render json: (@accounts.map do |account|
           {
             id: account.id,
             title: account.title,
             address: [:street_number, :street_name, :street_two, :city, :state, :zip_code, :full].inject({}){|hash, prop| hash[prop] = account.primary_address&.send(prop); hash }
           }
-        end, status: 200
+        end), status: 200
         
       end
 
