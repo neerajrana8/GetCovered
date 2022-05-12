821eb01d6488c9fba7d279af7f5d808a6c848efe
Neeraj Rana <neeraj.rana@carmatec.in>
2022-05-12 13:13:53 +0530

    accounts staff roles restriction

--- app/models/global_agency_permissions/available_permissions.rb
+++ app/models/global_agency_permissions/available_permissions.rb
@@ -23,5 +23,30 @@ module GlobalAgencyPermissions
       'insurables.buildings' => true,
       'insurables.units' => true
     }.freeze
+
+
+    RESTRICT_ACCOUNT_PERMISSIONS = {
+      'dashboard.leads' => true,
+      'dashboard.properties' => true,
+      'policies.policies' => true,
+      'policies.master' => true,
+      'policies.rent_mass_import' => true,
+      'policies.claims' => true,
+      'policies.coverage_proof' => true,
+      'requests.refunds' => true,
+      'requests.cancellations' => true,
+      'property_management.accounts' => true,
+      'property_management.managers' => true,
+      'property_management.users' => true,
+      'agencies.details' => true,
+      'agencies.agents' => true,
+      'agencies.manage_agents' => true,
+      'agencies.carriers' => true,
+      'insurables.create' => true,
+      'insurables.communities' => true,
+      'insurables.buildings' => true,
+      'insurables.units' => true,
+      'leases.leases' => true
+    }.freeze
   end
 end
