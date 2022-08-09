3688b0c498cb9689931ccfbbdb32dd37e8c1b330
dmr <noroot@falsetrue.io>
2022-08-10 01:07:49 +0300

    #GCVR2-700 Refactoring endpoint for leads

--- app/controllers/v2/staff_super_admin/leads_controller.rb
+++ app/controllers/v2/staff_super_admin/leads_controller.rb
@@ -7,6 +7,70 @@ module V2
       before_action :set_lead, only: %i[update show]
       before_action :set_substrate, only: :index
 
+      def index
+        filter = params[:filter]
+
+        date_utc_format = '%Y-%m-%d %H:%M:%S'
+
+        date_from_dt = DateTime.now - 1.month
+        date_to_dt = DateTime.now
+
+        if filter[:last_visit]
+          date_from_dt = Time.zone.parse(filter[:last_visit][:start]) unless filter[:last_visit][:start].nil?
+          date_to_dt = Time.zone.parse(filter[:last_visit][:end]) unless filter[:last_visit][:end].nil?
+        end
+
+        date_from = date_from_dt.utc.strftime(date_utc_format)
+        date_to = date_to_dt.end_of_day.utc.strftime(date_utc_format)
+
+        leads = Lead.all.includes(
+          :agency,
+          :profile,
+          :tracking_url,
+          :branding_profile,
+          user: :policy_quotes
+        )
+        leads = leads.by_agency(filter[:agency_id]) unless filter[:agency_id].nil?
+        leads = leads.by_account(filter[:account_id]) unless filter[:account_id].nil?
+        leads = leads.by_branding_profile(filter[:branding_profile_id]) unless filter[:branding_profile_id].nil?
+        leads = leads.archived if filter[:archived] == true
+        leads = leads.not_converted
+        leads = leads.by_last_visit(date_from, date_to)
+
+        policy_type_id = filter[:lead_events][:policy_type_id] if filter[:lead_events]
+
+        leads = leads.join_last_events(policy_type_id)
+
+        # Tracking urls and parameters filtering
+
+        if filter[:tracking_url].present?
+          tracking_urls = TrackingUrl.all
+          filter[:tracking_url].each do |v|
+            tracking_urls = tracking_urls.where("#{v.first} IN (?)", v.last)
+          end
+          tracking_url_ids = tracking_urls.pluck(:id)
+          leads = leads.where(tracking_url_id: tracking_url_ids)
+        end
+
+        # Pagination
+        params[:pagination][:page] += 1
+        params[:pagination][:per] = 20 if params[:pagination][:per].zero?
+
+        @leads = leads.page(params[:pagination][:page]).per(params[:pagination][:per])
+
+        # TODO: Deprecate headers pagination unless client side support fixed
+        response.headers['total-pages'] = @leads.total_pages
+        response.headers['current-page'] = @leads.current_page
+        response.headers['total-entries'] = leads.count
+
+        # TODO: Copied from old code, needs to be moved to separate endpoint
+        if need_to_download?
+          ::Leads::RecentLeadsReportJob.perform_later(@leads.pluck(:id), params.as_json, current_staff.email)
+          render json: { message: 'Report were sent' }, status: :ok
+        else
+          render 'v2/shared/leads/index'
+        end
+      end
 
       def show
         @visits = @lead.lead_events.order('DATE(created_at)').group('DATE(created_at)').count.keys.size
