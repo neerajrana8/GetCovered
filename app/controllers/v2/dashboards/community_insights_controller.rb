847844379da2766aceac9ecc6ee1cde06e4af053
dmr <noroot@falsetrue.io>
2022-08-10 19:51:06 +0300

    GCVR2-71 Apply cache for statistics aggregation query

--- app/controllers/v2/dashboards/community_insights_controller.rb
+++ app/controllers/v2/dashboards/community_insights_controller.rb
@@ -7,7 +7,6 @@ module V2
       # before_action :check_permissions
 
       def stats
-        @data = {}
         date_from = DateTime.now - 5.year
         date_to = DateTime.now
         date_utc_format = '%Y-%m-%d %H:%M:%S'
@@ -15,87 +14,96 @@ module V2
         filter = {}
         filter = params[:filter] if params[:filter].present?
 
-        if filter[:date]
-          date_from_dt = Time.zone.parse(filter[:date][:from]) unless filter[:date][:from].nil?
-          date_to_dt = Time.zone.parse(filter[:date][:to]) unless filter[:date][:to].nil?
-          date_from = date_from_dt.utc.strftime(date_utc_format)
-          date_to = date_to_dt.end_of_day.utc.strftime(date_utc_format)
-        end
+        cache_key = filter
 
-        units =
-          if filter[:insurable_id].present?
-            Insurable.where(insurable_id: filter[:insurable_id])
-          else
-            Insurable.where(insurable_type_id: InsurableType::UNITS_IDS)
+        stats = Rails.cache.read(cache_key)
+        if stats.nil?
+          if filter[:date]
+            date_from_dt = Time.zone.parse(filter[:date][:from]) unless filter[:date][:from].nil?
+            date_to_dt = Time.zone.parse(filter[:date][:to]) unless filter[:date][:to].nil?
+            date_from = date_from_dt.utc.strftime(date_utc_format)
+            date_to = date_to_dt.end_of_day.utc.strftime(date_utc_format)
           end
 
-        units_occupied_ids = units.where(occupied: true).pluck(:id)
-        units_cx = units_occupied_ids.count
-        policies =
-          PolicyInsurable
-            .joins(:policy)
-            .where(insurable_id: units_occupied_ids)
-            .where('policies.expiration_date > ?', Date.today)
+          units =
+            if filter[:insurable_id].present?
+              Insurable.where(insurable_id: filter[:insurable_id])
+            else
+              Insurable.where(insurable_type_id: InsurableType::UNITS_IDS)
+            end
 
-        master_policies_for_units =
-          policies
-            .where(policies: { policy_type_id: PolicyType::MASTER_IDS })
-            .pluck(:policy_id)
+          units_occupied_ids = units.where(occupied: true).pluck(:id)
+          units_cx = units_occupied_ids.count
+          policies =
+            PolicyInsurable
+              .joins(:policy)
+              .where(insurable_id: units_occupied_ids)
+              .where('policies.expiration_date > ?', Date.today)
 
-        master_policies_for_units_total = master_policies_for_units.count
+          master_policies_for_units =
+            policies
+              .where(policies: { policy_type_id: PolicyType::MASTER_IDS })
+              .pluck(:policy_id)
 
-        gc_policies_for_units = policies.where(policies: { agency_id: 1 }).pluck(:policy_id)
-        gc_policies_for_units_total = gc_policies_for_units.count
-        foreign_policies_for_units = policies.where(policies: { policy_in_system: false }).pluck(:policy_id)
-        foreign_policies_for_units_total = foreign_policies_for_units.count
+          master_policies_for_units_total = master_policies_for_units.count
 
-        policies_total = master_policies_for_units_total + gc_policies_for_units_total + foreign_policies_for_units_total
+          gc_policies_for_units = policies.where(policies: { agency_id: 1 }).pluck(:policy_id)
+          gc_policies_for_units_total = gc_policies_for_units.count
+          foreign_policies_for_units = policies.where(policies: { policy_in_system: false }).pluck(:policy_id)
+          foreign_policies_for_units_total = foreign_policies_for_units.count
 
-        units_all_ids = units.pluck(:id)
-        claims = Claim.by_created_at(date_from, date_to)
-        claims = claims.where(insurable_id: units_all_ids)
-        claims_amount_total = claims.sum(:amount)
-        claims_approved_cx = claims.where(status: :approved).count
-        claims_cx = claims.count
+          policies_total = master_policies_for_units_total + gc_policies_for_units_total + foreign_policies_for_units_total
 
-        claims_grouped = claims.group(:type_of_loss).count
-        claims_by_status = claims.group(:status).count
+          units_all_ids = units.pluck(:id)
+          claims_data = {}
+          unless units_all_ids.count.zero?
+            claims = Claim.by_created_at(date_from, date_to)
+            claims = claims.where(insurable_id: units_all_ids)
+            claims_amount_total = claims.sum(:amount)
+            claims_approved_cx = claims.where(status: :approved).count
+            claims_cx = claims.count
 
-        claim_stats = Claim.get_stats(date_from, date_to, units_all_ids)
+            claims_grouped = claims.group(:type_of_loss).count
+            claims_by_status = claims.group(:status).count
 
-        @claims_data = {
-          total: claims_cx,
-          paid_amount: claims_amount_total,
-          paid_percentage: ((claims_approved_cx.to_f / claims_cx.to_f) * 100).round(2),
-          by_type_of_loss: claims_grouped,
-          by_status: claims_by_status,
-          by_type_of_loss_by_status_charts: claim_stats
-        }
+            claim_stats = Claim.get_stats(date_from, date_to, units_all_ids)
 
-        @data = {
-          compliances: {
-            totals: {
-              units: units_cx,
-              policies: policies_total,
-              coverage: ((policies_total.to_f / units_cx.to_f) * 100).round(2)
-            },
-            master: {
-              policies: master_policies_for_units_total,
-              coverage: ((master_policies_for_units_total.to_f / units_cx.to_f) * 100).round(2)
-            },
-            gc: {
-              policies: gc_policies_for_units_total,
-              coverage: ((gc_policies_for_units_total.to_f / units_cx.to_f) * 100).round(2)
-            },
-            third_party: {
-              policies: foreign_policies_for_units_total,
-              coverage: ((foreign_policies_for_units_total.to_f / units_cx.to_f) * 100).round(2)
+            claims_data = {
+              total: claims_cx,
+              paid_amount: claims_amount_total,
+              paid_percentage: ((claims_approved_cx.to_f / claims_cx.to_f) * 100).round(2),
+              by_type_of_loss: claims_grouped,
+              by_status: claims_by_status,
+              by_type_of_loss_by_status_charts: claim_stats
             }
-          },
-          claims: @claims_data
-        }
+          end
 
-        render json: @data
+          stats = {
+            compliances: {
+              totals: {
+                units: units_cx,
+                policies: policies_total,
+                coverage: ((policies_total.to_f / units_cx.to_f) * 100).round(2)
+              },
+              master: {
+                policies: master_policies_for_units_total,
+                coverage: ((master_policies_for_units_total.to_f / units_cx.to_f) * 100).round(2)
+              },
+              gc: {
+                policies: gc_policies_for_units_total,
+                coverage: ((gc_policies_for_units_total.to_f / units_cx.to_f) * 100).round(2)
+              },
+              third_party: {
+                policies: foreign_policies_for_units_total,
+                coverage: ((foreign_policies_for_units_total.to_f / units_cx.to_f) * 100).round(2)
+              }
+            },
+            claims: claims_data
+          }
+          Rails.cache.write(cache_key, stats, expires_in: 5.minutes)
+        end
+
+        render json: stats
       end
 
       private
