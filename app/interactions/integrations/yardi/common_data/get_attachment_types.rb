f8b13a7b00133df7ffe088d37b838fd91be42fbf
Joshua Brinsfield <sapphous@gmail.com>
2022-08-12 13:39:12 -0500

    Add clean document upload code

---
+++ app/interactions/integrations/yardi/common_data/get_attachment_types.rb
@@ -0,0 +1,16 @@
+module Integrations
+  module Yardi
+    module CommonData
+      class GetAttachmentTypes < Integrations::Yardi::CommonData::Base=
+      
+        def execute
+          super({})
+        end
+                
+        def retry_request?(prior_attempts, elapsed_seconds)
+          prior_attempts < 3
+        end
+      end
+    end
+  end
+end
---
+++ app/interactions/integrations/yardi/common_data/import_tenant_lease_document_pdf.rb
@@ -0,0 +1,29 @@
+module Integrations
+  module Yardi
+    module CommonData
+      class ImportTenantLeaseDocumentPDF < Integrations::Yardi::CommonData::Base
+        string :property_id
+        string :resident_id
+        string :attachment
+        string :attachment_type
+        string :file_extension # pdf, xls, xlsx, doc, docx
+        string :description, default: "GC Verified Policy"
+        
+        def execute
+          super(**{
+            PropertyId: property_id,
+            TenantCode: resident_id,
+            AttachmentType: attachment_type,
+            Description: description,
+            FileExtension: file_extension,
+            Attachment: Base64.strict_encode64(attachment)
+          }.compact)
+        end
+                
+        def retry_request?(prior_attempts, elapsed_seconds)
+          prior_attempts < 3
+        end
+      end
+    end
+  end
+end
