740e53e4c57563d0b9c55b8efdb739616abb7950
dmr <noroot@falsetrue.io>
2022-08-15 18:11:35 +0300

    #GCVR2-709 Fix lead template for StaffSuperAdmin and StaffAccount roles

--- app/controllers/v2/staff_super_admin/policies_controller.rb
+++ app/controllers/v2/staff_super_admin/policies_controller.rb
@@ -6,6 +6,7 @@ module V2
   module StaffSuperAdmin
     class PoliciesController < StaffSuperAdminController
       include PoliciesMethods
+      include ActionController::Caching
 
       before_action :set_policy,
                     only: %i[update show refund_policy cancel_policy update_coverage_proof delete_policy_document
@@ -71,7 +72,7 @@ module V2
       def get_leads
         @leads = [@policy.primary_user.lead]
         @site_visits = @leads.last.lead_events.order("DATE(created_at)").group("DATE(created_at)").count.keys.size
-        render 'v2/shared/leads/index'
+        render 'v2/shared/leads/leads_by_policy'
       end
 
       def get_policies
