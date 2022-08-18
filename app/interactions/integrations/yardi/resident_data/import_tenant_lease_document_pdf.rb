ba0ba239622bd2d8231cf82d16a2c29991cc940d
Joshua Brinsfield <sapphous@gmail.com>
2022-08-18 14:43:21 -0500

    Maybe fix request template for pdf upload

--- app/interactions/integrations/yardi/resident_data/import_tenant_lease_document_pdf.rb
+++ app/interactions/integrations/yardi/resident_data/import_tenant_lease_document_pdf.rb
@@ -21,6 +21,24 @@ module Integrations
         def retry_request?(prior_attempts, elapsed_seconds)
           prior_attempts < 3
         end
+        
+        def request_template(**params)
+          <<~XML
+            <?xml version="1.0" encoding="utf-8"?>
+            <soap:Envelope xmlns:itf="http://tempuri.org/YSI.Interfaces.WebServices/ItfResidentData"
+                           xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+              <soap:Header/>
+              <soap:Body>
+                <itf:#{self.action} xmlns="#{self.xmlns}">
+                  #{params.map{|k,v| k.blank? ? stringify(v) : v.class == ::Array ? v.map{|vv| "<#{k}>#{vv}</#{k}>" }.join("") : "<itf:#{k}>#{stringify(v)}</itf:#{k}>" }.join("\n      ")}
+                </itf:#{self.action}>
+              </soap:Body>
+            </soap:Envelope>
+          XML
+        end
+        
+        
+        
       end
     end
   end
