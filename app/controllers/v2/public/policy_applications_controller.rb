f729f7198fbed19eefef5a70395a1c78ca507ac8
dmr <noroot@falsetrue.io>
2022-08-24 14:57:00 +0300

    #GCVR2-714 Fix concern inclusion for policy application

--- app/controllers/v2/public/policy_applications_controller.rb
+++ app/controllers/v2/public/policy_applications_controller.rb
@@ -7,7 +7,7 @@ module V2
   module Public
     class PolicyApplicationsController < PublicController
 
-      include Leads::CreateMethods
+      include ::Leads::CreateMethods
       include Devise::Controllers::SignInOut
       include PolicyApplicationMethods
 
