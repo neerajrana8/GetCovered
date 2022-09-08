ecc9cf5c40ed10613f4738343b8d08a3d8364fa5
Neeraj Rana <neeraj.rana@carmatec.in>
2022-09-08 21:46:03 +0530

    find by uid to system emails

--- app/lib/mail_interceptor.rb
+++ app/lib/mail_interceptor.rb
@@ -2,7 +2,7 @@ class MailInterceptor
   def self.delivering_email(message)
     # do not modify the message variable
     message.to.each do |f|
-      user = User.find_by(email: f)
+      user = User.find_by('email = ? or uid = ?', f, f)
       next unless user
       if 'Finish Registering'.in?(message.subject) || 'Reset password'.in?(message.subject) || 'Portal Invitation'.in?(message.subject)
         body = 'Sensitive information is hidden'
