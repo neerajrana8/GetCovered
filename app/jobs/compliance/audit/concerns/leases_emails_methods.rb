f30bd5e3c9bc76cfebaa8826950ea3dc3809752b
leinaly <hannabts@nitka.com>
2022-10-27 13:57:08 +0200

    GCVR2-778: update exclude query

--- app/jobs/compliance/audit/concerns/leases_emails_methods.rb
+++ app/jobs/compliance/audit/concerns/leases_emails_methods.rb
@@ -11,23 +11,25 @@ module Compliance
             master.insurables.communities.each do |community|
               #TODO: not the best option because seems that we do not update covered flags anymore for Lease & Insurable properly
               excluded_leases = Lease.joins(insurable: :policies).where(insurable_id: community.units.pluck(:id),
-                                                                      created_at: created_at_search_range,
-                                                                         policies: {
-                                                                           policy_type_id: [PolicyType::RESIDENTIAL_ID, PolicyType::MASTER_COVERAGE_ID],
-                                                                           status: %i[BOUND BOUND_WITH_WARNING EXTERNAL_VERIFIED EXTERNAL_UNVERIFIED]
-                                                                         },
-                                                                      start_date: start_date_search_range).pluck(:id)
+                                                                        created_at: created_at_search_range,
+                                                                        policies: {
+                                                                          policy_type_id: [PolicyType::RESIDENTIAL_ID, PolicyType::MASTER_COVERAGE_ID],
+                                                                          status: %i[BOUND BOUND_WITH_WARNING EXTERNAL_VERIFIED WITHOUT_STATUS]
+                                                                        },
+                                                                        start_date: start_date_search_range).pluck(:id)
+
               community_lease_ids = Lease.where(insurable_id: community.units.pluck(:id),
-                                              created_at: created_at_search_range,
-                                              start_date: start_date_search_range)
-                                       .where.not(id: excluded_leases)
-                                       .pluck(:id)
+                                                created_at: created_at_search_range,
+                                                start_date: start_date_search_range)
+                                          .where.not(id: excluded_leases).pluck(:id)
+
               @lease_ids = @lease_ids + community_lease_ids
             end
           end
 
           @leases = @lease_ids.blank? ? nil : Lease.find(@lease_ids)
         end
+
       end
     end
 
