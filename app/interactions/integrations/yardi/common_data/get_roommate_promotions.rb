817ec9f8678bd9d9f895b340183767ff3e4b766d
Joshua Brinsfield <sapphous@gmail.com>
2022-11-14 15:24:52 -0600

    Tweak it bro

--- app/interactions/integrations/yardi/common_data/get_roommate_promotions.rb
+++ app/interactions/integrations/yardi/common_data/get_roommate_promotions.rb
@@ -3,12 +3,17 @@ module Integrations
     module CommonData
       class GetUnitInformation < Integrations::Yardi::CommonData::Base
         string :property_id
+        date :move_out_start
+        date :move_out_end
+
         def execute
           super(**{
-            YardiPropertyId: property_id
+            YardiPropertyId: property_id,
+            MoveOutFrom: move_out_start,
+            MoveOutTo: move_out_end
           }.compact)
         end
-                
+
         def retry_request?(prior_attempts, elapsed_seconds)
           prior_attempts < 3
         end
