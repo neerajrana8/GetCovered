11eafb83e1ab91ac4af2f739d17357f5b930c7d8
dmr <noroot@falsetrue.io>
2022-11-16 18:57:30 +0300

    #GCVR2-928 Add data migration to restore missing mailing addresses

--- db/data/20221116153326_restore_missing_mailing_address.rb
+++ db/data/20221116153326_restore_missing_mailing_address.rb
@@ -4,7 +4,7 @@ class RestoreMissingMailingAddress < ActiveRecord::Migration[6.1]
   def up
     policies = Policy.where(policy_in_system: false, address: nil)
     policies.each do |p|
-      address_string = p.policy_users.where(primary: true).first&.address&.full
+      address_string = p.policy_users.where(primary: true).first&.user&.address&.full
       p.update_columns(address: address_string)
     end
   end
