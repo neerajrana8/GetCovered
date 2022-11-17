f560e8e673a6c369996235166561bc9a069bb8d4
dmr <noroot@falsetrue.io>
2022-11-17 16:29:47 +0300

    #GCVR2-982 Properly set insurable address relation to user address

--- db/data/20221117084937_restore_missing_mailing_address_with_primary_insurable.rb
+++ db/data/20221117084937_restore_missing_mailing_address_with_primary_insurable.rb
@@ -6,6 +6,8 @@ class RestoreMissingMailingAddressWithPrimaryInsurable < ActiveRecord::Migration
     # DEBUG policies = Policy.where(policy_in_system: false, id: [9147]).limit(10)
     policies.each do |policy|
       insurable_address = policy.primary_insurable&.addresses&.first
+      next if insurable_address.nil?
+      next if policy.primary_user.nil?
       if policy.primary_user&.address.nil?
         new_address_user = insurable_address.dup
         new_address_user.addressable_type = 'User'
