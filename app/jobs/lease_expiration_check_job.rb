247ae4a42d79bfb8602cced8af6b2aaa58f47ef8
dmr <noroot@falsetrue.io>
2022-12-07 14:21:12 +0300

    #GCVR2-768 #GCVR2-916: Update LeaseExpirationCheckJob cancel policy, update covered, and occupied statuses

--- app/jobs/lease_expiration_check_job.rb
+++ app/jobs/lease_expiration_check_job.rb
@@ -14,7 +14,26 @@ class LeaseExpirationCheckJob < ApplicationJob
   # Checks for leases expiring today and
   # activates them
   def perform(*_args)
-    @leases.each(&:deactivate)
+    # @leases.each(&:deactivate)
+    @leases.each do |lease|
+      lease.deactivate
+      policy = lease.insurable.policies.current.take
+
+      # NOTE: Update user occupation
+      lease.update_unit_occupation
+
+      # NOTE: Set lease covered false
+      lease.update covered: false
+
+      next if policy.blank?
+      next unless [PolicyType::MASTER_COVERAGE_ID, PolicyType::RESIDENTIAL_ID].include?(policy.policy_type_id)
+
+      # NOTE: Set policy status to CANCELLED
+      policy.update status: 'CANCELLED'
+
+      # NOTE: Call QBE for master_coverage update
+      policy.qbe_specialty_evict_master_coverage if policy.policy_type_id == PolicyType::MASTER_COVERAGE_ID
+    end
   end
 
   private
