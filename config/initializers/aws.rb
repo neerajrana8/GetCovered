7249c94b551e4debd9e9479fc9b257f8714296cd
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-01-17 14:11:19 +0500

    changes are pushed for muneeb's development setup

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,13 +2,15 @@
 require 'aws-sdk-core'
 
 env = Rails.env.to_sym
-aws_access_key_id = Rails.application.credentials.aws[env][:access_key_id]
-aws_secret_access_key = Rails.application.credentials.aws[env][:secret_access_key]
-aws_region = Rails.application.credentials.aws[env][:region]
-aws_bucket_name = Rails.application.credentials.aws[env][:bucket]
-Aws.config.update({
-                    region: aws_region,
-                    credentials: Aws::Credentials.new(aws_access_key_id, aws_secret_access_key),
-                  })
+if Rails.application.credentials.aws
+  aws_access_key_id = Rails.application.credentials.aws[env][:access_key_id]
+  aws_secret_access_key = Rails.application.credentials.aws[env][:secret_access_key]
+  aws_region = Rails.application.credentials.aws[env][:region]
+  aws_bucket_name = Rails.application.credentials.aws[env][:bucket]
+  Aws.config.update({
+                      region: aws_region,
+                      credentials: Aws::Credentials.new(aws_access_key_id, aws_secret_access_key),
+                    })
 
-$S3_BUCKET = Aws::S3::Resource.new.bucket(aws_bucket_name)
+  $S3_BUCKET = Aws::S3::Resource.new.bucket(aws_bucket_name)
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,14 @@
-sidekiq_config = { 
-	:host => Rails.application.credentials.redis[ENV["RAILS_ENV"].to_sym][:host],
-	:port => Rails.application.credentials.redis[ENV["RAILS_ENV"].to_sym][:port] 
-}
+if Rails.application.credentials.redis
+  sidekiq_config = {
+    :host => Rails.application.credentials.redis[ENV["RAILS_ENV"].to_sym][:host],
+    :port => Rails.application.credentials.redis[ENV["RAILS_ENV"].to_sym][:port]
+  }
 
-Sidekiq.configure_server do |config|
-  config.redis = sidekiq_config
-end
+  Sidekiq.configure_server do |config|
+    config.redis = sidekiq_config
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = sidekiq_config
+  Sidekiq.configure_client do |config|
+    config.redis = sidekiq_config
+  end
 end
