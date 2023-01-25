d3a56ad4b1fd3417565f6d4a5f9f17187fa83b19
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-01-25 16:55:40 +0500

    fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,13 @@
 require 'stripe'
 Stripe.api_key = Rails.application
-  .credentials
-  .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
+                      .credentials
+                      .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
 
 Rails.configuration.stripe = {
   publishable_key: Rails.application
-    .credentials
-    .stripe[ENV.fetch('RAILS_ENV').to_sym][:publishable_key],
+                        .credentials
+                        .stripe[ENV.fetch('RAILS_ENV').to_sym][:publishable_key],
   secret_key: Rails.application
-    .credentials
-    .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
+                   .credentials
+                   .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
 }
