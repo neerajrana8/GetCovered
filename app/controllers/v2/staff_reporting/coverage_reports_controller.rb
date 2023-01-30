c3f723a8050e48da24f43de2f563e9ea52259fd1
Joshua Brinsfield <sapphous@gmail.com>
2023-01-30 02:16:46 -0600

    Modify routes & CRC

--- app/controllers/v2/staff_reporting/coverage_reports_controller.rb
+++ app/controllers/v2/staff_reporting/coverage_reports_controller.rb
@@ -9,42 +9,51 @@ module V2
         super(:@coverage_reports, access_model(Reporting::CoverageReport))
       end
       
-      def fixed_filters
-        @organizable.nil? ? {} : { status: 'ready', visible: true, coverage_determinant: (@organizable.reporting_coverage_reports_settings || {})['coverage_determinant'] || 'any' }
+      def latest
+        @coverage-report = access_model(Reporting::CoverageReport).where(fixed_filters).order("report_time desc").limit(1).first
+        render template: 'v2/shared/reporting/coverage_reports/show',
+          status: :ok
       end
       
-      def default_filters
-        {
-          status: 'ready'
-        }
-      end
+      private
       
-      def supported_filters
-        {
-          coverage_determinant: [ :scalar, :array, :interval ],
-          report_time: [ :scalar, :array, :interval ]
-        }.merge(@organizable ? {} : {
-          status: [ :scalar, :array ],
-          completed_at: [ :scalar, :array, :interval ],
-          owner_type: [ :scalar, :array, :interval ],
-          owner_id: [ :scalar, :array, :interval ],
-          created_at: [ :scalar, :array, :interval ],
-          updated_at: [ :scalar, :array, :interval ]
-        })
-      end
-      
-      def default_pagination_per
-        50
-      end
-      
-      def view_path
-        'v2/shared/reporting/coverage_reports'
-      end
-      
-      def v2_should_render
-        { short: true, index: true }
-      end
-
+        def fixed_filters
+          @organizable.nil? ? {} : { status: 'ready', visible: true, coverage_determinant: (@organizable.reporting_coverage_reports_settings || {})['coverage_determinant'] || 'any' }
+        end
+        
+        def default_filters
+          {
+            status: 'ready'
+          }
+        end
+        
+        def supported_filters
+          {
+            coverage_determinant: [ :scalar, :array, :interval ],
+            report_time: [ :scalar, :array, :interval ]
+          }.merge(@organizable ? {} : {
+            status: [ :scalar, :array ],
+            completed_at: [ :scalar, :array, :interval ],
+            owner_type: [ :scalar, :array, :interval ],
+            owner_id: [ :scalar, :array, :interval ],
+            created_at: [ :scalar, :array, :interval ],
+            updated_at: [ :scalar, :array, :interval ]
+          })
+        end
+        
+        def default_pagination_per
+          50
+        end
+        
+        def view_path
+          'v2/shared/reporting/coverage_reports'
+        end
+        
+        def v2_should_render
+          { short: true, index: true }
+        end
+        
+      # end private
     end
   end
 end
--- config/routes/staff_reporting.rb
+++ config/routes/staff_reporting.rb
@@ -1,17 +1,22 @@
 # StaffReporting
 scope module: :staff_reporting, path: "reporting" do
 
-  get :coverage_reports,
+  post :coverage_reports,
     to: "coverage_reports#index",
     via: "get",
     path: "coverage-reports/:coverage_report_id"
-  get :coverage_entries,
+  post :coverage_entries,
     to: "coverage_entries#index",
     via: "get",
     path: "coverage-reports/:coverage_report_id/entries"
-  get :unit_coverage_entries,
+  post :unit_coverage_entries,
     to: "unit_coverage_entries#index",
     via: "get",
     path: "coverage-reports/:coverage_report_id/entries"
+  
+  get :latest_coverage_report,
+    to: "coverage_reports#latest",
+    via: "get",
+    path: "latest/coverage-report"
 
 end
