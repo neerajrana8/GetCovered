033ffb8b23a1103b00c3f0dd881274b953765509
dmr <noroot@falsetrue.io>
2023-01-31 16:43:49 +0400

    #GCVR2-1188 Add id to permitted parameters to pass MPC by id

--- app/controllers/concerns/master_policies_methods.rb
+++ app/controllers/concerns/master_policies_methods.rb
@@ -417,7 +417,7 @@ module MasterPoliciesMethods
         :account_id, :agency_id, :auto_renew, :carrier_id, :effective_date, :policy_type_id,
         :expiration_date, :number, system_data: [:landlord_sumplimental],
         policy_coverages_attributes: %i[policy_application_id title limit deductible enabled designation],
-        master_policy_configurations_attributes:
+        master_policy_configurations_attributes: [
           :id,
           :program_type,
           :grace_period,
