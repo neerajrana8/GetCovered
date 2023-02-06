284e78b4ec23653a023b7cf9a6889d91b1ef6323
Joshua Brinsfield <sapphous@gmail.com>
2023-02-05 22:16:40 -0600

    Test

--- config/routes/public.rb
+++ config/routes/public.rb
@@ -15,6 +15,10 @@ post 'qbe/communities/show',
 
 scope module: :public do
 
+
+  get :version_test
+    to: "super_duper_admin_controller#version_test"
+
   get '/agency-accounts/:agency_id',
     to: 'accounts#agency_accounts'
 
