533af7592a7ccc467dbf74947f670f4bc2ff2ba8
Joshua Brinsfield <sapphous@gmail.com>
2023-02-05 19:39:44 -0600

    Fix routes

--- config/routes/staff_reporting.rb
+++ config/routes/staff_reporting.rb
@@ -3,20 +3,16 @@ scope module: :staff_reporting, path: "reporting" do
 
   post :coverage_reports,
     to: "coverage_reports#index",
-    via: "get",
     path: "coverage-reports/:coverage_report_id"
   post :coverage_entries,
     to: "coverage_entries#index",
-    via: "get",
     path: "coverage-reports/:coverage_report_id/entries"
   post :unit_coverage_entries,
     to: "unit_coverage_entries#index",
-    via: "get",
-    path: "coverage-reports/:coverage_report_id/entries"
+    path: "coverage-reports/:coverage_report_id/unit-entries"
   
   post :latest_coverage_report,
     to: "coverage_reports#latest",
-    via: "get",
     path: "latest/coverage-report"
 
 end
