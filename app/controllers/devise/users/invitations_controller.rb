67cb96f17d303f7c856169f207bca8bfb711726c
hanna-bautsiuk <hannabts@nitka.com>
2023-02-08 15:54:12 +0100

    #GCVR2-649: clear pr from unused code

--- app/controllers/devise/users/invitations_controller.rb
+++ app/controllers/devise/users/invitations_controller.rb
@@ -2,16 +2,6 @@ class Devise::Users::InvitationsController < Devise::InvitationsController
   before_action :resource_from_invitation_token, only: [:edit, :update]
   after_action :save_token_for_password_reset, only: [:update]
 
-  #def create
-  #  ::User.invite!(invite_params)
-  #  render json: { success: true },
-  #         status: :created
-  #end
-
-  #def edit
-  #  # redirect_to "#{client_api_url}?invitation_token=#{params[:invitation_token]}"
-  #end
-
   def update
     @raw_invitation_token = accept_invitation_params[:invitation_token]
 
@@ -29,7 +19,6 @@ class Devise::Users::InvitationsController < Devise::InvitationsController
           resource.create_token
 
           sign_out(resource) if accept_invitation_params[:password_confirmation].present?
-          #sign_in(resource_name, resource)
 
           create_acct_user
 
@@ -48,24 +37,6 @@ class Devise::Users::InvitationsController < Devise::InvitationsController
       end
     else
       render json: { errors: ['Token already been used'] },
-             status: :unprocessable_entity
-    end
-  end
-
-  def update_old
-    @user.update(password: accept_invitation_params[:password], password_confirmation: accept_invitation_params[:password_confirmation])
-    @user.accept_invitation!
-    if @user.errors.empty?
-      @resource = @user
-      @token = @resource.create_token
-      @resource.save!
-      update_auth_header
-      acct = AccountUser.find_by(user_id: @user.id, account_id: @user.account_users.last&.account_id)
-      acct.update(status: 'enabled') if acct.present? && acct.status != 'enabled'
-      render json: { success: ['User updated.'] },
-             status: :accepted
-    else
-      render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
     end
   end
@@ -108,27 +79,5 @@ class Devise::Users::InvitationsController < Devise::InvitationsController
                   :password_confirmation,
                   :invitation_token,
                   profile_attributes: [:first_name, :last_name, :contact_phone, :birth_date])
-  end
-
-  # -------------- OLD --------------------
-
-  def resource_from_invitation_token_old
-    #raw_token = Devise.token_generator.digest(resource_class, :invitation_token, params[:invitation_token])
-
-    @user = ::User.find_by_invitation_token(params[:invitation_token], true)
-
-    return if params[:invitation_token] && @user
-    render json: { errors: ['Invalid token.'] }, status: :not_acceptable
-  end
-
-    def invite_params
-      params.permit(user: [:email, :invitation_token, :provider, :skip_invitation])
-    end
-
-    def accept_invitation_params
-      params.permit(:email, :password,
-                    :password_confirmation,
-                    :invitation_token,
-                    profile_attributes: [:first_name, :last_name, :contact_phone, :birth_date])
   end
 end
--- app/controllers/devise/users/passwords_controller.rb
+++ app/controllers/devise/users/passwords_controller.rb
@@ -1,31 +1,4 @@
 class Devise::Users::PasswordsController < DeviseTokenAuth::PasswordsController
   skip_after_action :update_auth_header, only: [:create, :edit, :update]
 
-  #def create
-  #  return render_create_error_missing_email unless resource_params[:email]
-
-  #  @email = get_case_insensitive_field_from_resource_params(:email)
-  #  @resource = find_resource(:email, @email)
-  #  if @resource
-  #    yield @resource if block_given?
-  #    @resource.settings['last_reset_password_base_url'] = request.headers['origin']
-  #   @resource.save
-  #   @resource.send_reset_password_instructions(
-  #      email: @email,
-  #      provider: 'email',
-  #      redirect_url: @redirect_url,
-  #     client_config: params[:config_name],
-  #     request_base_url: @request_base_url
-  #    )
-
-  #    if @resource.errors.empty?
-  #      return render_create_success
-  #    else
-  #      render_create_error @resource.errors
-  #    end
-  #  else
-  #    render_not_found_error
-  #  end
-  #end
-
 end
