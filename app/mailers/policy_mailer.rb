8e7bf4ca054a30f4b9f76d754364d5264c51b83e
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-02-17 13:32:17 +0500

    layout fixes

--- app/mailers/policy_mailer.rb
+++ app/mailers/policy_mailer.rb
@@ -1,11 +1,13 @@
 class PolicyMailer < ApplicationMailer
-
+  layout 'agency_styled_mail'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.policy_mailer.coverage_proof_uploaded.subject
   def coverage_proof_uploaded
     @policy = params[:policy]
+    @agency = @policy.agency
+    @branding_profile = @policy.branding_profile || BrandingProfile.global_default
 
     mail to: @policy.primary_user.email, subject: 'Policy Submission Received'
   end
