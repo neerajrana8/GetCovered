2406f8fd25b85128a9e37b0d7bb11f77eaa47f85
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-02-17 14:28:57 +0500

    preview fix

--- test/mailers/previews/policy_preview.rb
+++ test/mailers/previews/policy_preview.rb
@@ -3,6 +3,7 @@ class PolicyPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/policy/coverage_proof_uploaded
   def coverage_proof_uploaded
-    PolicyMailer.with(policy: Policy.first).coverage_proof_uploaded
+    policy = Policy.where.not(agency_id: nil, branding_profile_id: nil).last
+    PolicyMailer.with(policy: policy).coverage_proof_uploaded
   end
 end
