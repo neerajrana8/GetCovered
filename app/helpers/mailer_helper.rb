f6f95cf1c0e043a516c4cabacc364a916bbf2df5
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-02-23 00:11:42 +0100

    all_rights_reserved optimisations

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -4,10 +4,9 @@ module MailerHelper
   end
 
   def all_rights_reserved(from_date, to_date, text)
-    if from_date.present?
-      "#{from_date} - ©#{to_date} #{text}"
-    else
-      "©#{to_date.strftime('%Y')} #{text}"
-    end
+    rr_text = "©#{to_date.strftime('%Y')} #{text}"
+    return rr_text unless from_date.present?
+
+    "#{from_date} - #{rr_text}"
   end
 end
