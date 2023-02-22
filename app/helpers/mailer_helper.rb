420455f4ee7b034edba1f2bffd0bb3b8726e03be
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-02-23 00:03:45 +0100

    all_rights_reserved helper method is added

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -2,4 +2,12 @@ module MailerHelper
   def email_image_url(image)
     "#{Rails.configuration.email_images_url}/#{image}"
   end
+
+  def all_rights_reserved(from_date, to_date, text)
+    if from_date.present?
+      "#{from_date} - ©#{to_date} #{text}"
+    else
+      "©#{to_date.strftime('%Y')} #{text}"
+    end
+  end
 end
