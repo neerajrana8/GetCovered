cd6f53f01f8023a23fb3aaca2f55cdfd37871550
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-02-27 23:46:07 +0100

    EOF fixes

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,13 +1,11 @@
 require 'stripe'
-Stripe.api_key = Rails.application
-                      .credentials
-                      .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
+Stripe.api_key = Rails.application.credentials
+  .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
 
 Rails.configuration.stripe = {
-  publishable_key: Rails.application
-                        .credentials
-                        .stripe[ENV.fetch('RAILS_ENV').to_sym][:publishable_key],
-  secret_key: Rails.application
-                   .credentials
-                   .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
+  publishable_key: Rails.application.credentials
+    .stripe[ENV.fetch('RAILS_ENV').to_sym][:publishable_key],
+  secret_key: Rails.application.credentials
+    .stripe[ENV.fetch('RAILS_ENV').to_sym][:secret_key]
 }
+
