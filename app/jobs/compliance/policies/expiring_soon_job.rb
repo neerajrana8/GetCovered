ca820ca1b5ef2b47ee8780713d3ed368c6db5d12
Hanna Bautsiuk <hannabts@nitka.com>
2023-03-03 19:12:49 +0100

    #GCVR2-1027: prevent duplicates

--- app/jobs/compliance/policies/expiring_soon_job.rb
+++ app/jobs/compliance/policies/expiring_soon_job.rb
@@ -6,7 +6,7 @@ module Compliance
 
       def perform(*args)
         @policies.each do |policy|
-          Compliance::PolicyMailer.with(organization: policy.account).policy_expiring_soon(policy: policy).deliver_now
+          Compliance::PolicyMailer.with(organization: policy.account).policy_expiring_soon(policy: policy).deliver_now unless was_already_sent?(policy)
         end
       end
 
@@ -16,6 +16,11 @@ module Compliance
         @policies = Policy.where(status: ["BOUND", "BOUND_WITH_WARNING", "EXTERNAL_VERIFIED"],
                                  expiration_date: Date.today + 7.days,
                                  policy_type_id: 1).distinct
+      end
+
+      #TODO: need to move to service object
+      def was_already_sent?(policy)
+        ContactRecord.where(subject: t('policy_mailer.policy_expiring_soon.subject'), contactable_type: "User", contactable_id: policy&.primary_user.id).count > 0
       end
     end
   end
