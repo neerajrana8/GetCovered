b254c75dbfcecf7eda3dc7f7e82f3b9c8a720644
Joshua Brinsfield <sapphous@gmail.com>
2023-03-09 14:55:51 -0600

    Expand scope of user merge job to changed policies and to created policies or leases. Give YardiSyncJob the ability to skip things that have already been synced that day.

--- app/jobs/user_merge_job.rb
+++ app/jobs/user_merge_job.rb
@@ -4,8 +4,15 @@ class UserMergeJob < ApplicationJob
   def perform(days_to_check = 2)
     ActiveRecord::Base.logger.level = 1
     # generic systemwide user merges
-    user_ids = User.where("created_at >= ?", (Time.current - days_to_check.days).beginning_of_day).order(id: :asc).pluck(:id)
-    user_ids += PolicyUser.where("created_at >= ?", (Time.current - days_to_check.days).beginning_of_day).where.not(user_id: user_ids).pluck(:user_id)
+    user_ids = User.where("created_at >= ?", (Time.current - days_to_check.days).beginning_of_day)
+           .or(User.where("updated_at >= ?", (Time.current - days_to_check.days).beginning_of_day))
+           .order(id: :asc).pluck(:id)
+    user_ids += PolicyUser.where("created_at >= ?", (Time.current - days_to_check.days).beginning_of_day)
+            .or(PolicyUser.where("updated_at >= ?", (Time.current - days_to_check.days).beginning_of_day))
+            .where.not(user_id: user_ids).pluck(:user_id)
+    user_ids += LeaseUser.where("created_at >= ?", (Time.current - days_to_check.days).beginning_of_day)
+            .or(LeaseUser.where("updated_at >= ?", (Time.current - days_to_check.days).beginning_of_day))
+            .where.not(user_id: user_ids).pluck(:user_id)
     user_ids.each do |user_id|
       user = User.where(id: user_id).take
       next if user.nil?
@@ -19,18 +26,20 @@ class UserMergeJob < ApplicationJob
       LeaseUser.references(:leases).includes(:lease).where(
         leases: { insurable_id: PolicyInsurable.where(policy: user.policies.where(policy_type_id: 1), primary: true).select(:insurable_id) }
       ).select{|lu| lu.user&.profile&.first_name&.downcase&.strip == user.profile.first_name.downcase.strip && lu.user&.profile&.last_name&.downcase&.strip == user.profile.last_name.downcase.strip }
-       .each{|lu| matchez.push(lu.user_id) }
-      # consolidate matches
+       .each{|lu| matchez.push(lu.user_id) unless lu.user_id == user.id }
+      # consolidate matchez and pull up the appropriate users
       next if matchez.blank?
+      users = ([user] + User.where(id: matchez).to_a).uniq
       # merge matching users
-      matchez.uniq!
       ActiveRecord::Base.transaction do
-        users = ([user] + User.where(id: matchez).where.not(id: user.id).to_a).uniq
-        us = users.sort_by{|x| [-x.sign_in_count, x.email.nil? ? 1 : 0, x.created_at] }
-        while us.count > 1
-          us[-2].absorb!(us[-1])
-          us[-2].reload
-          us.pop
+        users = users.sort_by{|x| [-x.sign_in_count, x.email.nil? ? 1 : 0, x.created_at] }
+        while users.count > 1 && users[1].sign_in_count > 0
+          users = users.drop(1) # disallow merges of multiple signed-in user accounts (for security)
+        end
+        while users.count > 1
+          users[-2].absorb!(users[-1])
+          users[-2].reload
+          users.pop
         end
       end
     end
