e9179626bd184e6768ff41bfef2c38c6e002d005
Dylan Gaines <dylan@dylangaines.com>
2023-04-28 10:50:02 -0700

    Sorting out s3 upload

--- app/services/qbe/eoi_generator.rb
+++ app/services/qbe/eoi_generator.rb
@@ -5,23 +5,29 @@ module Qbe
 
     attr_accessor :document_title
     attr_accessor :template_path
-    attr_accessor :save_path
+    attr_accessor :local_save_path
+    attr_accessor :s3_save_path
     attr_accessor :locales
     attr_accessor :options
 
-    def initialize(document_title, template_path, save_path, locales, options)
+    def initialize(document_title, template_path, local_save_path, s3_save_path, locales, options)
       @document_title = document_title
       @template_path = template_path
-      @save_dir = "#{ Rails.root }/#{ save_path }"
-      @save_path = Rails.root.join(save_path, document_title)
+      @local_save_dir = "#{ Rails.root }/#{ local_save_path }"
+      @local_save_path = Rails.root.join(local_save_path, document_title)
+      @s3_save_path = s3_save_path.sub!(/\//, '').chomp('/')
       @locales = locales
       @options = { upload: false, save: false }.merge!(options)
+      @bucket = Rails.env == 'production' ? 'gc-public-prod' : 'gc-public-dev'
     end
 
     def call
       pdf = generate_pdf
       save_file(pdf) if options[:save]
-      upload_file() if options[:upload]
+      upload_file() if options[:save] && options[:upload]
+      build_public_url() if options[:save] && options[:upload]
+      response = options[:save] && options[:upload] ? @public_url : nil
+      return response
     end
 
     private
@@ -40,14 +46,14 @@ module Qbe
 
     def save_file(pdf)
       create_dir_unless_exists
-      File.open(@save_path, 'wb') do |file|
+      File.open(@local_save_path, 'wb') do |file|
         file << pdf
       end
     end
 
     def create_dir_unless_exists
-      unless File.directory?(@save_dir)
-        FileUtils.mkdir_p(@save_dir)
+      unless File.directory?(@local_save_dir)
+        FileUtils.mkdir_p(@local_save_dir)
       end
     end
 
@@ -56,12 +62,16 @@ module Qbe
                                            Rails.application.credentials.aws[Rails.env.to_sym][:secret_access_key])
       s3 = ::Aws::S3::Client.new(region: Rails.application.credentials.aws[Rails.env.to_sym][:region],
                                  credentials: credentials)
-      File.open(@save_path, 'rb') do |file|
-        puts "starting to upload #{ @save_path} to S3"
-        attempt = s3.put_object(bucket: Rails.application.credentials.aws[Rails.env.to_sym][:bucket],
-                                acl: 'public-read', key: @document_title, body: file)
-        pp attempt
+
+      File.open(@local_save_path, 'rb') do |file|
+        s3.put_object(bucket: @bucket, acl: 'public-read', key: "#{ @s3_save_path }/#{ @document_title }", body: file)
       end
+    end
+
+    def build_public_url
+      url_array = ['https://', @bucket, '.s3.', Rails.application.credentials.aws[Rails.env.to_sym][:region],
+                   '.amazonaws.com/', @s3_save_path, '/', @document_title]
+      @public_url = url_array.join('')
     end
 
   end
