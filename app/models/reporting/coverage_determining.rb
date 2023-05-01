0168050c7f16de5ef9ef6dc4d4036592e0e88934
Joshua Brinsfield <sapphous@gmail.com>
2023-05-01 15:50:54 -0500

    Fix coverage determination bug

--- app/models/reporting/coverage_determining.rb
+++ app/models/reporting/coverage_determining.rb
@@ -17,7 +17,7 @@ module Reporting
       internal_or_external: 5 # unit is covered completely by internal policies, and also covered completely by external policies (i.e. choose i or e and you are covered either way)
     }.freeze
     
-    COVERAGE_STATUSES_ORDER = [:internal_or_external, :internal, :external, :internal_and_external, :master, :none].freeze
+    COVERAGE_STATUSES_ORDER = ['internal_or_external', 'internal', 'external', 'internal_and_external', 'master', 'none'].freeze
     
     COVERAGE_DETERMINANT_ENUM_INFO = {
       enum_values: [COVERAGE_DETERMINANTS.keys.map{|x| x.to_s }],
