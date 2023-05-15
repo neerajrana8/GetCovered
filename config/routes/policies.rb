9d1081f56141675d108750cd52ff504c75b3f3de
Hanna Bautsiuk <hannabts@nitka.com>
2023-05-15 14:37:01 +0200

    GCVR2-1537

--- config/routes/policies.rb
+++ config/routes/policies.rb
@@ -2,4 +2,9 @@ scope module: :policies, path: 'policies' do
   post :list, controller: :policies, action: :list
   post 'show/:id', controller: :policies, action: :show
   post :calculate_cost, controller: :policies, action: :calculate_cost
+
+  # External policices
+  scope path: 'external' do
+    post :check, controller: :external_policies, action: :check, as: 'external_policy_check'
+  end
 end
