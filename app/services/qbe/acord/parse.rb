42332f5697f1c9504e1c53a574df3b70577a30ee
Dylan Gaines <dylan@dylangaines.com>
2023-05-24 10:42:05 -0700

    Breaking out duplicate reporting into its own service from parsed acord file and setting up a mailer

---
+++ app/services/qbe/acord/parse.rb
@@ -0,0 +1,119 @@
+require 'open-uri'
+require 'fileutils'
+require 'nokogiri'
+
+module Qbe
+  module Acord
+    # Qbe::Acord::Parse service
+    class Parse < ApplicationService
+
+      attr_accessor :checked_file_id
+      attr_accessor :override_read
+
+      def initialize(checked_file_id, override_read = false)
+        @checked_file_id = checked_file_id
+        @override_read = override_read
+      end
+
+      def call
+        raise "CheckedFile ID null" if @checked_file_id.nil?
+        raise "CheckedFile not found" unless set_checked_file()
+        raise "CheckedFile previously read" if (@checked_file.processed && @override_read == false)
+        build_s3_url()
+
+        begin
+          remote_document = URI.open(@s3_url)
+        rescue Exception => error
+          # Todo: Set up a failure notification
+          Rails.logger.debug error
+        else
+          @checked_file.update processed: true if read_remote_document(remote_document)
+        end
+      end
+
+      private
+
+      # set_checked_file()
+      # @return [true, false]
+      def set_checked_file
+        to_return = false
+        if CheckedFile.exists?(id: @checked_file_id)
+          @checked_file = CheckedFile.find(@checked_file_id)
+          to_return = true
+        end
+        return to_return
+      end
+
+      def build_s3_url
+        bucket = Rails.env == 'production' ? 'gc-public-prod' : 'gc-public-dev'
+        url_array = ['https://', bucket, '.s3.', Rails.application.credentials.aws[Rails.env.to_sym][:region],
+                     '.amazonaws.com/ACORD/', @checked_file.name]
+        @s3_url = url_array.join('')
+      end
+
+      def read_remote_document(remote_document)
+        process_status = false
+
+        xml = Nokogiri::XML(remote_document)
+        nodes = xml.xpath('//ACORD//InsuranceSvcRs')
+        nodes.each do |node|
+          transaction_type = node.at_xpath('RentPolicyStatusRS/TransactionType').content
+          policy_number = node.at_xpath('RentPolicyStatusRS/PersPolicy/PolicyNumber').content
+
+          if Policy.exists?(number: policy_number)
+            policy = Policy.find_by_number(policy_number)
+
+            case transaction_type
+            when 'N' # TransactionType New Business
+
+            when 'W' # TransactionType Renewal
+
+            when 'P' # TransactionType Pending Cancellation
+
+            when 'X' # TransactionType Rescind Cancellation
+
+            when 'C' # TransactionType Cancellation
+              cancellation_reason = node.at_xpath('RentPolicyStatusRS/PersPolicy/QBE_BusinessSource').content
+              cancel_policy(policy, cancellation_reason)
+            when 'R' # TransactionType Reinstatement
+
+            when 'E' # TransactionType Endorsement
+
+            when 'L' # TransactionType Claim Activity
+
+            end
+          else
+            add_to_duplicate_report(node)
+          end
+        end
+
+        if defined? @duplicate_report
+          puts @duplicate_report.length
+          Qbe::Acord::DuplicateReport.call(@duplicate_report) if @duplicate_report.length > 0
+        end
+
+        return process_status
+      end
+
+      def cancel_policy(policy, reason)
+        reason_map = {
+          'AP' => 'nonpayment',
+          'AR' => 'agent_request',
+          'IR' => 'insured_request',
+          'NP' => 'new_application_nonpayment',
+          'UW' => 'underwriter_cancellation'
+        }
+        policy.cancel(reason_map[reason])
+      end
+
+      def create_duplicate_report()
+        @duplicate_report = Array.new
+      end
+
+      def add_to_duplicate_report(node)
+        create_duplicate_report unless defined? @duplicate_report
+        @duplicate_report << node
+      end
+    end
+  end
+end
---
+++ spec/mailers/previews/carrier_qbe/duplicate_policies_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/carrier_qbe/duplicate_policies
+class CarrierQbe::DuplicatePoliciesPreview < ActionMailer::Preview
+
+end
