fd71bb4c6726394b97048d07451611f22e4e3c88
Muneeb Farooqi <muneebahmadfarooqi@gmail.com>
2023-05-25 00:58:15 +0500

    fixes

--- app/jobs/policies/send_policies_list_job.rb
+++ app/jobs/policies/send_policies_list_job.rb
@@ -24,15 +24,15 @@ module Policies
 
     def generate_csv(policies)
       csv_string = CSV.generate(headers: true) do |csv|
-        csv << ['Number', 'Status', 'T Code', 'Community', 'Building', 'Unit', 'PM Account', 'Agency', 'Effective_date',
+        csv << ['Number', 'Status', 'T Code', 'Community', 'Building', 'Unit', 'PM Account', 'Agency', 'Effective Date',
                 'Expiration Date', 'Cutomer Name', 'Email', 'Product', 'Billing Strategy', 'Update Date', 'Policy Source']
         policies.each do |policy|
           csv << [policy.dig('number'), policy.dig('status'), policy.dig('tcode'), policy.dig('primary_insurable', 'parent_community', 'title'),
                   policy.dig('primary_insurable', 'parent_building', 'title'), policy.dig('primary_insurable', 'title'),
-                  policy.dig('account', 'title'), policy.dig('agency', 'title'), policy.dig('effective_date')&.to_date&.strftime("%B %d, %Y"),
-                  policy.dig('expiration_date')&.to_date&.strftime("%B %d, %Y"), policy.dig('primary_user', 'full_name'),
+                  policy.dig('account', 'title'), policy.dig('agency', 'title'), policy.dig('effective_date')&.to_date&.strftime("%B %e, %Y"),
+                  policy.dig('expiration_date')&.to_date&.strftime("%B %e, %Y"), policy.dig('primary_user', 'full_name'),
                   (policy.dig('primary_user', 'email') || policy.dig('primary_user', 'contact_email')),
-                  policy.dig('policy_type_title'), policy.dig('billing_strategy'), policy.dig('updated_at')&.to_date&.strftime("%B %d, %Y"),
+                  policy.dig('policy_type_title'), policy.dig('billing_strategy'), policy.dig('updated_at')&.to_date&.strftime("%B %e, %Y"),
                   (policy.dig('policy_in_system') == true ? 'Internal' : 'External')]
         end
       end
