4c4e264933b74d3235e7b853491911e9cdf342fa
Dylan Gaines <dylan@dylangaines.com>
2023-05-25 08:19:16 -0700

    Setting up the qbe finance service, eventually want to move all qbe invoice generation in there, updates to renewal preperation service to use new finance service and queue mailer once prepared

--- app/services/qbe/renewal/prepare.rb
+++ app/services/qbe/renewal/prepare.rb
@@ -14,11 +14,14 @@ module Qbe
         raise "Policy record mismatch" unless @policy.is_a?(Policy)
 
         if @policy.update renewal_status: 'PREPARING'
+          term_count = @policy.renew_count.nil? ? 2 : @policy.renew_count + 1
           rate_refresh = Qbe::Renewal::RefreshRates.call(@policy)
-          build_invoices_for_term = true
+          build_invoices_for_term = Qbe::Finance::GenerateInvoicesForTerm.call(@policy, term_count)
 
           if rate_refresh && build_invoices_for_term
-            @policy.update renewal_status: 'PREPARED'
+            RenewalMailer.with(policy: @policy)
+                         .policy_renewing_soon
+                         .deliver_later if @policy.update renewal_status: 'PREPARED'
           else
             @policy.update renewal_status: 'PREPARATION_FAILED'
           end
